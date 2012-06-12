cbaf9e1244a83abaac0c815aca73414b68d2aeed
tonylukasavage <anthony.lukasavage@gmail.com>
2012-06-12 17:05:13 -0400

    added utils.js for common functions

---
+++ Alloy/lib/utils.js
@@ -0,0 +1,39 @@
+// The island of misfit toys... for functions
+
+var path = require('path'),
+	fs = require('fs'),
+	logger = require('../common/logger'),
+	jsp = require("../uglify-js/uglify-js").parser,
+	pro = require("../uglify-js/uglify-js").uglify;
+
+exports.ensureDir = function(p)
+{
+	if (!path.existsSync(p))
+	{
+		logger.debug("Creating directory: "+p);
+		fs.mkdirSync(p);
+	}
+}
+
+exports.stringifyJSON = function(j)
+{
+	var ast = jsp.parse("("+JSON.stringify(j)+")");
+	ast = pro.ast_mangle(ast);
+	var final_code = pro.gen_code(ast,{beautify:true,quote_keys:true}); 
+	return final_code = final_code.substring(1,final_code.length-2); // remove ( ) needed for parsing
+}
+
+exports.pad = function(x)
+{
+	if (x < 10)
+	{
+		return '0' + x;
+	}
+	return x;
+}
+
+exports.die = function(msg) 
+{
+	logger.error(msg);
+	process.exit(1);
+}
