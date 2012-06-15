bac034e6e08d4f8c35e1c58d48ff37a37d2a9cdd
tonylukasavage <anthony.lukasavage@gmail.com>
2012-06-15 13:17:19 -0400

    added _generated folder and app.js for no_ids example

---
+++ examples/no_ids/_generated/app.js
@@ -0,0 +1,21 @@
+/**
+ * Alloy for Titanium by Appcelerator
+ * This is generated code, DO NOT MODIFY - changes will be lost!
+ * Copyright (c) 2012 by Appcelerator, Inc.
+ */
+var Alloy = require("alloy"), _ = Alloy._, A$ = Alloy.A, M$ = Alloy.M, BC$ = Alloy.Backbone.Collection, TFL$ = Ti.UI.FILL, TSZ$ = Ti.UI.SIZE, $ = {};
+
+$.CFG = {}, $.index = A$(Ti.UI.createWindow({
+    backgroundColor: "white",
+    id: "index"
+}), "Window"), $.__alloyId0 = A$(Ti.UI.createView({
+    id: "__alloyId0"
+}), "View", $.index), $.index.add($.__alloyId0), $.__alloyId1 = A$(Ti.UI.createView({
+    backgroundColor: "#a00",
+    id: "__alloyId1"
+}), "View", $.__alloyId0), $.__alloyId0.add($.__alloyId1), $.__alloyId2 = A$(Ti.UI.createButton({
+    title: "my button",
+    id: "__alloyId2"
+}), "Button", $.__alloyId0), $.__alloyId0.add($.__alloyId2), function(exports) {
+    exports.open();
+}($.index);
