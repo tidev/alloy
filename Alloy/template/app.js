c87fd72e86ee7e3c95dd8c24a8e318f71ddc0470
tonylukasavage <anthony.lukasavage@gmail.com>
2012-06-18 16:42:13 -0400

    created app.js template

---
+++ Alloy/template/app.js
@@ -0,0 +1,18 @@
+/**
+ * Alloy for Titanium by Appcelerator
+ * This is generated code, DO NOT MODIFY - changes will be lost!
+ * Copyright (c) 2012 by Appcelerator, Inc.
+ */
+var Alloy = require('alloy'), 
+	_ = Alloy._, 
+	A$ = Alloy.A,
+	M$ = Alloy.M,
+	BC$ = Alloy.Backbone.Collection,
+	TFL$ = Ti.UI.FILL,
+	TSZ$ = Ti.UI.SIZE,
+	$ = {};
+
+<%= config %>
+
+(require('alloy/components/index')).create();
+
