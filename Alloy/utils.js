c3e259da8e7c8d17b8cbae2a7b720d8843dd7e68
tonylukasavage <anthony.lukasavage@gmail.com>
2012-06-20 16:33:02 -0400

    moved uglifying and alloy config generation to utils

--- Alloy/utils.js
+++ Alloy/utils.js
@@ -22,6 +22,51 @@ exports.XML = {
 	}
 };
 
+exports.generateConfig = function(configDir, alloyConfig) {
+	var cf = path.join(configDir,'config.json');
+	if (path.existsSync(cf))
+	{
+		var jf = fs.readFileSync(cf);
+		var j = JSON.parse(jf);
+		var o = j.global || {};
+		if (alloyConfig) {
+			o = _.extend(o, j['env:'+alloyConfig.deploytype]);
+			o = _.extend(o, j['os:'+alloyConfig.platform]);
+		}
+		return "$.CFG = " + JSON.stringify(o) + ";\n";
+	}
+	return '';
+};
+
+exports.processSourceCode = function(code, config) {
+	var defines = {},
+		DEFINES, ast;
+
+	config = config || {};
+	config.deploytype = config.deploytype || 'development';
+	config.beautify = config.beautify || true;
+
+	DEFINES = {
+		OS_IOS : config.platform == 'ios',
+		OS_ANDROID: config.platform == 'android',
+		OS_MOBILEWEB: config.platform == 'mobileweb',
+		ENV_DEV: config.deploytype == 'development',
+		ENV_DEVELOPMENT: config.deploytype == 'development',
+		ENV_TEST: config.deploytype == 'test',
+		ENV_PRODUCTION: config.deploytype == 'production'
+	};
+
+	for (var k in DEFINES) {
+		defines[k] = [ "num", DEFINES[k] ? 1 : 0 ];
+	}
+	
+	ast = jsp.parse(code); // parse code and get the initial AST
+	ast = pro.ast_mangle(ast,{except:['Ti','Titanium'],defines:defines}); // get a new AST with mangled names
+	ast = pro.ast_squeeze(ast); // get an AST with compression optimizations
+	
+	return pro.gen_code(ast,{beautify:config.beautify}); 
+};
+
 exports.copyFileSync = function(srcFile, destFile) 
 {
 	var BUF_LENGTH = 64 * 1024, 
