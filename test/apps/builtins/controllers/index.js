ab278ec66598ccc5e1fc892de310376570ba5f0b
tonylukasavage <anthony.lukasavage@gmail.com>
2012-06-22 11:10:11 -0400

    added basic "builtins" example

---
+++ test/apps/builtins/controllers/index.js
@@ -0,0 +1,16 @@
+// These "builtin" requires will be detected by the alloy compile process.
+// You do not need to manually copy them to their project location.
+// When the alloy compile finds these require calls, it will include them
+// in your Titanium project as "Resources/alloy/animation.js" and
+// "Resources/alloy/string.js" automatically.
+var animation = require('alloy/animation');
+	string = require('alloy/string');
+
+$.shake.on('click', function(e) { 
+	animation.shake($.mover);
+});
+$.trim.on('click', function(e) {
+	$.label.text = string.trim($.label.text);
+});
+
+$.index.open();
