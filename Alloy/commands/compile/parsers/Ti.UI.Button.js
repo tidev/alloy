ed282130ed5b65fef46a7ca9062c6dd1a52a7e76
tonylukasavage <anthony.lukasavage@gmail.com>
2012-06-25 15:44:02 -0400

    Added parsers for Ti.UI.Button and Ti.UI.Label

---
+++ Alloy/commands/compile/parsers/Ti.UI.Button.js
@@ -0,0 +1,13 @@
+var CU = require('../compilerUtils'),
+	U = require('../../../utils');
+
+exports.parse = function(node, state, args) {
+	// Get button title from node text, if present
+	var nodeText = U.XML.getNodeText(node);
+	if (nodeText) {
+		state.styles = CU.addStyleById(state.styles, args.id, 'title', nodeText);
+	}
+
+	// Generate runtime code using default
+	return require('./default').parse(node, state, args);
+};
---
+++ Alloy/commands/compile/parsers/Ti.UI.Label.js
@@ -0,0 +1,13 @@
+var CU = require('../compilerUtils'),
+	U = require('../../../utils');
+
+exports.parse = function(node, state, args) {
+	// Get label text from node text, if present
+	var nodeText = U.XML.getNodeText(node);
+	if (nodeText) {
+		state.styles = CU.addStyleById(state.styles, args.id, 'text', nodeText);
+	}
+
+	// Generate runtime code using default
+	return require('./default').parse(node, state, args);
+};
