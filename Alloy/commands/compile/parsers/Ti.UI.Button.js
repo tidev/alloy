59a62be8d1859169ce644cb8348707ab65c626ac
tonylukasavage <anthony.lukasavage@gmail.com>
2012-06-26 22:38:26 -0400

    generate own parser args with getParserArgs()

--- Alloy/commands/compile/parsers/Ti.UI.Button.js
+++ Alloy/commands/compile/parsers/Ti.UI.Button.js
@@ -1,13 +1,14 @@
 var CU = require('../compilerUtils'),
 	U = require('../../../utils');
 
-exports.parse = function(node, state, args) {
+exports.parse = function(node, state) {
 	// Get button title from node text, if present
-	var nodeText = U.XML.getNodeText(node);
+	var args = CU.getParserArgs(node, state),
+		nodeText = U.XML.getNodeText(node);
 	if (nodeText) {
 		state.styles = CU.addStyleById(state.styles, args.id, 'title', nodeText);
 	}
 
 	// Generate runtime code using default
-	return require('./default').parse(node, state, args);
+	return require('./default').parse(node, state);
 };
