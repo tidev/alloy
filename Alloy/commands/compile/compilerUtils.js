3e8ca2a8730ded580517c0e26424253ec1d7640f
tonylukasavage <anthony.lukasavage@gmail.com>
2012-07-02 10:46:37 -0400

    fixed platform check

--- Alloy/commands/compile/compilerUtils.js
+++ Alloy/commands/compile/compilerUtils.js
@@ -47,6 +47,7 @@ var implicitNamespaces = {
 var STYLE_ALLOY_TYPE = '__ALLOY_TYPE__';
 var STYLE_CONST_PREFIX = '__ALLOY_CONST__--';
 var STYLE_EXPR_PREFIX = '__ALLOY_EXPR__--';
+var PLATFORMS = ['ios', 'android', 'mobileweb'];
 
 //////////////////////////////////////
 ////////// public interface //////////
@@ -64,11 +65,35 @@ exports.getParserArgs = function(node, state) {
 	var name = node.nodeName,
 		ns = node.getAttribute('ns') || implicitNamespaces[name] || 'Ti.UI',
 		req = node.getAttribute('require'),
-		id = node.getAttribute('id') || state.defaultId || req || exports.generateUniqueId();
+		id = node.getAttribute('id') || state.defaultId || req || exports.generateUniqueId(),
+		platform = node.getAttribute('platform'),
+		platformObj = {};
 
 	ns = ns.replace(/^Titanium\./, 'Ti');
 	node.setAttribute('id', id);
 	if (state.defaultId) { delete state.defaultId; }
+
+	// process the platform attribute
+	if (platform) {
+		_.each((platform).split(','), function(p) {
+			var matches = U.trim(p).match(/^(\!{0,1})(.+)/);
+			if (matches !== null) {
+				var negate = matches[1];
+				var name = matches[2];
+				if (_.contains(PLATFORMS, name)) {
+					if (negate === '!') {
+						_.each(_.without(PLATFORMS, name), function(n) {
+							platformObj[n] = true;
+						});
+					} else {
+						platformObj[name] = true;
+					}
+					return;
+				}
+			}
+			U.die('Invalid platform type found: ' + p);
+		});
+	}
 	
 	return {
 		ns: ns,
@@ -78,6 +103,7 @@ exports.getParserArgs = function(node, state) {
 		symbol: exports.generateVarName(id),
 		classes: node.getAttribute('class').split(' ') || [],	
 		parent: state.parent || {},
+		platform: !platformObj ? undefined : platformObj
 	};
 };
 
