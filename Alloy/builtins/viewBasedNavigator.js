7e96e4d24f4c74bbebd1b1cae8882c262708b600
tonylukasavage <anthony.lukasavage@gmail.com>
2012-07-05 11:07:37 -0400

    reimported viewBasedNavigator without uglify, supports passing in component objects

--- Alloy/builtins/viewBasedNavigator.js
+++ Alloy/builtins/viewBasedNavigator.js
@@ -1,51 +1,107 @@
+var _win;
+var _openViews = [];
+
+exports.use = function (win) {
+    if (_win) {
+        _win.close();
+    }
+    _win = win;
+    _openViews = [];
+};
+
+exports.getWindow = function () {
+    return _win;
+};
+
+
+exports.open = function (evt) {
+    var component = evt.component || require("alloy/components/" + evt.id).create(evt);
+    var view = component.getRoot();
+
+    // Close anyone with the same ID that is already open.
+    exports.close({ id: evt.id, instant: true });
+
+    _openViews.push({
+        id: evt.id,
+        component: component,
+        view: view
+    });
+    if (!evt.instant) {
+        view.opacity = 0;
+    }
+    _win.add(view);
+    component.open(function () {
+        if (!evt.instant) {
+            view.animate(Ti.UI.createAnimation({
+                opacity: 1,
+                duration: 300
+            }));
+            view = null;
+        }
+    });
+
+    evt = null;
+
+    return view;
+};
+
+
+exports.close = function (evt) {
+    var view, component;
+    for (var i = _openViews.length - 1; i >= 0; i--) {
+        if (evt.id == _openViews[i].id) {
+            view = _openViews[i].view;
+            component = _openViews[i].component;
+            // TODO: give topmost view an API point for overriding closing; lets Android handle back button gracefully.
+            _openViews.splice(i, 1);
+            break;
+        }
+    }
+
+    if (view) {
+        if (evt.instant) {
+            _win.remove(view);
+        }
+        else {
+            view.animate(Ti.UI.createAnimation({
+                opacity: 0,
+                duration: 300
+            }));
+            if (component) {
+                component.cleanup && component.cleanup();
+                delete component.cleanup;
+            }
+            setTimeout(function () {
+                _win.remove(view);
+                view = null;
+            }, 1000);
+        }
+    }
+
+    evt = null;
+    return view;
+};
+
 function closeTopmost() {
-    if (_openViews.length == 1) _win.close(); else {
+    if (_openViews.length == 1) {
+        _win.close();
+    }
+    else {
         var view = _openViews.pop();
         _win.remove(view.view);
     }
 }
 
-var _win, _openViews = [];
-
-exports.use = function(win) {
-    _win && _win.close(), _win = win, _openViews = [];
-}, exports.getWindow = function() {
-    return _win;
-}, exports.open = function(evt) {
-    var component = require("alloy/components/" + evt.id).create(evt), view = component[evt.id];
-    return exports.close({
-        id: evt.id,
-        instant: !0
-    }), _openViews.push({
-        id: evt.id,
-        view: view
-    }), evt.instant || (view.opacity = 0), _win.add(view), component.open(function() {
-        evt.instant || (view.animate(Ti.UI.createAnimation({
-            opacity: 1,
-            duration: 300
-        })), view = null);
-    }), evt = null, view;
-}, exports.close = function(evt) {
-    var view;
-    for (var i = _openViews.length - 1; i >= 0; i--) if (evt.id == _openViews[i].id) {
-        view = _openViews[i].view, _openViews.splice(i, 1);
-        break;
-    }
-    return view && (evt.instant ? _win.remove(view) : (view.animate(Ti.UI.createAnimation({
-        opacity: 0,
-        duration: 300
-    })), view.cleanup && view.cleanup(), delete view.cleanup, setTimeout(function() {
-        _win.remove(view), view = null;
-    }, 1e3))), evt = null, view;
-};
-
-if (Ti.Platform.name === "iPhone OS") {
+if (Ti.Platform.name === 'iPhone OS') {
     var lastBottom;
-    Ti.App.addEventListener("keyboardFrameChanged", function(evt) {
+    Ti.App.addEventListener('keyboardFrameChanged', function keyboardFrameChanged(evt) {
         var newBottom = evt.keyboardFrame.y;
-        lastBottom != newBottom && (_win.animate({
-            bottom: Ti.Platform.displayCaps.platformHeight - newBottom,
-            duration: 50
-        }), lastBottom = newBottom);
+        if (lastBottom != newBottom) {
+            _win.animate({
+                bottom: Ti.Platform.displayCaps.platformHeight - newBottom,
+                duration: 50
+            });
+            lastBottom = newBottom;
+        }
     });
 }
