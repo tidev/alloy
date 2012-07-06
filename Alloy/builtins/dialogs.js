0730b9a552863704547f31556125af1dae87b72d
tonylukasavage <anthony.lukasavage@gmail.com>
2012-07-06 17:51:51 -0400

    add dialogs builtin

---
+++ Alloy/builtins/dialogs.js
@@ -0,0 +1,15 @@
+exports.confirm = function (args) {
+    var alertDialog = Ti.UI.createAlertDialog({
+        title: args.title || 'Confirm',
+        message: args.message || 'Are you sure?',
+        buttonNames: [args.no || 'No', args.yes || 'Yes'],
+        cancel: 0
+    });
+    alertDialog.addEventListener('click', function (evt) {
+        if (evt.index) {
+            args.callback && args.callback(args.evt || {});
+        }
+        args = null;
+    });
+    alertDialog.show();
+};
