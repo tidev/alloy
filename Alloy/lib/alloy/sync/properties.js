138a064b643f85badf7c3ed66b5b9f2667e7b2a6
tonylukasavage <anthony.lukasavage@gmail.com>
2012-07-09 19:05:28 -0400

    hey look at that, Ti.App.Properties syncing!

--- Alloy/lib/alloy/sync/properties.js
+++ Alloy/lib/alloy/sync/properties.js
@@ -1,40 +1,33 @@
 /**
  * Ti.App.Properties sync adapter which will store all models locally
  */
-var _ = require("alloy/underscore")._, 
-	db;
+var _ = require("alloy/underscore")._;
 
 function TiAppPropertiesSync(model) {
 	this.model = model;
-	this.table = model.config.adapter.tablename;
 	this.columns = model.config.columns;
-
 	var self = this;
 
 	this.create = function(opts) {
-		// need to increment 
-		// model.set("id", Ti.App.guid);
-		// var filename = model.config.adapter.filename;
-		// var f = Titanium.Filesystem.getFile(Titanium.Filesystem.applicationDataDirectory, filename);
-  //   	f.write(JSON.stringify(model));
+		_.each(self.columns, function(v,k) {
+			Ti.App.Properties['set'+v](k, self.model.get(k));
+		});
 	};
 
 	this.read = function(opts) {
-		var filename = model.config.adapter.filename;
-		var f = Titanium.Filesystem.getFile(Titanium.Filesystem.applicationDataDirectory, filename);
-		if (f.exists()) { Ti.API.info("Fetch results = " + f.read()); }
+		_.each(self.columns, function(v,k) {
+			var obj = {};
+			obj[k] = Ti.App.Properties['get'+v](k, self.model.config.defaults[k]);
+			self.model.set(obj); 
+		});
 	};
 
 	this.update = function(opts) {
-		var filename = model.config.adapter.filename;
-		var f = Titanium.Filesystem.getFile(Titanium.Filesystem.applicationDataDirectory, filename);
-    	f.write(JSON.stringify(model));
+		self.create(opts);
 	};
 	
 	this['delete'] = function(opts) {
-		// var filename = model.config.adapter.filename;
-		// var f = Titanium.Filesystem.getFile(Titanium.Filesystem.applicationDataDirectory, filename);
-		// if (f.exists()) { f.deleteFile(); }
+		// TODO: this ^^^
 	};
 }
 
@@ -42,6 +35,5 @@ function Sync(model, method, opts) {
 	var sync = new TiAppPropertiesSync(model);
 	return sync[method](opts);
 }
-
 
 module.exports.sync = Sync;
