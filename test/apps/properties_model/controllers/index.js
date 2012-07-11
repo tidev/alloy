07ac12ccdebdada99b2118d16f0c9408a7875be5
tonylukasavage <anthony.lukasavage@gmail.com>
2012-07-11 11:58:40 -0400

    updated properties_model test app to show off more features and Backbone integration

--- test/apps/properties_model/controllers/index.js
+++ test/apps/properties_model/controllers/index.js
@@ -1,19 +1,27 @@
-var app = new Alloy.Models.App; // new (require('alloy/models/App').Model)
+var app = new Alloy.Models.App; 
 
 // TODO: Should we always do this? Could put it in the model code itself
 // save all changes to Ti.App.Properties
-app.on('change', function() { app.save(); });
+app.on('change', function() { 
+	app.save(); 
+});
 
 // Change label when 'count' changes on model
 app.on('change:count', function(model) {
-	$.label.text = 'count: ' + model.get('count');
+	$.label.text = 'model: ' + JSON.stringify(model.attributes);
 });
 
 // fetch model from Ti.App.Properties, or use defaults
 app.fetch();
 
-// increment model 'count' 
-$.button.on('click', function() {
+// event handlers 
+$.create.on('click', function() {
+	app.save(app.defaults);
+});
+$.destroy.on('click', function() {
+	app.destroy();
+});
+$.increment.on('click', function() {
 	app.set({'count':app.get('count')+1});
 });
 
