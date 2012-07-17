a22803ff3bc3a91d83ba7438ac619a564f1eb025
tonylukasavage <anthony.lukasavage@gmail.com>
2012-07-17 13:14:53 -0400

    added directory and alloy path constants

--- Alloy/common/constants.js
+++ Alloy/common/constants.js
@@ -1,3 +1,5 @@
+exports.ALLOY_DIR = 'app';
+
 exports.FILE_EXT = {
 	VIEW: 'xml',
 	STYLE: 'tss',
@@ -5,4 +7,18 @@ exports.FILE_EXT = {
 	MIGRATION: 'js',
 	CONTROLLER: 'js',
 	CONFIG: 'json'
+};
+
+exports.DIR = {
+	VIEW: 'views',
+	STYLE: 'styles',
+	CONTROLLER: 'controllers',
+	MODEL: 'models',
+	MIGRATION: 'migrations',
+	CONFIG: 'config',
+	ASSETS: 'assets',
+	WIDGET: 'widgets',
+	LIB: 'lib',
+	VENDOR: 'vendor',
+	COMPONENT: 'components'
 };
