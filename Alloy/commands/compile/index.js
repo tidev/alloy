b12fbe6577d0694a4d275125d124d780cc220e08
tonylukasavage <anthony.lukasavage@gmail.com>
2012-07-17 13:15:46 -0400

    refactored the compile command to allow for folder organization of views, controlles, and widgets

--- Alloy/commands/compile/index.js
+++ Alloy/commands/compile/index.js
@@ -88,19 +88,22 @@ module.exports = function(args, program) {
 
 	// Process all views, including all those belonging to widgets
 	var viewCollection = U.getWidgetDirectories(outputPath);
-	viewCollection.push({ dir: path.join(outputPath,'app') });
+	viewCollection.push({ dir: path.join(outputPath,CONST.ALLOY_DIR) });
 	_.each(viewCollection, function(collection) {
-		_.each(fs.readdirSync(path.join(collection.dir,'views')), function(view) {
+		//_.each(fs.readdirSync(path.join(collection.dir,'views')), function(view) {
+		_.each(wrench.readdirSyncRecursive(path.join(collection.dir,CONST.DIR.VIEW)), function(view) {
 			if (viewRegex.test(view)) {
-				var basename = path.basename(view, '.'+CONST.FILE_EXT.VIEW);
-				parseView(basename, collection.dir, basename, collection.manifest);
+				console.log(view);
+				// var basename = path.basename(view, '.'+CONST.FILE_EXT.VIEW);
+				// parseView(basename, collection.dir, basename, collection.manifest);
+				parseView(view, collection.dir, collection.manifest);
 			}
 		});
 	});
 
 	// copy assets and libraries
-	U.copyAlloyDir(inputPath, ['assets','lib'], compileConfig.dir.resources);
-	U.copyAlloyDir(inputPath, 'vendor', path.join(compileConfig.dir.resources,'vendor'));
+	U.copyAlloyDir(inputPath, [CONST.DIR.ASSETS,CONST.DIR.LIB], compileConfig.dir.resources);
+	U.copyAlloyDir(inputPath, CONST.DIR.VENDOR, path.join(compileConfig.dir.resources,CONST.DIR.VENDOR));
 
 	// generate app.js
 	var appJS = path.join(compileConfig.dir.resources,"app.js");
@@ -133,27 +136,45 @@ module.exports = function(args, program) {
 ///////////////////////////////////////
 ////////// private functions //////////
 ///////////////////////////////////////
-function parseView(viewName,dir,viewid,manifest) {
-	var template = {
-		viewCode: '',
-		controllerCode: '',
-		onCreate: '' 
-	};
-	var state = { parent: {} };
-	var vd = dir ? path.join(dir,'views') : compileConfig.dir.views; 
-	var sd = dir ? path.join(dir,'styles') : compileConfig.dir.styles; 
+// function parseView(viewName,dir,viewId,manifest) {
+function parseView(view,dir,manifest) {
+	// validate parameters
+	if (!view) { U.die('Undefined view passed to parseView()'); }
+	if (!dir) { U.die('Failed to parse view "' + view + '", no directory given'); }
 
-	var viewFile = path.join(vd,viewName+"."+CONST.FILE_EXT.VIEW);
-	if (!path.existsSync(viewFile)) {
-		logger.warn('No ' + CONST.FILE_EXT.VIEW + ' view file found for view ' + viewFile);
+	var basename = path.basename(view, '.'+CONST.FILE_EXT.VIEW);
+		dirname = path.dirname(view),
+		viewName = basename,
+		viewId = basename,
+		template = {
+			viewCode: '',
+			controllerCode: '',
+			onCreate: '' 
+		},
+		state = { parent: {} },
+		files = {};
+
+	// create a list of file paths
+	_.each(['VIEW','STYLE','CONTROLLER'], function(fileType) {
+		var tmp = path.join(dir,CONST.DIR[fileType]);
+		if (dirname) { tmp = path.join(tmp,dirname); }
+		files[fileType] = path.join(tmp,viewName+'.'+CONST.FILE_EXT[fileType]);
+	});
+	files.COMPONENT = path.join(compileConfig.dir.resourcesAlloy,CONST.DIR.COMPONENT);
+	if (dirname) { files.COMPONENT = path.join(files.COMPONENT,dirname); }
+	files.COMPONENT = path.join(files.COMPONENT,viewName+'.js');
+
+	// validate view
+	if (!path.existsSync(files.VIEW)) {
+		logger.warn('No ' + CONST.FILE_EXT.VIEW + ' view file found for view ' + files.VIEW);
 		return;
 	}
 
-	var styleFile = path.join(sd,viewName+"."+CONST.FILE_EXT.STYLE);
-	var styles = CU.loadStyle(styleFile);
-	state.styles = styles;
+	// Load the style and update the state
+	state.styles = CU.loadStyle(files.STYLE);
 
-	var xml = fs.readFileSync(viewFile,'utf8');
+	// read and parse the view file
+	var xml = fs.readFileSync(files.VIEW,'utf8');
 	var doc = new DOMParser().parseFromString(xml);
 
 	// Give our document the <Alloy> root element if it doesn't already have one
@@ -163,7 +184,7 @@ function parseView(viewName,dir,viewid,manifest) {
 		doc = tmpDoc;
 	}
 	var docRoot = doc.documentElement;
-	var id = viewid || doc.documentElement.getAttribute('id') || viewName;
+	var id = viewId || doc.documentElement.getAttribute('id') || viewName;
 
 	// handle component-level events
 	_.each(['onCreate'], function(evt) {
@@ -177,30 +198,29 @@ function parseView(viewName,dir,viewid,manifest) {
 		template.viewCode += CU.generateNode(
 			rootChildren[i],
 			state,
-			i === 0 ? (viewid||viewName) : undefined,
+			i === 0 ? (viewId||viewName) : undefined,
 			i === 0);
 	}
 	template.controllerCode += CU.loadController(files.CONTROLLER);
 
-	// create commonjs module for this view/controller
+	// create component module code for this view/controller or widget
 	var code = _.template(fs.readFileSync(path.join(compileConfig.dir.template, 'component.js'), 'utf8'), template);
 	try {
-		code = CU.processSourceCode(code, compileConfig.alloyConfig, viewName+'.js');
+		code = CU.processSourceCode(code, compileConfig.alloyConfig, files.COMPONENT);
 	} catch (e) {
 		logger.error(code);
 		U.die(e);
 	}
 
+	// Write the view or widget to its runtime file
 	if (manifest) {
-		wrench.mkdirSyncRecursive(path.join(compileConfig.dir.resourcesAlloy, 'widgets', manifest.id, 'components'), 0777);
-		CU.copyWidgetAssets(path.join(dir,'assets'), compileConfig.dir.resources, manifest.id);
-		fs.writeFileSync(path.join(compileConfig.dir.resourcesAlloy, 'widgets', manifest.id, 'components', viewName + '.js'), code);
+		var widgetDir = dirname ? path.join(CONST.DIR.COMPONENT,dirname) : CONST.DIR.COMPONENT;
+		wrench.mkdirSyncRecursive(path.join(compileConfig.dir.resourcesAlloy, CONST.DIR.WIDGET, manifest.id, widgetDir), 0777);
+		CU.copyWidgetAssets(path.join(dir,CONST.DIR.ASSETS), compileConfig.dir.resources, manifest.id);
+		fs.writeFileSync(path.join(compileConfig.dir.resourcesAlloy, CONST.DIR.WIDGET, manifest.id, widgetDir, viewName + '.js'), code);
 	} else {
-		fs.writeFileSync(path.join(compileConfig.dir.resourcesAlloy, 'components', viewName + '.js'), code);
-	}
-}
-
-	} else {
+		wrench.mkdirSyncRecursive(path.dirname(files.COMPONENT), 0777);
+		fs.writeFileSync(files.COMPONENT, code);
 	}
 }
 
