08bd9f5b1c21e9178ca2ca428005c1fe200e2924
tonylukasavage <anthony.lukasavage@gmail.com>
2012-07-20 17:26:08 -0400

    added installModule() to utils

--- Alloy/utils.js
+++ Alloy/utils.js
@@ -42,6 +42,71 @@ exports.XML = {
 	}
 };
 
+exports.installModule = function(dir, opts)
+{
+	var tiapp = path.join(dir,'tiapp.xml');
+	if (path.existsSync(tiapp))
+	{
+		var xml = fs.readFileSync(tiapp);
+		var doc = new DOMParser().parseFromString(String(xml));
+		var modules = doc.documentElement.getElementsByTagName("modules");
+		var found = false;
+
+		if (modules.length > 0)
+		{
+			var items = modules.item(0).getElementsByTagName('module');
+			if (items.length > 0)
+			{
+				for (var c=0;c<items.length;c++)
+				{
+					var mod = items.item(c);
+					var name = exports.XML.getNodeText(mod);
+					if (name == opts.id)
+					{
+						found = true;
+						break;
+					}
+				}
+			}
+		}
+		
+		if (!found)
+		{
+			var node = doc.createElement('module');
+			if (opts.platform) {
+				node.setAttribute('platform',opts.platform);
+			}
+			node.setAttribute('version',opts.version || '1.0');
+			var text = doc.createTextNode(opts.id);
+			node.appendChild(text);
+			
+			var pna = null;
+			
+			// install the plugin into tiapp.xml
+			if (modules.length == 0)
+			{
+				var pn = doc.createElement('modules');
+				doc.documentElement.appendChild(pn);
+				doc.documentElement.appendChild(doc.createTextNode("\n"));
+				pna = pn;
+			}
+			else
+			{
+				pna = modules.item(0);
+			}
+			
+			pna.appendChild(node);
+			pna.appendChild(doc.createTextNode("\n"));
+			
+			var serializer = new XMLSerializer();
+			var newxml = serializer.serializeToString(doc);
+			
+			fs.writeFileSync(tiapp,newxml,'utf-8');
+			logger.info("Installed '" + opts.id + "' module to "+tiapp);
+		}
+	}
+}
+
 exports.copyAlloyDir = function(appDir, sources, destDir) {
 	var sources = _.isArray(sources) ? sources : [sources];
 	_.each(sources, function(source) {
