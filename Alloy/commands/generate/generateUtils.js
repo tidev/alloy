330367a4c76d62e83bd97023427d98236902b422
tonylukasavage <anthony.lukasavage@gmail.com>
2012-07-23 13:41:24 -0400

    all generators handle folder organization, refactor, cleanup

--- Alloy/commands/generate/generateUtils.js
+++ Alloy/commands/generate/generateUtils.js
@@ -1,17 +1,46 @@
-var CONST = require('../../common/constants');
+var basePath = '../../';
+var path = require('path'),
+	fs = require('fs'),
+	wrench = require('wrench'),
+	alloyRoot = path.join(__dirname,'..','..'),
+	_ = require(basePath + 'lib/alloy/underscore')._,
+	U = require(basePath + 'utils'),
+	CONST = require(basePath + 'common/constants');
 
-exports.pad = function(x)
-{
-	if (x < 10)
-	{
+function pad(x) {
+	if (x < 10) {
 		return '0' + x;
 	}
 	return x;
 }
 
-exports.generateMigrationFileName = function(t)
-{
+exports.generateMigrationFileName = function(t) {
 	var d = new Date;
-	var s = String(d.getUTCFullYear()) + String(exports.pad(d.getUTCMonth())) + String(exports.pad(d.getUTCDate())) + String(exports.pad(d.getUTCHours())) + String(exports.pad(d.getUTCMinutes())) + String(d.getUTCMilliseconds())
+	var s = String(d.getUTCFullYear()) + String(pad(d.getUTCMonth())) + String(pad(d.getUTCDate())) + String(pad(d.getUTCHours())) + String(pad(d.getUTCMinutes())) + String(d.getUTCMilliseconds())
 	return s + '_' + t + '.' + CONST.FILE_EXT.MIGRATION;
+}
+
+exports.generate = function(name, type, program, args) {
+	var ext = '.'+CONST.FILE_EXT[type];
+	var templatePath = path.join(alloyRoot,'template',type.toLowerCase()+ext);
+	var dir = path.join(program.outputPath,CONST.DIR[type],path.dirname(name));
+	var file = path.join(dir,path.basename(name,ext)+ext);
+
+	if (path.existsSync(file) && !program.force) {
+		U.die(" file already exists: " + file);
+	}
+	if (!path.existsSync(dir)) {
+		wrench.mkdirSyncRecursive(dir);
+	}
+
+	var templateContents = fs.readFileSync(templatePath,'utf8');
+	if (args.templateFunc) { templateContents = args.templateFunc(templateContents); }
+	var code = _.template(templateContents, args.template || {});
+	fs.writeFileSync(file, code);
+
+	return {
+		file: file,
+		dir: dir,
+		code: code
+	};
 }
--- Alloy/commands/generate/targets/migration.js
+++ Alloy/commands/generate/targets/migration.js
@@ -1,19 +1,18 @@
 var path = require('path'),
-	fs = require('fs'),
-	U = require('../../../utils'),
 	GU = require('../generateUtils'),
 	CONST = require('../../../common/constants'),
-	_ = require("../../../lib/alloy/underscore")._,
 	logger = require('../../../common/logger');
 
-module.exports = function(name, args, program) {
-	var migrationsDir = path.join(program.outputPath,'migrations');
-	U.ensureDir(migrationsDir);
-	
-	var templatePath = path.join(__dirname,'..','..','..','template','migration.' + CONST.FILE_EXT.MIGRATION);
-	var mf = path.join(migrationsDir, GU.generateMigrationFileName(name));
-	var md = _.template(fs.readFileSync(templatePath,'utf8'),{up:'',down:''});
-	fs.writeFileSync(mf,md);
-
-	logger.info('Generated empty migration named '+name);
+module.exports = function(name, args, program, template) {
+	var type = 'MIGRATION';
+	var dir = path.dirname(name);
+	name = path.join(dir,
+		GU.generateMigrationFileName(path.basename(name,'.'+CONST.FILE_EXT.MIGRATION)));
+	var info = GU.generate(name, type, program, {
+		template: template || {
+			up:'',
+			down:''
+		}
+	});
+	logger.info('Generated ' + type.toLowerCase() + ' named ' + name);
 }
