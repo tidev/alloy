20c9d974e78d75bd9018d76fdb174208503d920a
tonylukasavage <anthony.lukasavage@gmail.com>
2012-07-23 09:42:37 -0400

    fixed migration generation

--- Alloy/commands/generate/targets/migration.js
+++ Alloy/commands/generate/targets/migration.js
@@ -2,6 +2,7 @@ var path = require('path'),
 	fs = require('fs'),
 	U = require('../../../utils'),
 	GU = require('../generateUtils'),
+	CONST = require('../../../common/constants'),
 	_ = require("../../../lib/alloy/underscore")._,
 	logger = require('../../../common/logger');
 
@@ -11,7 +12,7 @@ module.exports = function(name, args, program) {
 	
 	var templatePath = path.join(__dirname,'..','..','..','template','migration.' + CONST.FILE_EXT.MIGRATION);
 	var mf = path.join(migrationsDir, GU.generateMigrationFileName(name));
-	var md = _.template(fs.readFileSync(templatePath,'utf8'),{});
+	var md = _.template(fs.readFileSync(templatePath,'utf8'),{up:'',down:''});
 	fs.writeFileSync(mf,md);
 
 	logger.info('Generated empty migration named '+name);
