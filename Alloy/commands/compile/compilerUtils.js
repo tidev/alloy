3b125b6efa50eb824aa039dd34cb7a5aec679e53
tonylukasavage <anthony.lukasavage@gmail.com>
2012-07-26 15:53:58 -0400

    add containing {} to style if not already present

--- Alloy/commands/compile/compilerUtils.js
+++ Alloy/commands/compile/compilerUtils.js
@@ -297,6 +297,7 @@ exports.loadStyle = function(tssFile) {
 	if (path.existsSync(tssFile)) {
 		var contents = fs.readFileSync(tssFile, 'utf8');
 		if (!/^\s*$/.test(contents)) {
+			contents = /^\s*\{.+\}\s*$/.test(contents) ? contents : '{' + contents + '}';
 			var code = processTssFile(contents);
 			var json = JSON.parse(code);
 			optimizer.optimizeStyle(json);
