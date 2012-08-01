3f949ee1369062592f77c6786b3feb7f0b8ecadd
tonylukasavage <anthony.lukasavage@gmail.com>
2012-08-01 18:36:05 -0400

    fixed lifecycle event checks, added todo for compile time handling

--- Alloy/template/component.js
+++ Alloy/template/component.js
@@ -7,10 +7,10 @@ function __layout() {
 	<%= viewCode %>
 }
 
+// TODO: make these assignments at compile time by manipulating 
+//       the uglifyjs AST
 var x = { __layout: __layout };
-if (_.isFunction(beforeLayout)) { x.beforeLayout = beforeLayout; }
-if (_.isFunction(controller)) { x.controller = controller; }
+try { x.beforeLayout = beforeLayout; } catch(e) {}
+try { x.controller = controller; } catch(e) {}
 
-var BaseController = require('BaseController');
-var Controller = BaseController.extend(x);
-module.exports = Controller;
+module.exports = require('BaseController').extend(x);
