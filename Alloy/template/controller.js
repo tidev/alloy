6012073c7ba426e12681119c4681c7fe448860bf
tonylukasavage <anthony.lukasavage@gmail.com>
2012-08-02 17:46:09 -0400

    default controller stripped down to nothing again

--- Alloy/template/controller.js
+++
@@ -1,8 +0,0 @@
-$.parentController = Alloy.getController('BaseController');
-
-// preLayout() is optional. It's safe to delete.
-function preLayout(args) {}
-
-// postLayout() is also optional, but most often all your app 
-// logic will be contained in this function.
-function postLayout(args) {}
