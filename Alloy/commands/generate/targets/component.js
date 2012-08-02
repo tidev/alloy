56449936bdedaf616211554f8a6f3662b060b370
Russ McMahon <rmcmahon@appcelerator.com>
2012-08-02 15:24:47 -0700

    Remove seperate model, view, controller, component generators and now in "generate controller NAME" the controller style view file is created.

--- Alloy/commands/generate/targets/component.js
+++
@@ -1,9 +0,0 @@
-var _ = require("../../../lib/alloy/underscore")._,
-	logger = require('../../../common/logger');
-
-module.exports = function(name, args, program) {
-	_.each(['view', 'controller'], function(target) {
-		(require('./' + target))(name, args, program);
-	});
-	logger.info('Generated view-controller named '+name);
-}
