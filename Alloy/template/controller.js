b75eae1e97b42f9c4eb3bb9ffd23e9f0ee099543
tonylukasavage <anthony.lukasavage@gmail.com>
2012-08-02 11:33:12 -0400

    update "new" project template, moved inline docs

--- Alloy/template/controller.js
+++ Alloy/template/controller.js
@@ -1,38 +1,9 @@
-// Change parentController if you want to inherit from a different
-// controller. In most cases you'll leave this as it is. You can
-// also delete it and 'BaseController' will be used by default.
 $.parentController = Alloy.getController('BaseController');
 
 function preLayout(args) {
-	// add your code here
+	// Add code, or delete this function
 }
 
 function postLayout(args) {
-	// Add your code here
+	// Add code, or delete this function
 }
-
-// Controller Lifecycle
-// --------------------
-// __init() --> preLayout() --> __layout() --> postLayout()
-//
-//  __init: [AUTO-GENERATED]
-//          Created by Alloy at compile time. Sets context of the $
-//          variable. Don't define your own __init() function, 
-//          you'll break stuff.
-//
-//  preLayout: [optional] 
-//             Code that needs to be executed before the view
-//             hierarchy is established. Not needed in most cases.
-//             The $ variable does not yet have access to IDed 
-//             elements from the view hierarchy yet. 
-//
-//  __layout: [AUTO-GENERATED]
-//            Created by Alloy at compile time. Creates the view
-//            hierarchy code based on your corresponding markup.
-//            Like __init(), don't define your own. 
-//
-//  postLayout: [optional]
-//              Your primary coding area. This code is executed 
-//              after all Alloy initialization and your markup's
-//              view hierarchy is created. All IDed elements from
-//              your markup are accessible from the $ variable.
--- Alloy/template/default/index.js
+++ Alloy/template/default/index.js
@@ -1,16 +1,43 @@
-var Alloy = require('alloy'),
-	Backbone = Alloy.Backbone,
-	_ = Alloy._,
-	$;
+// Change parentController if you want to inherit from a different
+// controller. In most cases you'll leave this as it is. You can
+// also delete this line and 'BaseController' will be used by default.
+$.parentController = Alloy.getController('BaseController');
 
-function init(args) {
-	$ = this;
-}
+// preLayout() is optional. It's safe to delete.
+function preLayout(args) {}
 
-function controller(args) {
-	$.t.on('click',function(e) { 
-		alert($.t.text);
+// postLayout() is also optional, but most often all your app 
+// logic will be contained in this function.
+function postLayout(args) {
+	$.label.on('click',function(e) { 
+		alert($.label.text);
 	});
 
 	$.index.open();
 }
+
+// Controller Lifecycle
+// --------------------
+// __init() --> preLayout() --> __layout() --> postLayout()
+//
+//  __init: [AUTO-GENERATED]
+//          Created by Alloy at compile time. Sets context of the $
+//          variable. Don't define your own __init() function, 
+//          you'll break stuff.
+//
+//  preLayout: [optional] 
+//             Code that needs to be executed before the view
+//             hierarchy is established. Not needed in most cases.
+//             The $ variable does not yet have access to IDed 
+//             elements from the view hierarchy. 
+//
+//  __layout: [AUTO-GENERATED]
+//            Created by Alloy at compile time. Creates the view
+//            hierarchy code based on your corresponding markup.
+//            Like __init(), don't define your own. 
+//
+//  postLayout: [optional]
+//              Your primary coding area. This code is executed 
+//              after all Alloy initialization and your markup's
+//              view hierarchy is created. All IDed elements from
+//              your markup are accessible from the $ variable.
