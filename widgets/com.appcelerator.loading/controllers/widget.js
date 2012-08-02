cf331f1fbf745f2f5c59bfda489dd000512c1184
tonylukasavage <anthony.lukasavage@gmail.com>
2012-08-01 23:20:34 -0400

    updated com.appcelerator.loading widget for new controller syntax

--- widgets/com.appcelerator.loading/controllers/widget.js
+++ widgets/com.appcelerator.loading/controllers/widget.js
@@ -1,13 +1,26 @@
-var args = arguments[0] || {};for (var k in args) {	$.loading[k] = args[k];	}
+var Alloy = require('alloy'),
+	Backbone = Alloy.Backbone,
+	_ = Alloy._,
+	$;
 
-if (Ti.Platform.osname === 'mobileweb') {
-    $.loading.duration = 100;
-} 
-$.loading.start();
+function init(args) {
+	$ = this;
+}
 
-////////////////////////////////////////////////////////
-////////// Exposed component object functions //////////
-////////////////////////////////////////////////////////
-$.setOpacity = function(opacity) {
-	$.loading.opacity = opacity;		
-};
+function controller(args) {
+	for (var k in args) {
+		$.loading[k] = args[k];	
+	}
+
+	if (Ti.Platform.osname === 'mobileweb') {
+	    $.loading.duration = 100;
+	} 
+	$.loading.start();
+
+	////////////////////////////////////////////////////////
+	////////// Exposed component object functions //////////
+	////////////////////////////////////////////////////////
+	$.setOpacity = function(opacity) {
+		$.loading.opacity = opacity;		
+	};
+}
