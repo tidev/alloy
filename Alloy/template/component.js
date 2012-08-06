d5a5d0ae8ecddcd9d647357d6651b2d941ab5bdd
tonylukasavage <anthony.lukasavage@gmail.com>
2012-08-06 10:17:17 -0400

    no lifecycle, flattened controllers, inheritance going to be done in markup

--- Alloy/template/component.js
+++ Alloy/template/component.js
@@ -2,20 +2,34 @@ var Alloy = require('alloy'),
 	Backbone = Alloy.Backbone,
 	_ = Alloy._,
 	A$ = Alloy.A,
-	arguments,
 	$;
 
-module.exports = Alloy.getController('BaseController').extend({
-	__init: function(args) {
-		$ = _.extend(this, $);
-		arguments = args;
+function Controller() {
+	Alloy.getController('BaseController').call(this);
+	$ = this;
+
+	<%= viewCode %>
+
+	<%= controllerCode %>
+}
+
+module.exports = Controller;
+
+/*
+< % = initFunction %>
+
+var __class = init();
+__class || (__class = Alloy.getController('BaseController'));
+
+module.exports = __class.extend({
+	__init: function() {
+		$ = this;
 	},
-	preLayout: <%= preLayoutCode %>
-	,
 	__layout: function() {
-		<%= viewCode %>
+		< % = viewCode %>
 	},
 	__postLayout: function() {
-		<%= controllerCode %>
+		< % = controllerCode %>
 	}
 });
+*/
