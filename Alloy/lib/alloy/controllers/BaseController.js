68cf46f64a2a210e342d6f3e1b8cea0af58fca11
tonylukasavage <anthony.lukasavage@gmail.com>
2012-08-06 10:16:08 -0400

    changed BaseController to support instance-based inheritance

--- Alloy/lib/alloy/controllers/BaseController.js
+++ Alloy/lib/alloy/controllers/BaseController.js
@@ -2,27 +2,40 @@ var Alloy = require('alloy'),
 	Backbone = Alloy.Backbone,
 	_ = Alloy._;
 
-var Controller = function(args) {
+var Controller = function() {
+	var fixArgs = Array.prototype.slice.call(arguments);
 	this.__iamalloy__ = true;
-	this.root = undefined;
+	//this.root = undefined;
+	_.extend(this, Backbone.Events, {
+		setParent: function(parent) {
+			if (this.root) {
+				parent.add(this.root);
+			} 
+		},
+		setRoot: function(root) {
+			this.root = root;
+		},
+		getRoot: function() {
+			return this.root;
+		}
+	});
 	if (this.__init) { this.__init(); }
-	if (this.preLayout) { this.preLayout(args); }
+	if (this.preLayout) { this.preLayout.apply(this, fixArgs); }
 	if (this.__layout) { this.__layout(); }
-	if (this.__postLayout) { this.__postLayout(args); }
+	if (this.__postLayout) { this.__postLayout.apply(this, fixArgs); }
 }
-Controller.extend = Backbone.Model.extend;
-_.extend(Controller.prototype, Backbone.Events, {
-	setParent: function(parent) {
-		if (this.root) {
-			parent.add(this.root);
-		} 
-	},
-	setRoot: function(root) {
-		this.root = root;
-	},
-	getRoot: function() {
-		return this.root;
-	}
-});
-
+//Controller.extend = Backbone.Model.extend;
+// _.extend(Controller.prototype, Backbone.Events, {
+// 	setParent: function(parent) {
+// 		if (this.root) {
+// 			parent.add(this.root);
+// 		} 
+// 	},
+// 	setRoot: function(root) {
+// 		this.root = root;
+// 	},
+// 	getRoot: function() {
+// 		return this.root;
+// 	}
+// });
 module.exports = Controller;
