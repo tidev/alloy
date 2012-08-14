1b6d135e698931e48096dac252540c5ecb901a90
tonylukasavage <anthony.lukasavage@gmail.com>
2012-08-14 00:21:23 -0400

    added exportsCode to component template, cleanup

--- Alloy/template/component.js
+++ Alloy/template/component.js
@@ -5,31 +5,14 @@ var Alloy = require('alloy'),
 	$;
 
 function Controller() {
-	Alloy.getController('<%= parentController %>').call(this);
+	require('alloy/controllers/<%= parentController %>').call(this);
 	$ = this;
 
 	<%= viewCode %>
 
 	<%= controllerCode %>
+
+	<%= exportsCode %>
 }
 
 module.exports = Controller;
-
-/*
-< % = initFunction %>
-
-var __class = init();
-__class || (__class = Alloy.getController('BaseController'));
-
-module.exports = __class.extend({
-	__init: function() {
-		$ = this;
-	},
-	__layout: function() {
-		< % = viewCode %>
-	},
-	__postLayout: function() {
-		< % = controllerCode %>
-	}
-});
-*/
