d362b734430b2eb98261a1a4b617717c700762de
tonylukasavage <anthony.lukasavage@gmail.com>
2012-08-14 13:29:46 -0400

    exportsCode goes before controllerCode, bring $ into functional scope

--- Alloy/template/component.js
+++ Alloy/template/component.js
@@ -1,18 +1,19 @@
 var Alloy = require('alloy'),
 	Backbone = Alloy.Backbone,
 	_ = Alloy._,
-	A$ = Alloy.A,
-	$;
+	A$ = Alloy.A;
 
 function Controller() {
 	require('alloy/controllers/<%= parentController %>').call(this);
-	$ = this;
+	
+	var exports, $;
+	exports = $ = this;
 
 	<%= viewCode %>
 
-	<%= controllerCode %>
-
 	<%= exportsCode %>
+
+	<%= controllerCode %>
 }
 
 module.exports = Controller;
