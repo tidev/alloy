18a4799080866660d1683543e09c6086b6a705a8
tonylukasavage <anthony.lukasavage@gmail.com>
2012-08-14 16:36:14 -0400

    updated loading widget

--- widgets/com.appcelerator.loading/controllers/widget.js
+++ widgets/com.appcelerator.loading/controllers/widget.js
@@ -1,17 +1,15 @@
-function postLayout(args) {
-	for (var k in args) {
-		$.loading[k] = args[k];	
-	}
+var args = arguments[0] || {};
+var loading = $.getView('loading');
 
-	if (Ti.Platform.osname === 'mobileweb') {
-	    $.loading.duration = 100;
-	} 
-	$.loading.start();
-
-	////////////////////////////////////////////////////////
-	////////// Exposed component object functions //////////
-	////////////////////////////////////////////////////////
-	$.setOpacity = function(opacity) {
-		$.loading.opacity = opacity;		
-	};
+for (var k in args) {
+	loading[k] = args[k];	
 }
+
+if (Ti.Platform.osname === 'mobileweb') {
+    loading.duration = 100;
+} 
+loading.start();
+
+exports.setOpacity = function(opacity) {
+	loading.opacity = opacity;		
+};
