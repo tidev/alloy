3f86add7f95f3169f7f75e8147076bc91545ecc6
Issam Hakimi <issam.hakimi@gmail.com>
2012-08-17 12:53:37 +0200

    features urlToJson and urlDecode added in string builtins

--- Alloy/builtins/string.js
+++ Alloy/builtins/string.js
@@ -29,3 +29,33 @@ exports.formatCurrency = !OS_MOBILEWEB ? String.formatCurrency : function (amoun
     var num = isNaN(amount) || amount === '' || amount === null ? 0.00 : amount;
     return '$' + parseFloat(num).toFixed(2);
 };
+
+exports.urlDecode = function (string){
+    if (!string) {
+        return '';
+    }
+    return string.replace(/%[a-fA-F0-9]{2}/ig, function (match) {
+        return String.fromCharCode(parseInt(match.replace('%', ''), 16));
+    });
+};
+
+exports.urlToJson = function (url){
+    var list = {};
+    var urlarr = url.split('&');
+    for (var n = 0; n < urlarr.length; ++n) {
+        var item = urlarr[n];
+        if (item == "") { continue; }
+        var e = item.indexOf('='),
+        	name,
+			value;
+        if (e < 0) {
+            name = item;
+            value = null;
+        } else {
+            name = item.substring(0, e);
+            value = item.substring(e + 1);
+        }
+        list[name] = value;
+    }						
+	return list;		
+};
