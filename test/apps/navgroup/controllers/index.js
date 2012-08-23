8026b0096c40606c9b4929b411a1eaefca3dfba3
tonylukasavage <anthony.lukasavage@gmail.com>
2012-08-23 17:46:13 -0400

    created "navgroup" test app

---
+++ test/apps/navgroup/controllers/index.js
@@ -0,0 +1,12 @@
+// attach the navgroup to Alloy.CFG so it can be accessed globally
+Alloy.CFG.navgroup = $.navgroup;
+
+// We can do most of this in markup once we resolve:
+// https://jira.appcelerator.org/browse/ALOY-202
+var button = Ti.UI.createButton({ title: 'next' });
+button.addEventListener('click', function(e) {
+	$.navgroup.open(Alloy.getController('win2').getView());
+});
+$.win1.rightNavButton = button;
+
+$.index.open();
