ead0df85394fd39fd80cb1acc9505ea365af626f
tonylukasavage <anthony.lukasavage@gmail.com>
2012-09-12 18:07:21 -0400

    get rid of pre and post functions after they are executed

--- Alloy/commands/compile/parsers/base.js
+++ Alloy/commands/compile/parsers/base.js
@@ -4,10 +4,16 @@ exports.parse = function(node, state, parser) {
 	var args = CU.getParserArgs(node, state),
 		code = ''; 
 
-	if (state.pre) { code += state.pre(node, state, args) || ''; }
+	if (state.pre) { 
+		code += state.pre(node, state, args) || ''; 
+		delete state.pre;
+	}
 	var newState = parser(node, state, args);
 	code += newState.code;
-	if (state.post) { code += state.post(node, newState, args) || ''; }
+	if (state.post) { 
+		code += state.post(node, newState, args) || ''; 
+		delete state.post;
+	}
 	newState.code = code;
 
 	return newState;
