e1411bfa30630179f9bbc52c3dab8ea86d7f3235
tonylukasavage <anthony.lukasavage@gmail.com>
2012-09-19 16:13:19 -0400

    workaround for ALOY-282

--- Alloy/commands/run/index.js
+++ Alloy/commands/run/index.js
@@ -1,4 +1,5 @@
 var path = require('path'),
+	wrench = require('wrench'),
 	_ = require("../../lib/alloy/underscore")._,
 	U = require('../../utils'),
 	logger = require('../../common/logger'),
@@ -10,7 +11,7 @@ module.exports = function(args, program) {
 	}
 
 	// Validate the input path
-	var inputPath = path.resolve(args.length > 0 ? args[0] : U.resolveAppHome());
+	var inputPath = path.resolve(args[0] || path.join(U.resolveAppHome(),'..'));
 	if (!path.existsSync(inputPath)) {
 		U.die('inputPath "' + inputPath + '" does not exist');
 	}
@@ -20,6 +21,16 @@ module.exports = function(args, program) {
 		if (!path.existsSync(path.join(inputPath,'app'))) {
 			U.die("This project doesn't seem to contain an Alloy app directory");
 		}
+	} 
+
+	// Check for platform
+	var platform = args[1] || 'iphone';
+	var checkPath = path.join(inputPath,'Resources',platform);
+
+	// assert that the platform directory exists
+	if (!path.existsSync(checkPath)) {
+		logger.warn('"Resources/' + platform + '" does not exist. Creating...');
+		wrench.mkdirSyncRecursive(checkPath,0777);
 	}
 	
 	//run the project
