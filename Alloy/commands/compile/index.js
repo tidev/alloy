db8dfb55e349068aee9d81308d93c2b0e7282771
tonylukasavage <anthony.lukasavage@gmail.com>
2012-09-19 13:04:20 -0400

    more logging improvement and cleanup

--- Alloy/commands/compile/index.js
+++ Alloy/commands/compile/index.js
@@ -25,7 +25,7 @@ var alloyRoot = path.join(__dirname,'..','..'),
 module.exports = function(args, program) {
 	var paths = U.getAndValidateProjectPaths(program.outputPath || args[0] || process.cwd());
 	var alloyConfig = {},
-		tmpPath, compilerMakeFile;
+		compilerMakeFile;
 
 	// remove the project's Resources directory, as it will all 
 	// be replaced by Alloy. Only do it if the "assets" path is 
@@ -58,7 +58,9 @@ module.exports = function(args, program) {
 	// log configuration data
 	logger.debug('----- CONFIGURATION -----');
 	_.each(alloyConfig, function(v,k) {
-		logger.debug(k + ' = ' + v);
+		if (k) {
+			logger.debug(k + ' = ' + v);
+		}
 	});
 	logger.debug('project path = ' + paths.project);
 	logger.debug('app path = ' + paths.app);
@@ -72,7 +74,7 @@ module.exports = function(args, program) {
 	// process project makefiles
 	var alloyJMK = path.resolve(path.join(paths.app,"alloy.jmk"));
 	if (path.existsSync(alloyJMK)) {
-		logger.debug("Found project specific makefile at " + "app/alloy.jmk".yellow);
+		logger.debug('Loading "alloy.jmk" compiler hooks...');
 		var script = vm.createScript(fs.readFileSync(alloyJMK), 'alloy.jmk');
 		
 		// process alloy.jmk compile file
@@ -83,11 +85,9 @@ module.exports = function(args, program) {
 			logger.error(e.stack);
 			U.die('Project build at "' + alloyJMK.yellow + '" generated an error during load.');
 		}
-	}
-	
-	// trigger our custom compiler makefile
-	if (compilerMakeFile.isActive) {
+
 		compilerMakeFile.trigger("pre:compile",_.clone(compileConfig));
+		logger.debug('');
 	}
 
 	// TODO: ti.physicalSizeCategory - https://jira.appcelerator.org/browse/ALOY-209
@@ -105,10 +105,12 @@ module.exports = function(args, program) {
 	U.copyAlloyDir(alloyRoot, 'lib', paths.resources); 
 	wrench.mkdirSyncRecursive(path.join(paths.resourcesAlloy, CONST.DIR.COMPONENT), 0777);
 	wrench.mkdirSyncRecursive(path.join(paths.resourcesAlloy, CONST.DIR.WIDGET), 0777);
+	logger.debug('');
 
 	// create the global style, if it exists
-	compileConfig.globalStyle = CU.loadStyle(path.join(paths.app,CONST.DIR.STYLE,CONST.GLOBAL_STYLE));
-
+	logger.debug('----- MVC GENERATION -----');
+	loadGlobalStyle(path.join(paths.app,CONST.DIR.STYLE,CONST.GLOBAL_STYLE));
+	
 	// Process all models
 	var models = processModels();
 
@@ -119,9 +121,6 @@ module.exports = function(args, program) {
 	var filterRegex = new RegExp('^(?:(?!' + filteredPlatforms.join('|') + '))');
 
 	// Process all views, including all those belonging to widgets
-	logger.debug('');
-	logger.debug('----- BEGIN CONTROLLER GENERATION -----');
-                    
 	var viewCollection = U.getWidgetDirectories(paths.project, paths.app);
 	viewCollection.push({ dir: path.join(paths.project,CONST.ALLOY_DIR) });
 	_.each(viewCollection, function(collection) {
@@ -137,7 +136,6 @@ module.exports = function(args, program) {
 				// generate runtime controller
 				logger.debug('[' + view + '] ' + (collection.manifest ? collection.manifest.id + ' ' : '') + 'view processing...');
 				parseAlloyComponent(view, collection.dir, collection.manifest);
-				logger.debug('');
 			}
 		});
 
@@ -153,16 +151,15 @@ module.exports = function(args, program) {
 
 				logger.debug('[' + controller + '] ' + (collection.manifest ? collection.manifest.id + ' ' : '') + 'controller processing...');
 				parseAlloyComponent(controller, collection.dir, collection.manifest, true);
-				logger.debug('');
 			}
 		});
 	});
-
-	logger.debug('----- BEGIN CONTROLLER GENERATION -----');
 	logger.debug('');
 
 	// copy assets and libraries
+	logger.debug('----- ASSETS & LIBS -----');
 	U.copyAlloyDir(paths.app, [CONST.DIR.ASSETS,CONST.DIR.LIB], compileConfig.dir.resources);
+	logger.debug('');
 
 	// generate app.js
 	var appJS = path.join(compileConfig.dir.resources,"app.js");
@@ -244,13 +241,13 @@ function parseAlloyComponent(view,dir,manifest,noView) {
 
 		// Load the style and update the state
 		if (path.existsSync(files.STYLE)) {
-			logger.debug('- STYLE: loading from "' + path.relative(path.join(dir,CONST.DIR.STYLE),files.STYLE) + '"');
+			logger.debug('  style:      "' + path.relative(path.join(dir,CONST.DIR.STYLE),files.STYLE) + '"');
 		}
 		state.styles = CU.loadAndSortStyle(files.STYLE);
 
 		// Load view from file into an XML document root node
 		try {
-			logger.debug('- VIEW: loading from "' + path.relative(path.join(dir,CONST.DIR.VIEW),files.VIEW)+ '"');
+			logger.debug('  view:       "' + path.relative(path.join(dir,CONST.DIR.VIEW),files.VIEW)+ '"');
 			var docRoot = U.XML.getAlloyFromFile(files.VIEW);
 		} catch (e) {
 			U.die([
@@ -301,7 +298,7 @@ function parseAlloyComponent(view,dir,manifest,noView) {
 
 	// process the controller code
 	if (path.existsSync(files.CONTROLLER)) {
-		logger.debug('- CONTROLLER: loading from "' + path.relative(path.join(dir,CONST.DIR.CONTROLLER),files.CONTROLLER) + '"');
+		logger.debug('  controller: "' + path.relative(path.join(dir,CONST.DIR.CONTROLLER),files.CONTROLLER) + '"');
 	}
 	var cCode = CU.loadController(files.CONTROLLER);
 	template.parentController = (cCode.parentControllerName != '') ? cCode.parentControllerName : "'BaseController'";
@@ -380,6 +377,8 @@ function processModels() {
 			logger.warn('Non-model file "' + modelFile + '" in models directory');
 			return;
 		}
+		logger.debug('[' + modelFile + '] model processing...');
+
 		var fullpath = path.join(compileConfig.dir.models,modelFile);
 		var basename = path.basename(fullpath, '.'+CONST.FILE_EXT.MODEL);
 		var modelJsFile = path.join(compileConfig.dir.models,basename+'.js');
@@ -449,11 +448,14 @@ function copyBuiltins() {
 	});
 	
 	if (alloyLibs.length > 0) {
+		logger.debug('');
+		logger.debug('----- BUILTINS -----');
 		// now find all our builtin libs and then copy them into 
 		// the project relative to the alloy directory so that 
 		// when they are required in the real project they will be available
 		var alloyDir = compileConfig.dir.resourcesAlloy; 
 		alloyLibs = _.uniq(alloyLibs);
+
 		_.each(alloyLibs,function(lib) {
 			// find all dependencies that look to be relative to our dependency
 			var depends = requires.findAllRequires(lib);
@@ -464,7 +466,7 @@ function copyBuiltins() {
 					if (path.existsSync(depend)) {
 						var rel = depend.substring(libdir.length);
 						var depDest = path.join(alloyDir,rel);
-						logger.debug('Copying builtin dependency '+depend.yellow+' to '.cyan+depDest.yellow);
+						logger.debug('[' + rel + '] "' + depend + '" -> "' + depDest + '"');
 						U.copyFileSync(depend,depDest);
 					}
 				}
@@ -472,9 +474,17 @@ function copyBuiltins() {
 			// now copy our builtin
 			var name = path.basename(lib);
 			var dest = path.join(alloyDir,name);
-			logger.debug('Copying builtin '+lib.yellow+' to '.cyan+dest.yellow);
+			logger.debug('[' + name + '] "' + lib + '" --> "' + dest + '"');
 			U.copyFileSync(lib,dest);
 		});
+		logger.debug('');
+	}
+}
+
+function loadGlobalStyle(filepath) {
+	if (path.existsSync(filepath)) {
+		logger.debug('[app.tss] global style processing...');
+		compileConfig.globalStyle = CU.loadStyle(filepath);
 	}
 }
 
@@ -482,15 +492,17 @@ function optimizeCompiledCode(config) {
 	var resourcesDir =  compileConfig.dir.resources, 
 		files = wrench.readdirSyncRecursive(resourcesDir);
 
+	logger.debug('----- OPTIMIZATIONS -----');
 	_.each(files,function(file){
 		var ext = file.substring(file.length-3);
 		if (ext == '.js') {
 			var f = path.join(resourcesDir,file);
-			logger.debug('Processing generated javascript file "' + file + '"');
+			logger.debug('Optimizing runtime javascript file "' + file + '"');
 
 			// we fix require paths to make sure they are correct and relative to the project
 			var newSrc = requires.makeRequiresRelative(f,resourcesDir,config);
 			fs.writeFileSync(f,newSrc,'utf-8');
 		}
 	});
+	logger.debug('');
 }
