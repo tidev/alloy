25f349ef3a6804c46fea3861f66b84d79ecf0668
tonylukasavage <anthony.lukasavage@gmail.com>
2012-09-26 12:10:53 -0400

    widget generation adds created widget to project dependencies (ALOY-226), using separate files for default widget

--- Alloy/commands/generate/targets/widget.js
+++ Alloy/commands/generate/targets/widget.js
@@ -1,10 +1,13 @@
 var path = require('path'),
 	fs = require('fs'),
 	wrench = require('wrench'),
+	jsonlint = require('jsonlint'),
 	U = require('../../../utils'),
 	_ = require("../../../lib/alloy/underscore")._,
 	CONST = require('../../../common/constants'),
 	logger = require('../../../common/logger');
+
+var VERSION_DEFAULT = '1.0';
 
 module.exports = function(name, args, program) {
 	var widgetId = args[0] || name;
@@ -19,7 +22,7 @@ module.exports = function(name, args, program) {
 	// create default alloy widget folders and files
 	_.each(types, function(type) {
 		var typeFolder = path.join(paths.widget, CONST.DIR[type]);
-		var typeTemplate = path.join(paths.template,type.toLowerCase() + '.' + CONST.FILE_EXT[type]);
+		var typeTemplate = path.join(paths.template,'widget',type.toLowerCase() + '.' + CONST.FILE_EXT[type]);
 
 		wrench.mkdirSyncRecursive(typeFolder, 0777);
 		fs.writeFileSync(
@@ -33,9 +36,22 @@ module.exports = function(name, args, program) {
 		path.join(paths.widget,'widget.json'), 
 		_.template(fs.readFileSync(paths.widgetTemplate,'utf8'), {
 			id: escapeDoubleQuotes(widgetId),
-			name: escapeDoubleQuotes(name)
+			name: escapeDoubleQuotes(name),
+			version: VERSION_DEFAULT
 		})
 	);
+
+	// Add this widget as a dependency to our project
+	var configReadPath = path.existsSync(paths.config) ? paths.config : paths.configTemplate;
+	var content = fs.readFileSync(configReadPath,'utf8'); 
+	try { 
+		var json = jsonlint.parse(content);
+		json.dependencies || (json.dependencies = {});
+		json.dependencies[widgetId] = VERSION_DEFAULT;
+		fs.writeFileSync(paths.config, U.prettyPrintJson(json));
+	} catch (e) {
+		U.die('Error while adding widget dependency to config.json in "' + configReadPath + '"', e);
+	}
 
 	logger.info('Generated widget named ' + name);
 }
@@ -49,9 +65,11 @@ function getPaths(appPath, widgetId) {
 		alloy: alloy,
 		template: template,
 		widgetTemplate: path.join(template,'widget.json'),
+		configTemplate: path.join(template,'config.json'),
 
 		// project paths
-		widget: path.join(appPath,'widgets',widgetId)
+		widget: path.join(appPath,'widgets',widgetId),
+		config: path.join(appPath,'config.json')
 	};
 }
 
