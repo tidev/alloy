ecb33a1fb5911e48da10019b967f9973c0f28a6d
tonylukasavage <anthony.lukasavage@gmail.com>
2012-09-29 18:01:28 -0400

    Ti.Android.MenuItems now properly handle properties and events

--- Alloy/commands/compile/parsers/Ti.Android.MenuItem.js
+++ Alloy/commands/compile/parsers/Ti.Android.MenuItem.js
@@ -1,21 +1,40 @@
 var CU = require('../compilerUtils'),
 	_ = require('../../../lib/alloy/underscore')._;
 
+// This is part of the Ti.Android.Menu.add() API docs
+var ADD_ARGS = [
+	 'itemId', 
+	 'groupId', 
+	 'title', 
+	 'order'
+];
+var addArgsString = '[' + _.map(ADD_ARGS, function(a) { return "'" + a + "'"}).join(',') + ']';
+
 exports.parse = function(node, state) {
 	return require('./base').parse(node, state, parse);
 };
 
 function parse(node, state, args) {
 	var code = '';
-
-	code += args.symbol + ' = A$(' + state.parent.symbol + '.add(';
-	code += CU.generateStyleParams(
+	var styleObjectCode = CU.generateStyleParams(
 		state.styles, 
 		args.classes, 
 		args.id, 
 		node.nodeName, 
 		_.defaults(state.extraStyle || {}, args.createArgs || {}) 
-	) + "), '" + node.nodeName + "', " + (args.parent.symbol || 'null') + ");";
+	);
+	var styleObjectSymbol = CU.generateUniqueId(); 
+	var initStyle = '_.pick(' + styleObjectSymbol + ',' + addArgsString + ')';
+
+	// TODO: http://jira.appcelerator.org/browse/ALOY-311
+	//       http://jira.appcelerator.org/browse/ALOY-312
+	//var postStyle = '_.omit(' + styleObjectSymbol + ',' + addArgsString + ')';
+
+	code += 'var ' + styleObjectSymbol + '=' + styleObjectCode + ';';
+	code += args.symbol + '=A$(' + state.parent.symbol + ".add(" + initStyle + "), '" + node.nodeName + "', " + (args.parent.symbol || 'null') + ");";
+	
+	// TODO: http://jira.appcelerator.org/browse/ALOY-313
+	code += '_.each(' + styleObjectSymbol + ',function(v,k) { ' + args.symbol + '[k] = v; });';
 
 	return {
 		parent: {},
