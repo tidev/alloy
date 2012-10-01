8468e434ab6aca2a0d5c24d713d760d341585902
tonylukasavage <anthony.lukasavage@gmail.com>
2012-10-01 10:47:06 -0400

    deleted parse file for now

--- Alloy/xmlparse.js
+++
@@ -1,6 +0,0 @@
-var XMLSerializer = require("xmldom").XMLSerializer,
-	DOMParser = require("xmldom").DOMParser;
-
-var filename = process.argv[2];
-var doc = new DOMParser().parseFromString(string);
-console.log(filename);
