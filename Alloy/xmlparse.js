ceb6224ce05f4d96a9d2bcc476e226d1a990a0d9
tonylukasavage <anthony.lukasavage@gmail.com>
2012-10-01 10:46:28 -0400

    updates

--- Alloy/xmlparse.js
+++ Alloy/xmlparse.js
@@ -2,4 +2,5 @@ var XMLSerializer = require("xmldom").XMLSerializer,
 	DOMParser = require("xmldom").DOMParser;
 
 var filename = process.argv[2];
+var doc = new DOMParser().parseFromString(string);
 console.log(filename);
