c6381e26cd77ed1146f0b0b6d90d32d3d63fbdcc
tonylukasavage <anthony.lukasavage@gmail.com>
2012-10-02 09:55:38 -0400

    created convenience function for easier test creation

--- test/specs/compile.js
+++ test/specs/compile.js
@@ -1,15 +1,22 @@
-var fs = require('fs');
-var path = require('path');
-var wrench = require('wrench');
-var exec = require('child_process').exec;
-var _ = require('../../Alloy/lib/alloy/underscore')._;
-var alloyRoot = path.join(__dirname,'..','..');
+var fs = require('fs'),
+	path = require('path'),
+	wrench = require('wrench'),
+	exec = require('child_process').exec,
+	_ = require('../../Alloy/lib/alloy/underscore')._;
+	
+// Paths and constants
+var alloyRoot = path.join(__dirname,'..','..'),
+	appsPath = path.join(alloyRoot,'test','apps'),
+	platforms = ['android','ios','mobileweb'],
+	TIMEOUT_DEFAULT = 750;
 
-var paths = {
-	apps: path.join(alloyRoot,'test','apps')
-};
-var platforms = ['android','ios','mobileweb'];
-
+// Turns the arguments given to the callback of the exec() function
+// into an object literal. 
+//
+// Params: 
+// * args: The "arguments" object from the callback of an exec() call
+//
+// Return: An object literal with the error, stdout, and stderr 
 function getExecObject(args) {
 	args = Array.prototype.slice.call(args, 0);
 	return {
@@ -19,28 +26,51 @@ function getExecObject(args) {
 	};
 }
 
+// Convenience function for handling asynchronous tests that rely on the
+// exec() function. The output values from the first runs() block will
+// be available as this.output in the second runs() block where the 
+// actual tests are evaluated.
+//
+// Params:
+// * cmd:     The command to run through exec()
+// * timeout: How long to wait for the command to execute before declaring 
+//            the test failed
+// * testFn:  The actual test function to execute on output returned from exec()
+//
+// Return: none
+function asyncExecTest(cmd, timeout, testFn) {
+	runs(function() {
+		var self = this;
+		self.done = false;
+		exec(cmd, function() {
+			self.done = true;
+			self.output = getExecObject(arguments);
+		});
+	});
+	waitsFor(
+		function() { return this.done; }, 
+		'exec("' + cmd + '") timed out', timeout || TIMEOUT_DEFAULT
+	);
+	runs(testFn);
+}
+
+// The alloy command test suite
 describe('alloy command', function() {
 	it('is available from the command line ala `alloy --version`', function() {
-		var o = {};
-		var done = false;
+		asyncExecTest('alloy --version', 500, function() {
+			var o = this.output;
 
-		runs(function() {
-			exec('alloy --version', function() {
-				done = true;
-				o = getExecObject(arguments);
-			});
-		});
-
-		waitsFor(function() { return done; }, "exec() timed out", 500);
-
-		runs(function() {
+			// Make sure we have no errors
 			expect(o.error).toBeFalsy();
+
+			// Make sure we get a valid version number
 			expect(/\d+\.\d+\.\d+(?:\-\d+){0,1}/.test(o.stdout)).toBe(true);
 		});
 	});
 
-	_.each(wrench.readdirSyncRecursive(paths.apps), function(file) {
-		var indexXml = path.join(paths.apps,file,'views','index.xml');
+	// Iterate through each test app and make sure it compiles for all platforms
+	_.each(wrench.readdirSyncRecursive(appsPath), function(file) {
+		var indexXml = path.join(appsPath,file,'views','index.xml');
 		if (path.existsSync(indexXml)) {
 			_.each(platforms, function(platform) {
 				it('can compile "' + file + '" test app for platform "' + platform + '"', function() {
