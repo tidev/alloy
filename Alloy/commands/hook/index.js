ad71ca9bb5a4bacfced159ba3e00e19a88972ade
tonylukasavage <anthony.lukasavage@gmail.com>
2012-10-04 15:16:08 -0400

    re-enabled "hook" command since node-app is in npm now

--- Alloy/commands/hook/index.js
+++ Alloy/commands/hook/index.js
@@ -1,29 +1,29 @@
-var logger = require('../../common/logger');
-
-module.exports = function(args, program) {
-	logger.warn('hook command not yet implemented');
-}
-
-// var path = require('path'),
-// 	wrench = require('wrench'),
-// 	appc = require('node-appc'),
-// 	logger = require('../../common/logger'),
-// 	alloyRoot = path.join(__dirname,'..','..','..'); 
-
-// var HOOK_NAME = 'alloy.js';
+// var logger = require('../../common/logger');
 
 // module.exports = function(args, program) {
-// 	// establish the Titanium path for this system
-// 	var installPath = appc.fs.resolvePath(appc.environ.os.sdkPaths[0], 'modules', 'alloy');
-// 	logger.info('Installing Alloy hook "' + HOOK_NAME + '" into Titanium...');
-
-// 	// blast the existing hooks path for Alloy
-// 	wrench.rmdirSyncRecursive(installPath, true);
-
-// 	// Copy the Alloy hook into Titanium
-// 	var hookPath = appc.fs.resolvePath(alloyRoot, 'hooks', HOOK_NAME);
-// 	var destPath = path.join(installPath, appc.pkginfo.package(module).version, 'cli', 'hooks', HOOK_NAME);
-// 	appc.fs.copyFileSync(hookPath, destPath);
-
-// 	logger.info('Installed "hooks/' + HOOK_NAME + '" to "' + destPath + '"');
+// 	logger.warn('hook command not yet implemented');
 // }
+
+var path = require('path'),
+	wrench = require('wrench'),
+	appc = require('node-appc'),
+	logger = require('../../common/logger'),
+	alloyRoot = path.join(__dirname,'..','..','..'); 
+
+var HOOK_NAME = 'alloy.js';
+
+module.exports = function(args, program) {
+	// establish the Titanium path for this system
+	var installPath = appc.fs.resolvePath(appc.environ.os.sdkPaths[0], 'modules', 'alloy');
+	logger.info('Installing Alloy hook "' + HOOK_NAME + '" into Titanium...');
+
+	// blast the existing hooks path for Alloy
+	wrench.rmdirSyncRecursive(installPath, true);
+
+	// Copy the Alloy hook into Titanium
+	var hookPath = appc.fs.resolvePath(alloyRoot, 'hooks', HOOK_NAME);
+	var destPath = path.join(installPath, appc.pkginfo.package(module).version, 'cli', 'hooks', HOOK_NAME);
+	appc.fs.copyFileSync(hookPath, destPath);
+
+	logger.info('Installed "hooks/' + HOOK_NAME + '" to "' + destPath + '"');
+}
