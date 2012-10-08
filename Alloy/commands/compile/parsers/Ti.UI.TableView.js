1876e3c3fc0071d4558242602a22121b09a29ace
tonylukasavage <anthony.lukasavage@gmail.com>
2012-10-08 14:44:58 -0400

    Added Ti.UI.SearchBar support to Ti.UI.TableView markup parser

--- Alloy/commands/compile/parsers/Ti.UI.TableView.js
+++ Alloy/commands/compile/parsers/Ti.UI.TableView.js
@@ -9,7 +9,10 @@ exports.parse = function(node, state) {
 function parse(node, state, args) {
 	var children = U.XML.getElementsFromNodes(node.childNodes),
 		arrayName = CU.generateUniqueId(),
-		code = 'var ' + arrayName + ' = [];\n';
+		code = 'var ' + arrayName + ' = [];\n',
+		isSearchBar = false,
+		hasRows = false,
+		searchBarName;
 
 	// iterate through all children
 	for (var i = 0, l = children.length; i < l; i++) {
@@ -20,28 +23,45 @@ function parse(node, state, args) {
 		if (childArgs.fullname === 'Alloy.Require') {
 			var inspect = CU.inspectRequireNode(child);
 			_.each(inspect.names, function(name) {
-				if (name !== 'Ti.UI.TableViewRow' &&
-					name !== 'Ti.UI.TableViewSection') {
-					U.die('TableView <Require> child at position ' + i + ' has elements that are not row(s) or section(s)');
+				if (name === 'Ti.UI.TableViewRow' ||
+					name === 'Ti.UI.TableViewSection') {
+					isSearchBar = false;
+				} else if (name === 'Ti.UI.SearchBar') {
+					isSearchBar = true;
+				} else {
+					U.die('TableView <Require> child at position ' + i + ' has elements that are not rows, sections, or SearchBar');
 				}
 			});
-		} else if (childArgs.fullname !== 'Ti.UI.TableViewRow' &&
-				   childArgs.fullname !== 'Ti.UI.TableViewSection') {
-			U.die('TableView child at position ' + i + ' is not a row or section, or a <Require> containing rows(s) or section(s).');
+		} else if (childArgs.fullname === 'Ti.UI.TableViewRow' ||
+				   childArgs.fullname === 'Ti.UI.TableViewSection') {
+			isSearchBar = false;
+		} else if (childArgs.fullname === 'Ti.UI.SearchBar') {
+			isSearchBar = true;
+		} else {
+			U.die('TableView child at position ' + i + ' is not a row or section, or a <Require> containing rows, sections, or SearchBar');
 		}
 
-		// generate code for the row/section
+		// generate code for the row/section/searchbar
 		code += CU.generateNode(child, {
 			parent: {},
 			styles: state.styles,
 			post: function(node, state, args) {
-				return arrayName + '.push(' + state.parent.symbol + ');\n';
+				if (isSearchBar) {
+					searchBarName = state.parent.symbol;
+				} else {
+					hasRows = true;
+					return arrayName + '.push(' + state.parent.symbol + ');\n';
+				}
 			}
 		});
 	}
 
 	// Create the initial TableView code
-	state.extraStyle = CU.createVariableStyle('data', arrayName);
+	var extras = [];
+	if (hasRows) { extras.push(['data', arrayName]); }
+	if (searchBarName) { extras.push(['search', searchBarName]) }
+	if (extras.length) { state.extraStyle = CU.createVariableStyle(extras); }
+
 	var tableState = require('./default').parse(node, state);
 	code += tableState.code;
 
