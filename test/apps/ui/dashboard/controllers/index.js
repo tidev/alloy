f50146617b020141e18150132f4ba75cebe09913
tonylukasavage <anthony.lukasavage@gmail.com>
2012-10-08 20:48:36 -0400

    updated "ui/dashboard" to model the API sample

--- test/apps/ui/dashboard/controllers/index.js
+++ test/apps/ui/dashboard/controllers/index.js
@@ -1,1 +1,23 @@
+var isEditable = false;
+
+function resetBadge(e) {
+	e.item.badge = 0;
+}
+
+function toggleEditMode(e) {
+	isEditable ? $.dash.stopEditing() : $.dash.startEditing();
+}
+
+function handleEdit(e) {
+	$.editButton.title = 'Done';
+  	$.editButton.style = Ti.UI.iPhone.SystemButtonStyle.DONE;
+  	isEditable = true;
+}
+
+function handleCommit(e) {
+	$.editButton.title = 'Edit';
+  	$.editButton.style = Ti.UI.iPhone.SystemButtonStyle.PLAIN;
+  	isEditable = false;
+}
+
 $.index.open();
