7a3fba9228e02e9029bde92c74d7fc7eacd06f8b
Carl Orthlieb <github@orthlieb.com>
2012-10-09 17:40:30 -0700

    Fix missing label on button
    Fix missing label on button

--- test/apps/widgets/widget_buttongrid/controllers/index.js
+++ test/apps/widgets/widget_buttongrid/controllers/index.js
@@ -1,4 +1,4 @@
-var animation = require("alloy/animation");
+var animation = require('alloy/animation');
 
 function IndexOpen(e) {
  
@@ -6,21 +6,21 @@ function IndexOpen(e) {
     $.buttongrid.relayout(e);    
 }
 
-var red = "#900A05";
-var brightred = "#B00C07";
-var black = "#000000";
+var red = '#900A05';
+var brightred = '#B00C07';
+var black = '#000000';
 var gray = '#888888';
 
 $.buttongrid.init({
     buttons: [
-        { id: 'Cloudy', title: "Cloudy", backgroundColor: black, backgroundSelectedColor: gray },
-        { id: 'Drizzle', title: "Drizzle" },
+        { id: 'Cloudy', title: 'Cloudy', backgroundColor: black, backgroundSelectedColor: gray },
+        { id: 'Drizzle', title: 'Drizzle' },
         { id: 'Haze', title: 'Haze' },
-        { id: 'MostlyCloudy', title: "Mostly Cloudy" },
-        { id: 'SlightDrizzle' },
+        { id: 'MostlyCloudy', title: 'Mostly Cloudy' },
+        { id: 'SlightDrizzle', title: 'Slight Drizzle' },
         { id: 'Snow', title: 'Snow' },
         { id: 'Sunny', title: 'Sunny' },
-        { id: 'Thunderstorms', title: 'Thunderstorms', click: function (e) { alert("Thunder!"); } }
+        { id: 'Thunderstorms', title: 'Thunderstorms', click: function (e) { alert('Thunder!'); } }
     ],
     buttonWidth: Alloy.isTablet ? 200: 100,
     buttonHeight: Alloy.isTablet ? 200 : 100,
