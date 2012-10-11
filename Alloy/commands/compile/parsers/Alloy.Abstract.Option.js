6931425ccec1626b9447466591956ae5c3d4ae77
tonylukasavage <anthony.lukasavage@gmail.com>
2012-10-11 16:22:47 -0400

    parsers for abstract Option(s)

---
+++ Alloy/commands/compile/parsers/Alloy.Abstract.Option.js
@@ -0,0 +1,20 @@
+var _ = require('../../../lib/alloy/underscore')._,
+	U = require('../../../utils'),
+	CU = require('../compilerUtils'); 
+
+exports.parse = function(node, state) {
+	return require('./base').parse(node, state, parse);
+};
+
+function parse(node, state, args) {
+	if (!state.itemsArray) {
+		U.die('Invalid use of <Option>. Must be the child of <Options>.');
+	}
+	var string = U.trim(U.XML.getNodeText(node) || '').replace(/"/g,'\\"');
+
+	return {
+		parent: {},
+		styles: state.styles,
+		code: state.itemsArray + '.push("' + string + '");'
+	};
+}
