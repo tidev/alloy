00d4779e0439be3c73d9f2863b55afe710aac483
tonylukasavage <anthony.lukasavage@gmail.com>
2012-10-11 15:49:17 -0400

    farewell repetitive code, hello Alloy.Abstract._ItemContainer.js

--- Alloy/commands/compile/parsers/Ti.UI.AlertDialog.js
+++ Alloy/commands/compile/parsers/Ti.UI.AlertDialog.js
@@ -1,70 +1,14 @@
-var _ = require('../../../lib/alloy/underscore')._,
-	U = require('../../../utils'),
-	CU = require('../compilerUtils'),
-	logger = require('../../../common/logger');
-
-var VALID = [
-	'Alloy.Abstract.ButtonNames'
-];
+var _ = require('../../../lib/alloy/underscore')._;
 
 exports.parse = function(node, state) {
-	return require('./base').parse(node, state, parse);
-};
-
-function parse(node, state, args) {
-	var config = CU.getCompilerConfig(),
-		isAndroid = config && config.alloyConfig && config.alloyConfig.platform === 'android',
-		androidView = null,
-		extras = [],
-		code = '';
-
-	_.each(U.XML.getElementsFromNodes(node.childNodes), function(child) {
-		switch(CU.validateNodeName(child, VALID)) {
-			case 'Alloy.Abstract.ButtonNames': 
-				var bnState = {
-					parent: {},
-					styles: state.styles,
-					buttonNameArray: CU.generateUniqueId()
-				};
-
-				code += CU.generateNode(child, bnState);
-				extras.push(['buttonNames', bnState.buttonNameArray]);
-				break;
-			default:
-				// process potential androidView
-				if (androidView === null) {
-					if (isAndroid) {
-						androidView = CU.generateNode(child, {
-							parent: {},
-							styles: state.styles,
-							post: function(node, state, args) {
-								extras.push(['androidView', state.parent.symbol]);
-							}
-						});
-						code += androidView;
-					} else {
-						logger.warn('Additional views in AlertDialog only supported on Android');
-					}
-				} else {
-					U.die('AlertDialog can only have one androidView');
-				}	
-				break;
+	state = _.extend(state, {
+		itemContainerDefinition: {
+			children: [
+				{ name:'Alloy.Abstract.ButtonNames', property:'buttonNames' }
+			],
+			androidView: true,
+			inViewHierarchy: false
 		}
 	});
-
-	// Add options and button names to the style, if present
-	if (extras.length) {	
-		state.extraStyle = CU.createVariableStyle(extras);
-	}
-	state.parent = {};
-
-	var alertState = require('./default').parse(node, state);
-	code += alertState.code;
-
-	// Update the parsing state
-	return {
-		parent: {},
-		styles: state.styles,
-		code: code
-	};
+	return require('./Alloy.Abstract._ItemContainer').parse(node, state);
 };
--- Alloy/commands/compile/parsers/Ti.UI.ButtonBar.js
+++ Alloy/commands/compile/parsers/Ti.UI.ButtonBar.js
@@ -1,52 +1,15 @@
-var _ = require('../../../lib/alloy/underscore')._,
-	U = require('../../../utils'),
-	CU = require('../compilerUtils');
-
-var VALID = [
-	'Alloy.Abstract.BarItemTypes',
-	'Alloy.Abstract.Labels'
-];
+var _ = require('../../../lib/alloy/underscore')._;
 
 exports.parse = function(node, state) {
-	return require('./base').parse(node, state, parse);
-};
-
-function parse(node, state, args) {
-	var extras = [],
-		code = '';
-
-	_.each(U.XML.getElementsFromNodes(node.childNodes), function(child, index) {
-		switch(CU.validateNodeName(child, VALID)) {
-			case 'Alloy.Abstract.Labels':
-				child.nodeName = 'BarItemTypes';
-				child.setAttribute('ns', 'Alloy.Abstract');
-			case 'Alloy.Abstract.BarItemTypes': 
-				var bitState = {
-					parent: {},
-					styles: state.styles,
-					barItemTypesArray: CU.generateUniqueId()
-				};
-
-				code += CU.generateNode(child, bitState);
-				extras.push(['labels', bitState.barItemTypesArray]);
-
-				// get rid of the node when we're done so we can pass the current state
-				// back to generateNode() and then process any additional views that 
-				// need to be added to the view hierarchy
-				node.removeChild(child);
-				break;
-			default:
-				// do nothing, additional views will be processed on the next pass
-				break;
+	state = _.extend(state, {
+		itemContainerDefinition: {
+			children: [
+				{ name:'Alloy.Abstract.BarItemTypes', property:'labels' }
+			],
+			translations: [
+				{ from: 'Alloy.Abstract.Labels', to: 'Alloy.Abstract.BarItemTypes' }
+			]
 		}
 	});
-
-	if (extras.length) {	
-		state.extraStyle = CU.createVariableStyle(extras);
-	}
-	var buttonBarState = require('./default').parse(node, state);
-	code += buttonBarState.code;
-
-	// Update the parsing state
-	return _.extend(buttonBarState, {code:code}); 
+	return require('./Alloy.Abstract._ItemContainer').parse(node, state);
 };
