e92570f5600276a3afe0412c5a70aefc03ded31a
tonylukasavage <anthony.lukasavage@gmail.com>
2012-10-11 15:46:31 -0400

    added androidView and inViewHierarchy handling

--- Alloy/commands/compile/parsers/Alloy.Abstract._ItemContainer.js
+++ Alloy/commands/compile/parsers/Alloy.Abstract._ItemContainer.js
@@ -1,12 +1,15 @@
 var _ = require('../../../lib/alloy/underscore')._,
 	U = require('../../../utils'),
-	CU = require('../compilerUtils');
+	CU = require('../compilerUtils'),
+	logger = require('../../../common/logger');
 
 function fixDefinition(def) {
 	def || (def = {});
 	def.children || (def.children = []);
 	def.translations || (def.translations = []);
 	def.doRemoveNode = def.doRemoveNode || typeof(def.doRemoveNode) === 'undefined';
+	def.processOthers = def.processOthers || function(){};
+	def.inViewHierarchy = def.inViewHierarchy || typeof(def.inViewHierarchy) === 'undefined';
 	return def;
 }
 
@@ -16,6 +19,9 @@ exports.parse = function(node, state) {
 
 function parse(node, state, args) {
 	var def = fixDefinition(state.itemContainerDefinition),
+		config = CU.getCompilerConfig(),
+		isAndroid = config && config.alloyConfig && config.alloyConfig.platform === 'android',
+		androidView = null;
 		extras = [],
 		code = '';
 
@@ -50,15 +56,39 @@ function parse(node, state, args) {
 			if (def.doRemoveNode) {
 				node.removeChild(child);
 			}
+
+		// process potential androidView if defined
+		} else if (def.androidView) {
+			if (androidView === null) {
+				if (isAndroid) {
+					androidView = CU.generateNode(child, {
+						parent: {},
+						styles: state.styles,
+						post: function(node, state, args) {
+							extras.push(['androidView', state.parent.symbol]);
+						}
+					});
+					code += androidView;
+				} else {
+					logger.warn('Additional views in ' + theNode + ' only supported on Android');
+				}
+			} else {
+				U.die(theNode + ' can only have one androidView');
+			}
 		}
 	});
 
 	if (extras.length) {	
 		state.extraStyle = CU.createVariableStyle(extras);
 	}
+	if (!def.inViewHierarchy) {
+		state.parent = {};
+	}
+
 	var outState = require('./default').parse(node, state);
 	code += outState.code;
 
 	// Update the parsing state
-	return _.extend(outState, {code:code}); 
+	return _.extend(outState, {code:code});
+	 
 };
