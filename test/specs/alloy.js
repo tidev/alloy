423c636c92756b01262c4f1e080df02877020768
tonylukasavage <anthony.lukasavage@gmail.com>
2012-10-18 14:55:49 -0400

    replaced cli.js test with alloy.js for testing default alloy execution with no params

---
+++ test/specs/alloy.js
@@ -0,0 +1,29 @@
+var fs = require('fs'),
+	path = require('path'),
+	TU = require('../lib/testUtils'),
+	CONST = require('../../Alloy/common/constants'),
+	_ = require('../../Alloy/lib/alloy/underscore')._,
+	strip = require('stripcolorcodes');
+
+var TIMEOUT_DEFAULT = 1000;
+
+// The alloy command test suite
+describe('`alloy`', function() {
+	it('can be executed', function() {
+		TU.asyncExecTest('alloy', TIMEOUT_DEFAULT, function() {
+			expect(this.output.error).toBeFalsy();
+		});
+	});
+
+	it('displays help when it receives no arguments', function() {
+		TU.asyncExecTest('alloy', TIMEOUT_DEFAULT, function() {
+			expect(strip(this.output.stdout)).toMatch(/Usage\:\s+alloy\s+COMMAND\s+\[ARGS\]\s+\[OPTIONS\]/);
+		});
+	});
+
+	it('fails when given an invalid command name', function() {
+		TU.asyncExecTest('alloy invalidCommand', TIMEOUT_DEFAULT, function() {
+			expect(this.output.error).toBeTruthy();
+		});
+	});
+});
