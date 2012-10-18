6a8e81e7ee664bd0404319ec8e0f9c210cedb490
tonylukasavage <anthony.lukasavage@gmail.com>
2012-10-18 14:10:33 -0400

    unit testing for "alloy install" command

---
+++ test/specs/install.js
@@ -0,0 +1,57 @@
+var fs = require('fs'),
+	path = require('path'),
+	TU = require('../lib/testUtils'),
+	CONST = require('../../Alloy/common/constants');
+
+var TIMEOUT_DEFAULT = 1000;
+
+var alloyRoot = path.join(__dirname,'..','..');
+var	harnessRoot = path.join(alloyRoot,'test','projects','Harness');
+var TiAppRoot = path.join(alloyRoot,'test','projects','TiApp');
+var harness = {
+	root: harnessRoot,
+	hook: path.join(harnessRoot,'plugins',CONST.PLUGIN_NAME,'hooks','alloy.js'),
+	plugin: path.join(harnessRoot,'plugins',CONST.PLUGIN_NAME,'plugin.py')
+};
+var alloy = {
+	root: alloyRoot,
+	hook: path.join(alloyRoot,'hooks','alloy.js'),
+	plugin: path.join(alloyRoot,'Alloy','plugin','plugin.py')
+}
+
+// The alloy command test suite
+describe('`alloy install', function() {
+	it('plugin` is supported', function() {
+		TU.asyncExecTest('alloy install plugin "' + harness.root + '"', TIMEOUT_DEFAULT, function() {
+			expect(this.output.error).toBeFalsy();
+		});
+	});
+
+	it('plugin` generates plugins and hooks', function() {
+		expect(path.existsSync(harness.hook)).toBeTruthy();
+		expect(path.existsSync(harness.plugin)).toBeTruthy();
+	});
+
+	it('plugin` generated files match those in Alloy', function() {
+		expect(fs.readFileSync(alloy.hook,'utf8')).toBe(fs.readFileSync(harness.hook,'utf8'));
+		expect(fs.readFileSync(alloy.plugin,'utf8')).toBe(fs.readFileSync(harness.plugin,'utf8'));
+	});
+
+	it('plugin` fails when given a non-existent project path', function() {
+		TU.asyncExecTest('alloy install plugin /some/path/that/does/not/exist', TIMEOUT_DEFAULT, function() {
+			expect(this.output.error).toBeTruthy();
+		});
+	});
+
+	it('plugin` fails when given a non-alloy project path', function() {
+		TU.asyncExecTest('alloy install plugin "' + TiAppRoot + '"', TIMEOUT_DEFAULT, function() {
+			expect(this.output.error).toBeTruthy();
+		});
+	});
+
+	it('` fails when an invalid type is used', function() {
+		TU.asyncExecTest('alloy install invalidType "' + harness.root + '"', TIMEOUT_DEFAULT, function() {
+			expect(this.output.error).toBeTruthy();
+		});
+	});
+});
