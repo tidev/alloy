ac401d57843cddf3f3d16603cce06fad76a0c8bf
tonylukasavage <anthony.lukasavage@gmail.com>
2012-10-19 15:46:48 -0400

    more accurate description of tests for output

--- test/specs/new.js
+++ test/specs/new.js
@@ -67,8 +67,8 @@ var TO_BE_CREATED = [
 
 _.each(RUNS, function(run) {
 	// The alloy new command test suite
-	describe('`' + run.cmd + '`', function() {
-		it('executes without error', function() {
+	describe('alloy new command', function() {
+		it('executes `' + run.cmd + '` with ' + (run.success ? 'success' : 'error'), function() {
 			// Create a copy of TiApp to work with
 			wrench.rmdirSyncRecursive(TiAppCopy, true);
 			wrench.mkdirSyncRecursive(TiAppCopy, 0777);
