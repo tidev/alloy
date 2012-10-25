efc2e4b9733ac617e223001e61379361ef803a90
tonylukasavage <anthony.lukasavage@gmail.com>
2012-10-25 16:33:01 -0700

    added version check test to "alloy", removed it from "compile"

--- test/specs/alloy.js
+++ test/specs/alloy.js
@@ -8,7 +8,7 @@ var fs = require('fs'),
 var TIMEOUT_DEFAULT = 1000;
 
 // The alloy command test suite
-describe('`alloy`', function() {
+describe('alloy', function() {
 	it('can be executed', function() {
 		TU.asyncExecTest('alloy');
 	});
@@ -25,6 +25,20 @@ describe('`alloy`', function() {
 		TU.asyncExecTest('alloy invalidCommand', {
 			test: function() {
 				expect(this.output.error).toBeTruthy();
+			}
+		});
+	});
+
+	it('--version returns the current version', function() {
+		TU.asyncExecTest('alloy --version', {
+			test: function() {
+				var o = this.output;
+
+				// Make sure we have no errors
+				expect(o.error).toBeFalsy();
+
+				// Make sure we get a valid version number
+				expect(/\d+\.\d+\.\d+(?:\-\d+){0,1}/.test(o.stdout)).toBe(true);
 			}
 		});
 	});
--- test/specs/compile.js
+++ test/specs/compile.js
@@ -15,27 +15,13 @@ var alloyRoot = path.join(__dirname,'..','..'),
 	};
 
 // The alloy command test suite
-describe('alloy command', function() {
-	it('is available from the command line ala `alloy --version`', function() {
-		TU.asyncExecTest('alloy --version', {
-			test: function() {
-				var o = this.output;
-
-				// Make sure we have no errors
-				expect(o.error).toBeFalsy();
-
-				// Make sure we get a valid version number
-				expect(/\d+\.\d+\.\d+(?:\-\d+){0,1}/.test(o.stdout)).toBe(true);
-			}
-		});
-	});
-
+describe('alloy compile', function() {
 	// Iterate through each test app and make sure it compiles for all platforms
 	_.each(wrench.readdirSyncRecursive(paths.apps), function(file) {
 		var indexXml = path.join(paths.apps,file,'views','index.xml');
 		if (path.existsSync(indexXml)) {
 			_.each(PLATFORMS, function(platform) {
-				it('can compile "' + file + '" test app for platform "' + platform + '"', function() {
+				it('compiles "' + file + '" test app for platform [' + platform + ']', function() {
 					var cmds = [
 						'jake app:setup dir=' + file + ' quiet=1',
 						'alloy compile ' + paths.harness + ' --config platform=' + platform
