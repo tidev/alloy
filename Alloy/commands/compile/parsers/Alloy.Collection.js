026341478d2ec781dffac766b56b7e3920d6dd5d
tonylukasavage <anthony.lukasavage@gmail.com>
2012-11-07 15:44:51 -0500

    XML tag for <Collection>

---
+++ Alloy/commands/compile/parsers/Alloy.Collection.js
@@ -0,0 +1,47 @@
+var CU = require('../compilerUtils'),
+	U = require('../../../utils'),
+	_ = require('../../../lib/alloy/underscore')._,
+	logger = require('../../../common/logger');
+
+exports.parse = function(node, state) {
+	return require('./base').parse(node, state, parse);
+};
+
+function parse(node, state, args) {
+	var code = '';
+	var isSingleton = node.getAttribute('instance') !== 'true';
+	var id = node.getAttribute('id');
+	var src = node.getAttribute('src');
+	
+	if (!src) { 
+		U.die([
+			'Error processing <Collection>:',
+			'All collections must have a "src" attribute'
+		]); 
+	} else {
+		// TODO: validate the src exists
+	}
+
+	if (isSingleton) {
+		if (id) {
+			logger.warn([
+				'id attribute ignored in singleton <Collection>',
+				'id is always equal to src attribute with singleton',
+				'Use the attribute `instance="true"` to create a new instance of the collection.'
+			]);
+		} 
+		id = src;
+	} else {
+		id || (id = args.id);
+	}
+
+	var col = 'Alloy.Collections[\'' + id + '\']';
+	code += col + ' || (' + col + ' = Alloy.createCollection(\'' + id + '\'));'
+	
+	return {
+		code: code,
+		args: {
+			symbol: col
+		}
+	};
+};
