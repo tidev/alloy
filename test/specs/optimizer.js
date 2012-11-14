cd757e1f4d0fe6a51a84a7e49cdc685ff6a46c53
tonylukasavage <anthony.lukasavage@gmail.com>
2012-11-14 17:57:45 -0500

    adding test from optimizer.js into new unit testing space

--- test/specs/optimizer.js
+++ test/specs/optimizer.js
@@ -1,5 +1,6 @@
 var fs = require('fs'),
 	path = require('path'),
+	colors = require('colors'),
 	TU = require('../lib/testUtils'),
 	CONST = require('../../Alloy/common/constants'),
 	_ = require('../../Alloy/lib/alloy/underscore')._,
@@ -8,47 +9,43 @@ var fs = require('fs'),
 	optimizer = require('../../Alloy/commands/compile/optimizer');
 
 var tests = [
+	// make sure we didn't break normal conditionals and assigments
 	['var a = Ti.Platform.name', 'var a="<%= Ti_Platform_name %>"'],
 	['var a = Titanium.Platform.name', 'var a="<%= Titanium_Platform_name %>"'],
 	['var a = Ti.Platform.name=="<%= Titanium_Platform_name %>" ? 1 : 0', 'var a=1'],
 	['var a = Ti.Platform.name=="<%= Titanium_Platform_name %>", b', 'var a=1,b'],
 	['var a = Ti.Platform.name=="<%= Titanium_Platform_name %>", b, c = 2', 'var a=1,b,c=2'],
 	['var a = Ti.Platform.name=="<%= Titanium_Platform_name %>"', 'var a=1'],
-	//['var a = Ti.Platform.name!="iPhone OS"', 'var a=1', androidDefines],
-	//['var a = Ti.Platform.name=="iPhone OS"', 'var a=0', androidDefines],
+	['var a = Ti.Platform.name!="iPhone OS"', 'var a=0', ['ios']],
+	['var a = Ti.Platform.name=="iPhone OS"', 'var a=0', notPlatform('ios')],
+	['var a, b = Ti.Platform.name=="<%= Titanium_Platform_name %>", c = 2;', 'var a,b=1,c=2'],
+	['var a = "1"', 'var a="1"'],
+	['var a = true', 'var a=true'],
+	['var a = 1', 'var a=1'],
+	['var a', 'var a'],
+	['var a = {}', 'var a={}'],
+	['var a = new Object', 'var a=new Object'],
+	['var a = new Object()', 'var a=new Object'],
+	['var a = Ti.Platform.name', 'var a="<%= Ti_Platform_name %>"'],
+	['var a = Ti.Platform.osname', 'var a="android"', ['android']],
+	['var a = Ti.Platform.osname', 'var a="mobileweb"', ['mobileweb']],
+	['var a, b = 1, c = 2;', 'var a,b=1,c=2'],
+	['var a = 1;', 'var a=1'],
+	['var a =+1;', 'var a=+1'],
+	['var a =1+1;', 'var a=1+1'],
+	['var a = 1.0;', 'var a=1'],
+	['var a = 1.02;', 'var a=1.02'],
+	['var a = -1.02;', 'var a=-1.02'],
+	['var a = !1', 'var a=!1'],
+	['var a = true ? 1 : 0;', 'var a=true?1:0'],
+
 ];
 
 /*
 var tests = 
 [
-	// make sure we didn't break normal conditionals and assigments
-	['var a = Ti.Platform.name', 'var a="iPhone OS"', ['ios']],
-	['var a = Ti.Platform.name', 'var a="android"', ['android']],
-	['var a = Ti.Platform.name=="iPhone OS" ? 1 : 0', 'var a=1', ['ios']],
-	['var a = Ti.Platform.name=="iPhone OS", b', 'var a=1,b', iosDefines],
-	['var a = Ti.Platform.name=="iPhone OS", b, c = 2', 'var a=1,b,c=2', iosDefines],
-	['var a = Ti.Platform.name=="iPhone OS"', 'var a=1', iosDefines],
-	['var a = Ti.Platform.name!="iPhone OS"', 'var a=1', androidDefines],
-	['var a = Ti.Platform.name=="iPhone OS"', 'var a=0', androidDefines],
-	['var a, b = Ti.Platform.name=="iPhone OS", c = 2;', 'var a,b=1,c=2', iosDefines],
-	['var a = "1"', 'var a="1"', iosDefines],
-	['var a = true', 'var a=true', iosDefines],
-	['var a = 1', 'var a=1', iosDefines],
-	['var a', 'var a', iosDefines],
-	['var a = {}', 'var a={}', iosDefines],
-	['var a = new Object', 'var a=new Object', iosDefines],
-	['var a = new Object()', 'var a=new Object', iosDefines],
-	['var a = Ti.Platform.name', 'var a="iPhone OS"', iosDefines],
-	['var a = Ti.Platform.osname', 'var a="android"', androidDefines],
-	['var a, b = 1, c = 2;', 'var a,b=1,c=2', iosDefines],
-	['var a = 1;', 'var a=1', iosDefines],
-	['var a =+1;', 'var a=+1', iosDefines],
-	['var a =1+1;', 'var a=1+1', iosDefines],
-	['var a = 1.0;', 'var a=1', iosDefines],
-	['var a = 1.02;', 'var a=1.02', iosDefines],
-	['var a = -1.02;', 'var a=-1.02', iosDefines],
-	['var a = !1', 'var a=!1', iosDefines],
-	['var a = true ? 1 : 0;', 'var a=true?1:0', iosDefines],
+
+	
 	["var num = isNaN(amount) || amount === '' || amount === null ? 0.00 : amount;", 'var num=isNaN(amount)||amount===""||amount===null?0:amount', iosDefines],
 	
 	// make sure we didn't break normal if conditions
@@ -112,6 +109,67 @@ _.each(platforms, function(obj,p) {
 	});
 });
 
+// The alloy command test suite
+describe('optimizer.js', function() {
+	_.each(tests, function(test, index) {
+		describe('test #' + (index+1), function() {
+			_.each(platforms, function(platformObj, platform) {
+				describe('[' + platform + ']', function() {
+					var ast, code, 
+						testContent = _.template(test[0], platforms[platform]),
+						prefix = pad(platform),
+						defines = {
+							OS_ANDROID: platform === 'android',
+							OS_IOS: platform === 'ios',
+							OS_MOBILEWEB: platform === 'mobileweb'
+						};
+
+					it(prefix + testContent.blue, function() {
+						expect(true).toBe(true);
+					});
+
+					it(prefix + 'parses AST with uglifyjs', function() {
+						var parseFunction = function() {
+							ast = jsp.parse(testContent);
+						};
+						expect(parseFunction).not.toThrow();
+					});
+					
+					it(prefix + 'optimizes AST via optimizer.js', function() {
+						var optimizeFunction = function() {
+							ast = optimizer.optimize(ast, defines);
+						};
+						expect(optimizeFunction).not.toThrow();
+					});
+
+					it(prefix + 'generates code from optimized AST via uglifyjs', function() {
+						var generateFunction = function() {
+							code = pro.gen_code(ast, {beautify:false}); 
+						};
+						expect(generateFunction).not.toThrow();
+					});
+
+					it(prefix + 'generated code matches expected code', function() {
+						var passFor = test[2];
+						var expected = _.template(test[1], platforms[platform]);
+
+						if (!passFor || _.contains(passFor, platform)) {
+							expect(code).toBe(expected);
+						} else {
+							expect(code).not.toBe(expected);
+						}
+					});
+				});
+			});
+		});
+	});
+});
+
+// helper functions
+function notPlatform(platform) {
+	return _.reject(CONST.PLATFORMS, function(p) { return p === platform; } );
+}
+
 function pad(string) {
 	var ret = '';
 	for (var i = 0; i < 10 - string.length; i++) {
@@ -120,49 +178,4 @@ function pad(string) {
 	return ret;
 }
 
-// The alloy command test suite
-describe('optimizer.js', function() {
-	_.each(tests, function(test, index) {
-		_.each(platforms, function(platformObj, platform) {
-			var ast, code,
-				prefix = 'test #' + (index+1) + ' [' + platform + ']' + pad(platform),
-				defines = {
-					OS_ANDROID: platform === 'android',
-					OS_IOS: platform === 'ios',
-					OS_MOBILEWEB: platform === 'mobileweb'
-				};
 
-			it(prefix + 'parses AST with uglifyjs', function() {
-				var parseFunction = function() {
-					ast = jsp.parse(_.template(test[0], platforms[platform]));
-				};
-				expect(parseFunction).not.toThrow();
-			});
-			
-			it(prefix + 'optimizes AST', function() {
-				var optimizeFunction = function() {
-					ast = optimizer.optimize(ast, defines);
-				};
-				expect(optimizeFunction).not.toThrow();
-			});
-
-			it(prefix + 'generates code from optimized AST', function() {
-				var generateFunction = function() {
-					code = pro.gen_code(ast, {beautify:false}); 
-				};
-				expect(generateFunction).not.toThrow();
-			});
-
-			it(prefix + 'generated code matches expected code', function() {
-				var passFor = test[2];
-				var expected = _.template(test[1], platforms[platform]);
-
-				if (!passFor || _.contains(passFor, platform)) {
-					expect(code).toBe(expected);
-				} else {
-					expect(code).not.toBe(expected);
-				}
-			});
-		});
-	});
-});
