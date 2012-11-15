a2e3294d6ab695b9a2f44d229f969a7fd9933453
tonylukasavage <anthony.lukasavage@gmail.com>
2012-11-15 10:56:20 -0500

    removed refactored out logic

--- Alloy/commands/compile/optimizer.js
+++ Alloy/commands/compile/optimizer.js
@@ -10,16 +10,10 @@ var jsp = require("../../uglify-js/uglify-js").parser,
 	logger = require('../../common/logger.js'),
 	U = require('../../utils'),
 	platformDefines,
-	platformName, platformOsName
-;
+	platformName, 
+	platformOsName;
 
 var JSON_NULL = JSON.parse('null');
-var EQUALITY_SIGNS = {
-	'==': 1,
-	'===': 1,
-	'!=':1,
-	'!==':1
-};
 
 function getVariableStringValue(a)
 {
@@ -40,201 +34,15 @@ function getVariableStringValue(a)
 	return p.substring(0,p.length-1);
 }
 
-function isEqualityOperation(v) {
-	return (v in EQUALITY_SIGNS);
-}
-
-function isEqualOp(v) {
-	return (v === '===' || v === '==');
-}
-
-function removeCode()
-{
-	return ['block'];  // this is effectively an empty code block
-}
-
-function falseVar(name)
-{ 
-	return ['var', [ [ name, [ 'num', 0 ] ] ] ];  // this is effectively "false"
-}
-
-var VAR_IGNORES = {
-	num:1,
-	string:1,
-	name:1,
-	object:1,
-	new:1
-}
-
-function processVarEntry()
-{
-	var rhs = this[1][0];
-	if (rhs.length === 1)
-	{
-		// this is a variable declaration with no assignment
-		return this;
-	}
-	if (rhs[1][0] in VAR_IGNORES)
-	{
-		return this;
-	}
-	if (rhs[1][0] == 'dot')
-	{
-		var varname = getVariableStringValue(rhs[1]);
-		if (varname == 'Ti.Platform.name' || varname == 'Titanium.Platform.name')
-		{
-			return ["var",[[rhs[0],["string",platformName]]]]
-		}
-		if ((varname == 'Ti.Platform.osname' || varname == 'Titanium.Platform.osname') &&
-			platformDefines.OS_ANDROID && platformName === 'android')
-		{
-			return ["var",[[rhs[0],["string","android"]]]]
-		}
-		if ((varname == 'Ti.Platform.osname' || varname == 'Titanium.Platform.osname') &&
-			platformDefines.OS_MOBILEWEB && platformName === 'mobileweb')
-		{
-			return ["var",[[rhs[0],["string","mobileweb"]]]]
-		}
-	}
-	var isConditionalAssignment;
-	try
-	{
-		isConditionalAssignment = this[1][0][1][0]=='conditional';
-	}
-	catch(E)
-	{
-		console.log(this[1])
-		console.log(JSON.stringify(this));
-		console.log(E.stack);
-	}
-	var hasSimpleAssignment = !isConditionalAssignment && this[1][0][1][0]!='binary';
-	if (hasSimpleAssignment)
-	{
-		return this;
-	}
-	if (isConditionalAssignment)
-	{
-		var entry = this[1][0][1][1];
-		if (entry[0]=='name') // skip if normal conditional
-		{
-			return this;
-		}
-		// console.log(entry)
-		var ifTrue = this[1][0][1][2];
-		var ifFalse = this[1][0][1][3];
-		var result = processIf.call([[],entry]);
-		if (result === null)
-		{
-			return this;
-		}
-		if (result[0] == 'block')
-		{
-			return ['var', [ [ this[1][0][0], ifFalse ] ] ];
-		}
-		else if (result[0] === 'num')
-		{
-			return ['var', [ [ this[1][0][0], result[1] ? ifTrue : ifFalse ] ] ];
-		}
-	}
-	var result = processIf.call(this[1][0]);
-	if (result===null)
-	{
-		return this;
-	}
-	if (result[0]=='block' && result.length == 1)
-	{
-		// this is a deadcode removal indicator, return false
-		return falseVar(this[1][0][0]);
-	}
-	else
-	{
-		// return the result as the value of the variable
-		return ['var', [ [ this[1][0][0], result ] ] ];
-	}
-}
-
-function processVar()
-{	
-	var entries = this[1];
-	for (var c=0;c<entries.length;c++)
-	{
-		try
-		{
-			if (entries[c][1]!==null)
-			{
-				var result = processVarEntry.call([this[0], [entries[c]]])[1][0];
-				entries[c] = result;
-			}
-		}
-		catch(E)
-		{
-			console.log('error trying to parse => '+JSON.stringify(this));
-			console.log('error at this line => '+JSON.stringify(entries[c]));
-			return this;
-		}
-	}
-	return this;
-}
-
+// Replace the Ti.Platform variables with strings, when possible. We don't need to do 
+// anything else. uglifyjs will take care of optimization for us by eliminating dead
+// code based on string to string comparisons in the ast_squeeze() step.
 function processObject() {
 	var translate = getVariableStringValue(this);
 	if (_.contains(['Ti.Platform.name','Titanium.Platform.name'], translate) && platformName) {
 		return ['string',platformName];
 	} else if (_.contains(['Ti.Platform.osname','Titanium.Platform.osname'], translate) && platformOsName) {
 		return ['string',platformOsName];
-	}
-	return null;
-}
-
-function processIf() 
-{
-	// if [0]
-	// binary [1],
-	// iflogic [2],
-	// elselogic [3]
-	var op = this[1][1];
-	if (this[1][0] === 'binary' && isEqualityOperation(op))
-	{
-		var lhs = this[1][2];
-		var rhs = this[1][3];
-
-		// right hand must be a string
-		if (rhs[0] !== 'string') {
-			return null;
-		}
-
-		var varName = getVariableStringValue(lhs);
-		var value = getVariableStringValue(rhs);
-		
-		if ((varName === 'Titanium.Platform.osname' || varName === 'Ti.Platform.osname') && (value === 'iphone' || value === 'ipad'))
-		{
-			// TODO: make compile time "universal" distinction btw iphone and ipad - https://jira.appcelerator.org/browse/ALOY-223
-			return null;
-		}
-		
-		if (varName === 'Titanium.Platform.name' || varName === 'Ti.Platform.name' || (varName === 'Ti.Platform.osname' && value === 'android'))
-		{
-			if ((isEqualOp(op) && value === platformName) || (!isEqualOp(op) && value!== platformName))
-			{
-				if (this[2])
-				{
-					return this[2][1][0];
-				}
-				return ['num',1]; // this is effectively "true"
-			}
-			else
-			{
-				var elseLogic = this[3];
-				if (elseLogic)
-				{
-					return this[3][1][0];
-				}
-				else
-				{
-					return removeCode();
-				}
-			}
-		}
 	}
 	return null;
 }
@@ -272,9 +80,7 @@ function optimize(ast, defines, fn) {
 		var w = pro.ast_walker();
 		return w.with_walkers({
 				"dot": processObject,
-				"sub": processObject,
-				//"if" : processIf,
-				//"var" :processVar
+				"sub": processObject
 		}, function() {
 			return w.walk(ast);
 		});
