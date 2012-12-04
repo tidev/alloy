9717fe13b31d437fb4bddab8d96d851f7852886a
tonylukasavage <anthony.lukasavage@gmail.com>
2012-12-04 14:39:41 -0500

    cleanup model

--- test/apps/models/tableview_binding/models/myModel.js
+++ test/apps/models/tableview_binding/models/myModel.js
@@ -1,5 +1,4 @@
 exports.definition = {
-	
 	config: {
 		"columns": {
 			"title":"string",
@@ -10,28 +9,6 @@ exports.definition = {
 			"type": "sql",
 			"collection_name": "myModel"
 		}
-	},		
-
-	extendModel: function(Model) {		
-		_.extend(Model.prototype, {
-						
-			// extended functions go here
-
-		}); // end extend
-		
-		return Model;
-	},
-	
-	
-	extendCollection: function(Collection) {		
-		_.extend(Collection.prototype, {
-			
-			// extended functions go here			
-			
-		}); // end extend
-		
-		return Collection;
 	}
-		
 }
 
