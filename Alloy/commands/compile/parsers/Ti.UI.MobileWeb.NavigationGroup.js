af4b5f99643fe2cd45cfdd2d526c8043f87dc5a2
tonylukasavage <anthony.lukasavage@gmail.com>
2012-12-07 11:23:07 -0500

    removed unnecessary requires

--- Alloy/commands/compile/parsers/Ti.UI.MobileWeb.NavigationGroup.js
+++ Alloy/commands/compile/parsers/Ti.UI.MobileWeb.NavigationGroup.js
@@ -1,7 +1,3 @@
-var _ = require('../../../lib/alloy/underscore')._,
-	U = require('../../../utils'),
-	CU = require('../compilerUtils'); 
-
 exports.parse = function(node, state) {
 	return require('./Ti.UI.iPhone.NavigationGroup').parse(node, state);
 };
