6b9b2e91b7107a2917df103e6e6fb9361e8ccf75
tonylukasavage <anthony.lukasavage@gmail.com>
2012-12-10 10:50:39 -0500

    removed another superfluous fetch(), better comments

--- test/apps/models/todo_binding/controllers/row.js
+++ test/apps/models/todo_binding/controllers/row.js
@@ -26,14 +26,13 @@ function toggleStatus(e) {
 	// finc the todo task by id
 	var todo = todos.get(id);
 
-	// set the current done and date_completed fields for the model
+	// set the current "done" and "date_completed" fields for the model,
+	// then save to presistence, and model-view binding will automatically
+	// reflect this in the tableview
 	todo.set({
     		"done": todo.get('done') ? 0 : 1,
     		"date_completed": moment().unix()
   	}).save(); // save to presistence
-
-  	// update views from sql storage
-  	todos.fetch();
 }
 
 // delete the IDed todo from the collection
