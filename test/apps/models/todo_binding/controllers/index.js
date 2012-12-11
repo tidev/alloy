2eeb73ad6e9725788cd5c52a34308e2446676fbf
tonylukasavage <anthony.lukasavage@gmail.com>
2012-12-11 18:23:57 -0500

    UX improvements in models/todo_binding test app, formatting

--- test/apps/models/todo_binding/controllers/index.js
+++ test/apps/models/todo_binding/controllers/index.js
@@ -36,7 +36,7 @@ function transformFunction(model) {
 
 // open the "add item" window
 function addToDoItem() {
-  Alloy.createController("add").getView().open();
+  	Alloy.createController("add").getView().open();
 }
 
 // Show task list based on selected status type
