981cf8067cdcdadd3af487d1b120c69ab392ba13
tonylukasavage <anthony.lukasavage@gmail.com>
2013-01-09 16:30:16 -0800

    add db_name and comments to models/sql_preload

--- test/apps/models/sql_preload/models/fighters.js
+++ test/apps/models/sql_preload/models/fighters.js
@@ -7,7 +7,17 @@ exports.definition = {
 		"adapter": {
 			"type": "sql",
 			"collection_name": "fighters",
-            "db": "fighters.sqlite"
+
+			// The pre-loaded sqlite DB with which we'd like to
+			// initialize our database.
+            "db_file": "fighters.sqlite",
+
+            // db_name is only really necessary if we use a pre-loaded 
+            // sqlite database and then remove it later. Explicitly
+            // specifying the database name let's Alloy know in which 
+            // file it can find your database. This value should be the 
+            // same as the name of your pre-loaded db.
+            "db_name": "fighters"
 		}
 	}
 }
