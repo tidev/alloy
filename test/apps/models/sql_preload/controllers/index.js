11363ec2088fddcd9b60aead23f4f152d90adbfe
tonylukasavage <anthony.lukasavage@gmail.com>
2013-01-16 16:46:02 -0500

    row clicks show model attributes, header click adds test fighter models

--- test/apps/models/sql_preload/controllers/index.js
+++ test/apps/models/sql_preload/controllers/index.js
@@ -1,3 +1,29 @@
-Alloy.Collections.fighters.fetch();
+var fighters = Alloy.Collections.fighters;
+var counter = 1;
+
+function showId(e) {
+	if (e.row.model) {
+		alert(e.row.model);
+	}
+}
+
+function addTestFighter(e) {
+	// create the test fighter model
+	var model = Alloy.createModel('fighters', {
+		name: 'Name ' + counter,
+		nickname: 'Nickname ' + counter
+	});
+	counter++;
+
+	// add model to the collection and save it to sqlite
+	fighters.add(model);
+	model.save();
+
+	// let's refresh so we can see the ids from the 
+	// sqlite database in the row click alerts
+	fighters.fetch();
+}
+
+fighters.fetch();
 
 $.index.open();
--- test/apps/models/sql_preload/models/fighters.js
+++ test/apps/models/sql_preload/models/fighters.js
@@ -8,10 +8,17 @@ exports.definition = {
 			"type": "sql",
 			"collection_name": "fighters",
 
-			// tells model to use myapp.sqlite file to install 
-			// database and to use "myapp" as the database name
-			// for all further operations
-			"db_file": "/myapp.sqlite"
+			// db_file tells model to use myapp.sqlite file to install 
+			// database and to use "myapp" as the database name for
+			// all further operations
+			"db_file": "/myapp.sqlite",
+
+			// idAttribute tells Alloy/Backbone to use this column in
+			// my table as its unique identifier field. Without 
+			// specifying this, Alloy's default behavior is to create
+			// and "alloy_id" field which will uniquely identify your
+			// rows in the table.
+			"idAttribute": "id" 
 		}
 	}
 }
