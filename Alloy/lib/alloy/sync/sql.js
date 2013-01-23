ca9d2fee5c3aa3a53b69efffdc733c30fe76e62a
tonylukasavage <anthony.lukasavage@gmail.com>
2013-01-23 10:50:55 -0500

    minor refactor of getMigrationFor(), makes sure to close recordset even if its empty

--- Alloy/lib/alloy/sync/sql.js
+++ Alloy/lib/alloy/sync/sql.js
@@ -276,17 +276,16 @@ function Sync(method, model, opts) {
 
 // Gets the current saved migration
 function GetMigrationFor(dbname, table) {
+	var mid = null;
 	var db = Ti.Database.open(dbname);
-	db.execute('CREATE TABLE IF NOT EXISTS migrations (latest TEXT, model TEXT)');
-	var rs = db.execute('SELECT latest FROM migrations where model = ?', table);
+	db.execute('CREATE TABLE IF NOT EXISTS migrations (latest TEXT, model TEXT);');
+	var rs = db.execute('SELECT latest FROM migrations where model = ?;', table);
 	if (rs.isValidRow()) {
-		var mid = rs.field(0);
-		rs.close();
-		db.close()
-		return mid + '';
+		var mid = rs.field(0) + '';
 	}
-	db.close()
-	return null;
+	rs.close();
+	db.close();
+	return mid;
 }
 
 function Migrate(Model) {
