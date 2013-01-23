c074dd7bda1431676a14f055edd7d871098ae99b
tonylukasavage <anthony.lukasavage@gmail.com>
2013-01-23 09:23:47 -0500

    swicth to "sql" adapter

--- test/apps/models/sql_keywords/models/fighters.js
+++ test/apps/models/sql_keywords/models/fighters.js
@@ -5,22 +5,22 @@ exports.definition = {
 			nickname: 'TEXT',
 
 			// column values can have types with keywords
-			fighterId: 'INTEGER PRIMARY KEY AUTOINCREMENT' 
+			fighterId: 'INTEGER PRIMARY KEY AUTOINCREMENT'
 		},
 
 		"adapter": {
-			"type": "sql_new",
+			"type": "sql",
 
 			// The table name inside the sqlite database to use for
 			// models and collections based on this definition.
 			"collection_name": "fighters",
 
 			// idAttribute tells Alloy/Backbone to use this column in
-			// my table as its unique identifier field. Without 
+			// my table as its unique identifier field. Without
 			// specifying this, Alloy's default behavior is to create
 			// and "alloy_id" field which will uniquely identify your
 			// rows in the table with a text GUID.
-			"idAttribute": "fighterId" 
+			"idAttribute": "fighterId"
 		}
 	}
 }
