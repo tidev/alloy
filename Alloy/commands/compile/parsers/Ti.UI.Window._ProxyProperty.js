4870fb0b3d318e780a4134678a93c06cc086fcf6
tonylukasavage <anthony.lukasavage@gmail.com>
2013-01-29 17:39:39 -0500

    Add leftNavButton and rightNavButton support in markup for Ti.UI.iPad.Popover. Easy peesy.

--- Alloy/commands/compile/parsers/Ti.UI.Window._ProxyProperty.js
+++ Alloy/commands/compile/parsers/Ti.UI.Window._ProxyProperty.js
@@ -4,7 +4,8 @@ exports.parse = function(node, state) {
 	_.extend(state, {
 		proxyPropertyDefinition: {
 			parents: [
-				'Ti.UI.Window'
+				'Ti.UI.Window',
+				'Ti.UI.iPad.Popover'
 			]
 		}
 	});
