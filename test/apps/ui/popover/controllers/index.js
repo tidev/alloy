cfda082cba7960ea2c718bc589d065fe188fb8fb
tonylukasavage <anthony.lukasavage@gmail.com>
2013-01-29 19:46:18 -0500

    make sure to only show popover on ipad

--- test/apps/ui/popover/controllers/index.js
+++ test/apps/ui/popover/controllers/index.js
@@ -1,6 +1,10 @@
 function openPopover() {
-	var popover = Alloy.createController('popover').getView();
-	popover.show({view:$.button});	
+	if (Ti.Platform.osname === 'ipad') {
+		var popover = Alloy.createController('popover').getView();
+		popover.show({view:$.button});	
+	} else {
+		alert('Popover only supported on iPad');
+	}
 }
 
 $.index.open();
