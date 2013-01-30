9d6b2ab0436105245284e62220b8ce16010bb83b
tonylukasavage <anthony.lukasavage@gmail.com>
2013-01-30 09:46:10 -0500

    removed old custom code for parsing TSS files, using grammar now

--- Alloy/commands/compile/compilerUtils.js
+++ Alloy/commands/compile/compilerUtils.js
@@ -616,9 +616,6 @@ exports.loadStyle = function(tssFile, manifest) {
 			
 		// Process tss file then convert to JSON
 		try {
-			// var code = processTssFile(contents, manifest);
-			// var json = jsonlint.parse(code);
-
 			var json = require('../../grammar/tss').parse(contents);
 			optimizer.optimizeStyle(json);
 		} catch (e) {
@@ -913,89 +910,6 @@ exports.formatAST = function(ast,config,fn)
 ///////////////////////////////////////
 ////////// private functions //////////
 ///////////////////////////////////////
-function processTssFile(f, manifest) {
-	var widgetId = manifest && manifest.id ? manifest.id : null;
-
-	// Handle "call" ASTs
-    function do_call() {
-    	var name = this[1][1];
-    	var code;
-    	if (name === 'expr') { 
-    		logger.warn('expr() in TSS files is deprecated and will be removed in Alloy 0.4.0.');
-    		code = pro.gen_code(this[2][0]);
-    	} else if (name === 'L') {
-    		code = pro.gen_code(this);
-    	} else if (name === 'WPATH' && widgetId) {
-    		var node = this[2][0];
-    		if (node[0] === 'string') {
-    			var content = node[1];
-    			var wpath = path.join(path.dirname(content),manifest.id,path.basename(content));
-    			
-    			// TODO: http://jira.appcelerator.org/browse/ALOY-296
-    			if (compilerConfig && compilerConfig.alloyConfig &&
-    				compilerConfig.alloyConfig.platform === 'android' && 
-    				!/^\//.test(wpath)) { 
-    				wpath = '/' + wpath; 
-    			} 
-    			
-    			return ['string', wpath];
-    		} else {
-    			U.die('WPATH() can only be used on literal strings in tss style files');
-    		}
-    	} else {
-    		return null;
-    	}
-    	return ['string', STYLE_EXPR_PREFIX + code];
-    };
-
-    // Recursively assemble the full name of a dot-notation variable
-    function processDot(dot,name) {
-    	switch(dot[0]) {
-    		case 'dot':
-    			return processDot(dot[1], '.' + (dot[2] || '') + name);
-    			break;
-    		case 'name':
-    			var pre = dot[1];
-    			if (pre === 'Ti' || pre === 'Titanium' || pre === 'Alloy') {
-    				return pre + name;
-    			} else {
-    				return null;
-    			}
-    			break;
-    	}
-    }
-
-    // Handle all AST "dot"s, looking for Titanium constants
-    function do_dot() {
-    	var name = processDot(this,'');
-    	if (name === null) {
-    		return null;
-    	} else {
-    		return ['string', STYLE_EXPR_PREFIX + name];
-    	}
-    }
-
-    // Generate AST and add the handlers for "call" and "dot" to the AST walker
-    var ast = jsp.parse('module.exports = ' + f);
-	var walker = pro.ast_walker();
-	var new_ast = walker.with_walkers({
-		"call": do_call,
-		"dot": do_dot
-	}, function(){
-        return walker.walk(ast);
-    });
-
-    // generate code based on the new AST. Make sure to keep keys quoted so the
-    // JSON parses without exception. The wild [1][0][1][3] array is how we grab 
-    // just the style object from the AST, leaving behind the appended "module.exports = "
-    return pro.gen_code(new_ast[1][0][1][3], { 
-    	beautify: true, 
-    	quote_keys: true,
-    	ignore_numbers: true,
-    	double_quotes: true
-    }) || '';
-}
-
 function sortStyles(componentStyle) {
 	var mergedStyle = {},
 		regex = /^\s*([\#\.]{0,1})([^\[]+)(?:\[([^\]]+)\])*\s*$/,
