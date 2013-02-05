7d8b14d9b961640a2f2799ca9eae9c4c1eee3374
tonylukasavage <anthony.lukasavage@gmail.com>
2013-02-05 18:20:21 -0500

    adding collection binding support to Ti.UI.DashboardView

--- Alloy/commands/compile/parsers/Ti.UI.DashboardView.js
+++ Alloy/commands/compile/parsers/Ti.UI.DashboardView.js
@@ -1,6 +1,7 @@
 var _ = require('../../../lib/alloy/underscore')._,
 	U = require('../../../utils'),
-	CU = require('../compilerUtils');
+	CU = require('../compilerUtils'),
+	CONST = require('../../../common/constants');
 
 var VALID = [
 	'Ti.UI.DashboardItem'
@@ -13,31 +14,64 @@ exports.parse = function(node, state) {
 function parse(node, state, args) {
 	var children = U.XML.getElementsFromNodes(node.childNodes),
 		arrayName = CU.generateUniqueId(),
-		code = 'var ' + arrayName + ' = [];\n';
+		isCollectionBound = args[CONST.BIND_COLLECTION] ? true : false,
+		code = 'var ' + arrayName + '=[];\n';
 
 	// iterate through all children
-	_.each(U.XML.getElementsFromNodes(node.childNodes), function(child, index) {
-		if (CU.validateNodeName(child, VALID)) {
-			// generate code for the DashboardItem
-			code += CU.generateNodeExtended(child, state, {
-				parent: {},
-				post: function(node, state, args) {
-					return arrayName + '.push(' + state.parent.symbol + ');\n';
-				}
-			});
+	if (!isCollectionBound) {
+		_.each(U.XML.getElementsFromNodes(node.childNodes), function(child, index) {
+			if (CU.validateNodeName(child, VALID)) {
+				// generate code for the DashboardItem
+				code += CU.generateNodeExtended(child, state, {
+					parent: {},
+					post: function(node, state, args) {
+						return arrayName + '.push(' + state.parent.symbol + ');\n';
+					}
+				});
 
-			// When we are done processing the DashboardItem, remove it from the
-			// markup. That way we can just pass back the current DashboardView state as 
-			// the returned state and it can continue to process any other children
-			// without special handling
-			node.removeChild(child);
-		}
-	});
+				// When we are done processing the DashboardItem, remove it from the
+				// markup. That way we can just pass back the current DashboardView state as 
+				// the returned state and it can continue to process any other children
+				// without special handling
+				node.removeChild(child);
+			}
+		});
+	}
 
-	// Create the initial Map code
+	// Create the initial DashboardView code
+	if (isCollectionBound) {
+		_.each(CONST.BIND_PROPERTIES, function(p) {
+			node.removeAttribute(p);
+		});
+	}
 	state.extraStyle = CU.createVariableStyle('data', arrayName);
 	var dashState = require('./default').parse(node, state);
 	code += dashState.code;
+
+	if (isCollectionBound) {
+		var localModel = CU.generateUniqueId();
+		var itemCode = '';
+		var localArray = 'data';
+
+		_.each(U.XML.getElementsFromNodes(node.childNodes), function(child) {
+			itemCode += CU.generateNodeExtended(child, state, {
+				parent: {},
+				local: true,
+				model: localModel,
+				post: function(node, state, args) {
+					return localArray + '.push(' + state.parent.symbol + ');\n';
+				}
+			});
+			node.removeChild(child);
+		});
+
+		code += _.template(CU.generateCollectionBindingTemplate(args), {
+			localModel: localModel,
+			pre: 'var ' + localArray + '=[];',
+			items: itemCode,
+			post: dashState.parent.symbol + '.data=' + localArray + ';'
+		});
+	} 
 
 	// Update the parsing state
 	return _.extend(dashState, {code:code}); 
