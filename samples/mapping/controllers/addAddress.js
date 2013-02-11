d2ee3627652e8b1cb18993992c662b83c1ff7474
tonylukasavage <anthony.lukasavage@gmail.com>
2013-02-11 12:45:12 -0500

    just use $, don't add confusion by adding self=this

--- samples/mapping/controllers/addAddress.js
+++ samples/mapping/controllers/addAddress.js
@@ -1,10 +1,8 @@
 var geo = require('geo');
 
-var self = this;
-
 $.button.addEventListener('click', function(e) {
     $.textField.blur();
     geo.forwardGeocode($.textField.value, function(geodata) {
-        self.trigger('addAnnotation', {geodata: geodata});
+        $.trigger('addAnnotation', {geodata: geodata});
     });
 });
