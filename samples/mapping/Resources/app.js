68d1df87a22336958a6bc7c14ce91beb25f8fef2
tonylukasavage <anthony.lukasavage@gmail.com>
2013-02-11 12:03:12 -0500

    restructured mapping sample to fit samples format

--- samples/mapping/Resources/app.js
+++
@@ -1,7 +0,0 @@
-var Alloy = require("alloy"), _ = Alloy._, Backbone = Alloy.Backbone, geo = require("geo");
-
-Alloy.Globals.LATITUDE_BASE = 37.389569;
-
-Alloy.Globals.LONGITUDE_BASE = -122.050212;
-
-Alloy.createController("index");
