24782a6032e1e59d5f10f112be85c54e066086b1
tonylukasavage <anthony.lukasavage@gmail.com>
2013-02-11 12:36:57 -0500

    file cleanup

--- samples/mapping/alloy.js
+++ samples/mapping/alloy.js
@@ -1,15 +1,3 @@
-// The contents of this file will be executed before any of
-// your view controllers are ever executed, including the index.
-// You have access to all functionality on the `Alloy` namespace.
-//
-// This is a great place to do any initialization for your app
-// or create any global variables/functions that you'd like to
-// make available throughout your app. You can easily make things
-// accessible globally by attaching them to the `Alloy.Globals`
-// object. For example:
-//
-// Alloy.Globals.someGlobalFunction = function(){};
-
 var geo = require('geo');
 
 Alloy.Globals.LATITUDE_BASE = 37.389569;
