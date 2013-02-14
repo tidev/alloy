a4d22bcce7e5210fc40016c585672e653bf516db
tonylukasavage <anthony.lukasavage@gmail.com>
2013-02-14 16:29:18 -0500

    made models/login test app fail gracefully on non-ios platforms (ALOY-521)

--- test/apps/models/login/alloy.js
+++ test/apps/models/login/alloy.js
@@ -1,7 +1,5 @@
 if (OS_IOS) {
 	Alloy.Models.user = Alloy.createModel('user');
 	Alloy.Models.user.fetch();
-} else {
-	alert('This test app is only supported for iOS');
-}
+} 
 
--- test/apps/models/login/controllers/index.js
+++ test/apps/models/login/controllers/index.js
@@ -1,3 +1,7 @@
-// If the user is already logged in, show the home view. If
-// the user is not yet login, show the login view.
-Alloy.createController(Alloy.Models.user.validateAuth() ? 'home' : 'login').getView().open();
+if (OS_IOS) {
+	// If the user is already logged in, show the home view. If
+	// the user is not yet login, show the login view.
+	Alloy.createController(Alloy.Models.user.validateAuth() ? 'home' : 'login').getView().open();
+} else {
+	$.index.open();
+}
