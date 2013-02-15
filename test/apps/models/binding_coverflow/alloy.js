bd3b4442a2f38ae0d8fbe1fc64cfae0877f5662d
tonylukasavage <anthony.lukasavage@gmail.com>
2013-02-15 16:32:49 -0500

    update models/binding_coverflow test app to use new collection binding

--- test/apps/models/binding_coverflow/alloy.js
+++ test/apps/models/binding_coverflow/alloy.js
@@ -4,8 +4,8 @@ var Images = Backbone.Collection.extend({
 });
 Alloy.Collections.images = new Images();
 Alloy.Collections.images.reset([
-	{ url: '/image1.png' },
-	{ url: '/image2.png' },
-	{ url: '/image3.png', width: 100, height: 100 },
-	{ url: '/image4.png', width: 50, height: 50 }
+	{ url: '/appc1.png' },
+	{ url: '/appc2.png' },
+	{ url: '/appc3.png', width: 100, height: 100 },
+	{ url: '/appc4.png', width: 50, height: 50 }
 ]);
--- test/apps/models/binding_coverflow/controllers/index.js
+++ test/apps/models/binding_coverflow/controllers/index.js
@@ -1,14 +1,3 @@
-function isDefined(val) {
-	return typeof val !== 'undefined' && val !== null;
-}
-
-function transformData(model) {
-	var attrs = model.toJSON();
-	!isDefined(attrs.height) && (attrs.height = Ti.UI.SIZE);
-	!isDefined(attrs.width) && (attrs.width = Ti.UI.SIZE);
-	return attrs;
-}
-
 // Fire a dummy event to initiate binding
 Alloy.Collections.images.trigger('fetch');
 
