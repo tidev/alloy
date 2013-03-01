fce6d1c6faa1170e6254f1fdf0324b1de9c63ef1
tonylukasavage <anthony.lukasavage@gmail.com>
2013-03-01 16:50:43 -0500

    Adding an AST parser for finding the base controller

---
+++ Alloy/commands/compile/ast/controller.js
@@ -0,0 +1,18 @@
+var uglifyjs = require('uglify-js');
+
+exports.getBaseController = function(code) {
+	var baseController = 'baseController';
+	var ast = uglifyjs.parse(code);
+	ast.walk(new uglifyjs.TreeWalker(function(node) {
+		if (node instanceof uglifyjs.AST_Assign) {
+			var left = node.left.print_to_string();
+			if (left === 'exports.baseController' ||
+				left === 'exports["baseController"]' ||
+				left === "exports['baseController']") {
+				baseController = node.right.print_to_string();
+			}
+		}
+	}));
+
+	return baseController;
+};
