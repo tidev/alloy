f091f956722d29f5f14b43edf0a1324ebbb5a33b
tonylukasavage <anthony.lukasavage@gmail.com>
2013-03-18 18:44:17 -0400

    simplify version check

--- test/apps/ui/listview/alloy.js
+++ test/apps/ui/listview/alloy.js
@@ -1,5 +1,3 @@
-var parts = Ti.version.split('.');
-var version = parts[0]*100 + parts[1]*10 + parts[2]*1; 
-if (version < 310) {
+if (version >= '3.1.0') {
 	alert('Ti.UI.ListView requires Titanium SDK 3.1.0 or higher');
 }
