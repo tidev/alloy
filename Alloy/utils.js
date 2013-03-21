1eb936d525924db7fb8de7c0475ad3ab1c66870d
tonylukasavage <anthony.lukasavage@gmail.com>
2013-03-21 17:03:34 -0400

    add deepExtend() to utils.js

--- Alloy/utils.js
+++ Alloy/utils.js
@@ -599,5 +599,53 @@ exports.installPlugin = function(alloyPath, projectPath) {
 	});
 }
 
+var deepExtend = function (/*obj_1, [obj_2], [obj_N]*/) {
+    if (arguments.length < 1 || typeof arguments[0] !== 'object') {
+        return false;
+    }
 
+    if (arguments.length < 2) return arguments[0];
+
+    var target = arguments[0];
+
+    // convert arguments to array and cut off target object
+    var args = Array.prototype.slice.call(arguments, 1);
+
+    var key, val, src, clone;
+
+    args.forEach(function (obj) {
+        if (typeof obj !== 'object') return;
+
+        for (key in obj) {
+            if (obj[key] !== void 0) {
+                src = target[key];
+                val = obj[key];
+
+                if (val === target) continue;
+
+                if (typeof val !== 'object' || val === null) {
+                    target[key] = val;
+                    continue;
+                }
+
+                if (typeof src !== 'object') {
+                    clone = (Array.isArray(val)) ? [] : {};
+                    target[key] = deepExtend(clone, val);
+                    continue;
+                }
+
+                if (Array.isArray(val)) {
+                    clone = (Array.isArray(src)) ? src : [];
+                } else {
+                    clone = (!Array.isArray(src)) ? src : {};
+                }
+
+                target[key] = deepExtend(clone, val);
+            }
+        }
+    });
+
+    return target;
+};
+exports.deepExtend = deepExtend;
 
