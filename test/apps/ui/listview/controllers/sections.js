95d7f2ec829e02e5647b583f19923c1b0a735e2e
tonylukasavage <anthony.lukasavage@gmail.com>
2013-04-04 14:27:48 -0400

    create basic, sections, and templates tab for ui/listview

---
+++ test/apps/ui/listview/controllers/sections.js
@@ -0,0 +1,12 @@
+function onItemClick(e) {
+	// get the clicked section
+	var section = $.list.sections[e.sectionIndex];
+
+	// get the clicked item from that section
+	var item = section.getItemAt(e.itemIndex);
+
+	// print the item's title
+	Ti.API.info('sectionIndex: ' + e.sectionIndex);
+	Ti.API.info('itemIndex: ' + e.itemIndex);
+	Ti.API.info('itemclick: ' + item.properties.title);
+}
