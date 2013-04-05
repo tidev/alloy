aff5e29894671cff8ddc6b94df8b2b26088a88d6
tonylukasavage <anthony.lukasavage@gmail.com>
2013-04-05 17:16:05 -0400

    skip ViewTemplates when processing events

--- Alloy/commands/compile/compilerUtils.js
+++ Alloy/commands/compile/compilerUtils.js
@@ -229,7 +229,8 @@ exports.generateNode = function(node, state, defaultId, isTopLevel, isModelOrCol
 
 	// handle any events from markup
 	if (args.events && args.events.length > 0 && 
-		!_.contains(CONST.SKIP_EVENT_HANDLING, args.fullname)) {
+		!_.contains(CONST.SKIP_EVENT_HANDLING, args.fullname) &&
+		!state.isViewTemplate) {
 		// determine which function name to use for event handling:
 		// * addEventListener() for Titanium proxies
 		// * on() for everything else (controllers, models, collections)
