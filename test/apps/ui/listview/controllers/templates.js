c08d9140d6f192a6561c4543a7099108a4a53a30
tonylukasavage <anthony.lukasavage@gmail.com>
2013-04-11 14:31:29 -0400

    used required ItemTemplates in the ui/listview test app

--- test/apps/ui/listview/controllers/templates.js
+++ test/apps/ui/listview/controllers/templates.js
@@ -1,25 +1,50 @@
+var IMAGES = [
+	'/images/alien.png',
+	'/images/predator.png',
+	'/images/pod.png'
+];
+
 var moreItems = [];
 var lastPos = 'left';
+var imageCtr = 0;
 for (var i = 0; i < 1023; i++) {
+
 	// gradual increase to red
 	var hex = (i%256).toString(16);
 	if (hex.length < 2) {
 		hex = '0' + hex;
 	}
 
-	// get the label textAlign
-	var textAlign = i % 2 ? 'center' : lastPos === 'left' ? (lastPos = 'right') : (lastPos = 'left');
-
-	// create tems based on template2 from markup
-	moreItems.push({
-		template: 'template2',
-		label: {
-			text: 'row ' + (i+1),
-			color: '#' + hex + '0000', 
-			textAlign: textAlign,
-			width: Ti.UI.FILL
-		}
-	});
+	// Use a different template every other row
+	if (i%2) {
+		moreItems.push({
+			template: 'requiredTemplate',
+			leftImage: {
+				image: IMAGES[imageCtr++]
+			},
+			rightImage: {
+				image: i%5 ? '/images/up.png' : '/images/down.png'
+			},
+			title: {
+				text: 'This is my title'
+			},
+			subtitle: {
+				text: 'And this is the subtitle'
+			}
+		});
+		imageCtr >= IMAGES.length && (imageCtr = 0); 
+	} else {
+		var textAlign = lastPos === 'left' ? (lastPos = 'right') : (lastPos = 'left');
+		moreItems.push({
+			template: 'template2',
+			label: {
+				text: 'row ' + (i+1),
+				color: '#' + hex + '0000', 
+				textAlign: textAlign,
+				width: Ti.UI.FILL
+			}
+		});
+	}
 }
 
 // append programmatic items to the ones from markup
