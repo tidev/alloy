baee4a0deda426dbd5e72ab2621593f76b646195
tonylukasavage <anthony.lukasavage@gmail.com>
2013-04-21 09:07:31 -0400

    add 1.2.0 prereq doc for listview

---
+++ test/apps/ui/listview/IMPORTANT.md
@@ -0,0 +1,6 @@
+## Prerequisites
+
+* Titanium 3.1.0+
+* Alloy 1.2.0+
+
+The test app will fail with a compile error if you attempt to compile it with any earlier version of Alloy. 
--- test/apps/ui/listview/alloy.js
+++ test/apps/ui/listview/alloy.js
@@ -1,3 +1,5 @@
 if (Ti.version < '3.1.0') {
 	alert('Ti.UI.ListView requires Titanium SDK 3.1.0 or higher');
+} else if (Alloy.version < '1.2.0') {
+	alert('ListView markup integration requires Alloy 1.2.0 or higher');
 }
