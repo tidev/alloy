c7f9ea7dc5d0a5311428d039d83de2588e5c3dc2
tonylukasavage <anthony.lukasavage@gmail.com>
2013-04-22 08:19:07 -0700

    replaced "simple" test app with "config_json"

---
+++ test/apps/basics/config_json/controllers/index.js
@@ -0,0 +1,4 @@
+function showAlert(e) {
+	alert('Alloy.CFG.someValue = ' + Alloy.CFG.someValue);
+}
+$.index.open();
--- test/apps/basics/simple/controllers/index.js
+++
@@ -1,5 +0,0 @@
-function showAlert(e) {
-	alert('$.' + e.source.id + ' says hello! Just this once though.');
-	$.b.removeEventListener("click",showAlert);
-}
-$.index.open();
