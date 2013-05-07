0f8df6501471c185b7b1d5d36c33cc8fbe31a434
tonylukasavage <anthony.lukasavage@gmail.com>
2013-05-07 16:12:57 -0400

    use cleaner description for compile unit tests

--- test/specs/compile.js
+++ test/specs/compile.js
@@ -1,6 +1,7 @@
 var fs = require('fs'),
 	path = require('path'),
 	wrench = require('wrench'),
+	colors = require('colors'),
 	TU = require('../lib/testUtils'),
 	CONST = require('../../Alloy/common/constants'),
 	_ = require('../../Alloy/lib/alloy/underscore')._,
@@ -27,51 +28,54 @@ var alloyRoot = path.join(__dirname,'..','..'),
 describe('alloy compile', function() {
 	// Iterate through each test app and make sure it compiles for all platforms
 	_.each(wrench.readdirSyncRecursive(paths.apps), function(file) {
-		var indexXml = path.join(paths.apps,file,'views','index.xml');
-		if (path.existsSync(indexXml)) {
-			it('preparing test app "' + file + '"', function() {
-				TU.asyncExecTest('jake app:setup dir=' + file + ' quiet=1');
-			});
-
-			_.each(platforms, function(platform,k) {
-				it('compiles "' + file + '" test app for platform [' + platform.platform + ']', 
-					function() {
-						TU.asyncExecTest(
-							'alloy compile ' + paths.harness + ' --config platform=' + platform.platform, {
-							test: function() {
-								var o = this.output;
-
-								// Make sure there were no compile errors
-								expect(o.error).toBeFalsy();
-							},
-							timeout: TIMEOUT_COMPILE
-						}
-					);
+		describe(file.yellow, function() {
+			var indexJs = path.join(paths.apps,file,'controllers','index.js');
+			if (path.existsSync(indexJs)) {
+				it('preparing test app', function() {
+					TU.asyncExecTest('jake app:setup dir=' + file + ' quiet=1');
 				});
 
-				it('leaves no compiler directives in generated code', function() {
-					var hrDir = path.join(paths.harness,'Resources');
-					var cPaths = [
-						path.join(hrDir,'alloy'),
-						path.join(hrDir,platform.titaniumFolder,'alloy')
-					];
+				_.each(platforms, function(platform,k) {
+					describe('for ' + platform.platform.cyan, function () {
+						it('compiles without critical error', 
+							function() {
+								TU.asyncExecTest(
+									'alloy compile ' + paths.harness + ' --config platform=' + platform.platform, {
+									test: function() {
+										var o = this.output;
 
-					_.each(cPaths, function(cPath) {
-						if (!fs.existsSync(cPath)) { return; }
-						var files = wrench.readdirSyncRecursive(cPath);
-						_.each(files, function(file) {
-							var fullpath = path.join(cPath,file);
-							if (!fs.statSync(fullpath).isFile() ||
-								!/\.js$/.test(fullpath)) {
-								return;
-							} 
-							var content = fs.readFileSync(fullpath, 'utf8');
-							expect(cdRegex.test(content)).toBeFalsy();
+										// Make sure there were no compile errors
+										expect(o.error).toBeFalsy();
+									},
+									timeout: TIMEOUT_COMPILE
+								}
+							);
+						});
+
+						it('leaves no compiler directives in generated code', function() {
+							var hrDir = path.join(paths.harness,'Resources');
+							var cPaths = [
+								path.join(hrDir,'alloy'),
+								path.join(hrDir,platform.titaniumFolder,'alloy')
+							];
+
+							_.each(cPaths, function(cPath) {
+								if (!fs.existsSync(cPath)) { return; }
+								var files = wrench.readdirSyncRecursive(cPath);
+								_.each(files, function(file) {
+									var fullpath = path.join(cPath,file);
+									if (!fs.statSync(fullpath).isFile() ||
+										!/\.js$/.test(fullpath)) {
+										return;
+									} 
+									var content = fs.readFileSync(fullpath, 'utf8');
+									expect(cdRegex.test(content)).toBeFalsy();
+								});
+							}); 
 						});
 					});
-					 
 				});
-			});
-		}
+			}
+		});
 	});
 });
