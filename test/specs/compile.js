145282df7312b52718b78a6af180dc6f96013871
tonylukasavage <anthony.lukasavage@gmail.com>
2013-05-09 11:41:09 -0400

    support _generated folder in test apps for comparing generated code testing

--- test/specs/compile.js
+++ test/specs/compile.js
@@ -8,6 +8,7 @@ var fs = require('fs'),
 	platforms = require('../../platforms/index');
 	
 var TIMEOUT_COMPILE = 10000;
+var GEN_FOLDER = '_generated';
 
 var alloyRoot = path.join(__dirname,'..','..'),
 	paths = {
@@ -30,52 +31,69 @@ describe('alloy compile', function() {
 	_.each(wrench.readdirSyncRecursive(paths.apps), function(file) {
 		describe(file.yellow, function() {
 			var indexJs = path.join(paths.apps,file,'controllers','index.js');
-			if (path.existsSync(indexJs)) {
-				it('preparing test app', function() {
-					TU.asyncExecTest('jake app:setup dir=' + file + ' quiet=1');
-				});
+			if (!path.existsSync(indexJs)) { return; }
 
-				_.each(platforms, function(platform,k) {
-					describe('for ' + platform.platform.cyan, function () {
-						it('compiles without critical error', 
-							function() {
-								TU.asyncExecTest(
-									'alloy compile ' + paths.harness + ' --config platform=' + platform.platform, {
-									test: function() {
-										var o = this.output;
+			it('preparing test app', function() {
+				TU.asyncExecTest('jake app:setup dir=' + file + ' quiet=1');
+			});
 
-										// Make sure there were no compile errors
-										expect(o.error).toBeFalsy();
-									},
-									timeout: TIMEOUT_COMPILE
-								}
-							);
-						});
+			_.each(platforms, function(platform,k) {
+				describe('for ' + platform.platform.cyan, function () {
+					it('compiles without critical error', 
+						function() {
+							TU.asyncExecTest(
+								'alloy compile ' + paths.harness + ' --config platform=' + platform.platform, {
+								test: function() {
+									// Make sure there were no compile errors
+									expect(this.output.error).toBeFalsy();
+								},
+								timeout: TIMEOUT_COMPILE
+							}
+						);
+					});
 
-						it('leaves no compiler directives in generated code', function() {
-							var hrDir = path.join(paths.harness,'Resources');
-							var cPaths = [
-								path.join(hrDir,'alloy'),
-								path.join(hrDir,platform.titaniumFolder,'alloy')
-							];
+					it('leaves no compiler directives in generated code', function() {
+						var hrDir = path.join(paths.harness,'Resources');
+						var cPaths = [
+							path.join(hrDir,'alloy'),
+							path.join(hrDir,platform.titaniumFolder,'alloy')
+						];
 
-							_.each(cPaths, function(cPath) {
-								if (!fs.existsSync(cPath)) { return; }
-								var files = wrench.readdirSyncRecursive(cPath);
-								_.each(files, function(file) {
-									var fullpath = path.join(cPath,file);
-									if (!fs.statSync(fullpath).isFile() ||
-										!/\.js$/.test(fullpath)) {
-										return;
-									} 
-									var content = fs.readFileSync(fullpath, 'utf8');
-									expect(cdRegex.test(content)).toBeFalsy();
-								});
-							}); 
+						_.each(cPaths, function(cPath) {
+							if (!fs.existsSync(cPath)) { return; }
+							var files = wrench.readdirSyncRecursive(cPath);
+							_.each(files, function(file) {
+								var fullpath = path.join(cPath,file);
+								if (!fs.statSync(fullpath).isFile() ||
+									!/\.js$/.test(fullpath)) {
+									return;
+								} 
+								var content = fs.readFileSync(fullpath, 'utf8');
+								expect(cdRegex.test(content)).toBeFalsy();
+							});
+						}); 
+					});
+
+					var genFolder = path.join(paths.apps,file,GEN_FOLDER,platform.platform);
+					if (!fs.existsSync(genFolder)) { return; }
+					var hrFolder = path.join(paths.harness,'Resources');
+					var files = wrench.readdirSyncRecursive(genFolder);
+
+					_.each(files, function(gFile) {
+						var goodFile = path.join(genFolder,gFile);
+						if (!fs.statSync(goodFile).isFile()) { return; }
+						
+						it('matches known good generated code for ' + gFile.yellow, function () {
+							var newFile = path.join(hrFolder,gFile);
+							expect(fs.existsSync(newFile)).toBeTruthy();
+
+							var goodFileContents = fs.readFileSync(goodFile, 'utf8');
+							var newFileContents = fs.readFileSync(newFile, 'utf8');
+							expect(goodFileContents === newFileContents).toBeTruthy();
 						});
 					});
 				});
-			}
+			});
 		});
 	});
 });
