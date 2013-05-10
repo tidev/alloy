87f347b00202f24ac609c831e28970e6039bfddb
tonylukasavage <anthony.lukasavage@gmail.com>
2013-05-09 20:21:55 -0400

    updating basics test apps "simple" and "builtins" to use testing helper module

--- test/apps/basics/simple/specs/index.js
+++ test/apps/basics/simple/specs/index.js
@@ -1,46 +1,33 @@
-var j = require('jasmine'),
-	CR = require('ConsoleReporter'),
-	describe = j.describe,
-	it = j.it,
-	expect = j.expect,
-	jasmine = j.jasmine;
+Ti.include('runtimeTester.js');
 
 module.exports = function($) {
+	addMatchers();
+
 	describe('index controller', function() {
-		it('has an #index component', function() {
-			expect($.index).toBeDefined();
-			expect($.index).not.toBeNull();
+		validateUiComponent($, 'index', {
+			api: 'Ti.UI.Window',
+			style: {
+				id: 'index',
+				backgroundColor: '#fff',
+				exitOnClose: true,
+				fullscreen: false
+			}
 		});
 
-		it('#index component is a Ti.UI.Window', function() {
-			expect(_.isFunction($.index.open)).toBeTruthy();
-		});
-
-		it('#index component has correct style', function() {
-			expect($.index.backgroundColor).toEqual('#fff');
-			expect($.index.exitOnClose).toEqual(true);
-			expect($.index.fullscreen).toEqual(false);
-			expect($.index.id).toEqual('index');
-		});
-
-		it('has an #label component', function() {
-			expect($.label).toBeDefined();
-			expect($.label).not.toBeNull();
-		});
-
-		it('#label component is a Ti.UI.Label', function() {
-			expect($.label.text).toBeTruthy();
-		});
-
-		it('#label component has correct style', function() {
-			expect($.label.color).toEqual('#000');
-			expect($.label.font.fontSize).toEqual(OS_MOBILEWEB ? '18px' : '18dp');
-			expect($.label.font.fontWeight).toEqual('bold');
-			expect($.label.height).toEqual(Ti.UI.SIZE);
-			expect($.label.width).toEqual(Ti.UI.SIZE);
-			expect($.label.text).toEqual('Hello, World!');
-			expect($.label.id).toEqual('label');
-		});
+		validateUiComponent($, 'label', {
+			api: 'Ti.UI.Label',
+			style: {
+				color: "#000",
+		        font: {
+		            fontSize: OS_MOBILEWEB ? "18px" : "18dp",
+		            fontWeight: "bold"
+		        },
+		        height: Ti.UI.SIZE,
+		        width: Ti.UI.SIZE,
+		        text: "Hello, World!",
+		        id: "label"
+			}
+		});		
 
 		it('#label can change text property', function() {
 			var newtext = 'TEST TEXT';
@@ -49,10 +36,5 @@ module.exports = function($) {
 		});
 	});
 
-	jasmine.getEnv().addReporter(new CR({
-		doneCallback: function(runner) {
-			alert(runner.specs().length + ' specs, ' + runner.results().failedCount + ' failed');
-		}
-	}));
-	jasmine.getEnv().execute();
+	launchTests();
 };
