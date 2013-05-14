9eb53c8f8140bc5e2f31535bf65cad0eaf3f20fd
tonylukasavage <anthony.lukasavage@gmail.com>
2013-05-14 12:43:14 -0400

    added _generated and runtime testing for advanced/native_modules

---
+++ test/apps/advanced/native_modules/_generated/blackberry/alloy/controllers/index.js
@@ -0,0 +1,24 @@
+function Controller() {
+    require("alloy/controllers/BaseController").apply(this, Array.prototype.slice.call(arguments));
+    arguments[0] ? arguments[0]["__parentSymbol"] : null;
+    arguments[0] ? arguments[0]["$model"] : null;
+    arguments[0] ? arguments[0]["__itemTemplate"] : null;
+    var $ = this;
+    var exports = {};
+    $.__views.index = Ti.UI.createWindow({
+        backgroundColor: "#fff",
+        fullscreen: false,
+        exitOnClose: true,
+        id: "index"
+    });
+    $.__views.index && $.addTopLevelView($.__views.index);
+    exports.destroy = function() {};
+    _.extend($, $.__views);
+    $.index.open();
+    require("specs/index")($);
+    _.extend($, exports);
+}
+
+var Alloy = require("alloy"), Backbone = Alloy.Backbone, _ = Alloy._;
+
+module.exports = Controller;
