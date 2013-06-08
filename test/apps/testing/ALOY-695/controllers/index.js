a9ee1fa999756dc7c1c78930647634c8bf454a66
tonylukasavage <anthony.lukasavage@gmail.com>
2013-06-07 22:31:55 -0400

    testing/ALOY-695 test app

---
+++ test/apps/testing/ALOY-695/controllers/index.js
@@ -0,0 +1,20 @@
+var ctr = 0;
+function addNewLabel(e) {
+	var index = ctr % Alloy.Globals.classes.length;
+	var label = $.UI.create('Label', {
+			classes: Alloy.Globals.classes[index],
+			id: 'newLabel' + (ctr+1),
+			text: 'this is label #' + (ctr+1),
+			touchEnabled: false
+		}
+	);
+
+	$.index.add(label);
+	ctr++;
+}
+
+function openFooBar(e) {
+	Alloy.createController('foo/bar').getView().open();
+}
+
+$.index.open();
