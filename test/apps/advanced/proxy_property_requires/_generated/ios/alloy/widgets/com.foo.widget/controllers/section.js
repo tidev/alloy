93d6a5b29cd92eb637f13e79549e6f2e30f5773c
tonylukasavage <anthony.lukasavage@gmail.com>
2013-06-08 10:56:32 -0400

    updated _generated code for __path template change

--- test/apps/advanced/proxy_property_requires/_generated/ios/alloy/widgets/com.foo.widget/controllers/section.js
+++ test/apps/advanced/proxy_property_requires/_generated/ios/alloy/widgets/com.foo.widget/controllers/section.js
@@ -7,6 +7,7 @@ function WPATH(s) {
 function Controller() {
     new (require("alloy/widget"))("com.foo.widget");
     require("alloy/controllers/BaseController").apply(this, Array.prototype.slice.call(arguments));
+    this.__path = "section";
     arguments[0] ? arguments[0]["__parentSymbol"] : null;
     arguments[0] ? arguments[0]["$model"] : null;
     arguments[0] ? arguments[0]["__itemTemplate"] : null;
--- test/apps/advanced/themes/_generated/android/alloy/controllers/dialog.js
+++ test/apps/advanced/themes/_generated/android/alloy/controllers/dialog.js
@@ -8,6 +8,7 @@ function Controller() {
         $.dialog.close();
     }
     require("alloy/controllers/BaseController").apply(this, Array.prototype.slice.call(arguments));
+    this.__path = "dialog";
     arguments[0] ? arguments[0]["__parentSymbol"] : null;
     arguments[0] ? arguments[0]["$model"] : null;
     arguments[0] ? arguments[0]["__itemTemplate"] : null;
