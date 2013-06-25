4ebbe9da939777ac5a5c1d9a54e0c98ab6b98d36
tonylukasavage <anthony.lukasavage@gmail.com>
2013-06-25 16:18:23 -0400

    dynamic styling sample app

--- test/apps/advanced/dynamic_styling/alloy.js
+++ test/apps/advanced/dynamic_styling/alloy.js
@@ -34,13 +34,6 @@ views.reset([
 		subtitle: 'Dynamically add and remove TSS classes from UI components',
 		shortTitle: 'Add/Remove Classes',
 		controller: 'classes'
-	},
-	{
-		id: 6,
-		title: 'Styling Plain Titanium Proxies',
-		subtitle: 'Turn plain Titanium proxies into fully styled components',
-		shortTitle: 'Styling Ti Proxies',
-		controller: 'tiproxies'
 	}
 ]);
 
@@ -61,3 +54,13 @@ Alloy.Globals.closeWindow = function() {
 		Alloy.Globals.currentWindow = null;
 	}
 }
+
+Alloy.Globals.print = function(o, title) {
+	if (title) {
+		Ti.API.info('******** ' + title + ' ********');
+	}
+	Ti.API.info(JSON.stringify(o, null, '  '));
+	Ti.API.info(' ');
+}
+
+Alloy.Globals.htmlTemplate = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"><html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /><script type="text/javascript" src="web/syntaxhighlighter/scripts/shCore.js"></script><script type="text/javascript" src="web/syntaxhighlighter/scripts/shBrushJScript.js"></script><link type="text/css" rel="stylesheet" href="web/syntaxhighlighter/styles/shCoreDefault.css"/><script type="text/javascript">SyntaxHighlighter.all();</script></head><body style="background: white; font-family: Helvetica; "><pre class="brush: <%= BRUSH %>; gutter: false;"><%= CODE %></pre></html>';
