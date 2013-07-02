85d7e5afd7efb3cadfe9980256b782cc0dcaac36
Fokke Zandbergen <mail@fokkezb.nl>
2013-07-02 20:40:54 +0200

    Fix for TC-2561

--- Alloy/lib/alloy/sync/sql.js
+++ Alloy/lib/alloy/sync/sql.js
@@ -379,7 +379,7 @@ function installDatabase(config) {
 		throw 'Invalid sql database filename "' + dbFile + '"';
 	}
 	//var isAbsolute = match[1] ? true : false;
-	var dbName = config.adapter.db_name = match[2];
+	var dbName = config.adapter.db_name || match[2];
 
 	// install and open the preloaded db
 	Ti.API.debug('Installing sql database "' + dbFile + '" with name "' + dbName + '"');
