84cd50663a037e71ced18955bb6810dc02ba8c82
David Bankier <dbankier@hotmail.com>
2013-07-09 20:42:34 +1000

    xml2tss 0.3; generate style [name]; generate styles --all

--- Alloy/commands/generate/targets/styles.js
+++ Alloy/commands/generate/targets/styles.js
@@ -1,49 +1,48 @@
 var basePath = '../../../';
 var path = require('path'),
-    fs = require('fs'),
-    wrench = require('wrench'),
-    xml2tss = require('xml2tss'),
-    alloyRoot = path.join(__dirname,'..','..'),
-    _ = require(basePath + 'lib/alloy/underscore')._,
-    U = require(basePath + 'utils'),
-    CONST = require(basePath + 'common/constants'),
-    logger = require(basePath + 'logger');
+		wrench = require('wrench'),
+		xml2tss = require('xml2tss'),
+		_ = require(basePath + 'lib/alloy/underscore')._,
+		U = require(basePath + 'utils'),
+		CONST = require(basePath + 'common/constants'),
+		logger = require(basePath + 'logger');
 
 // a recursive function to generate styles since xml2tss is async
 function generateStyles(targets) {
-  if (targets.length > 0) {
-    // generate style
-    var current = targets.pop();
-    xml2tss.convertFile(current.view_path, function(err,content) {
-      fs.writeFileSync(current.style_path, content);
-      logger.info('Style generated: ' + current.style);
-      generateStyles(targets);
-    });
-  }
+	if (targets.length > 0) {
+		// generate style
+		var current = targets.pop();
+		xml2tss.updateFile(current.view_path, current.style_path, function(err,ok) {
+			if (ok) {
+				logger.info('Style generated: ' + current.style);
+			}
+			generateStyles(targets);
+		});
+	}
 }
 
 module.exports = function(name, args, program) {
-  var paths      = U.getAndValidateProjectPaths(program.outputPath),
-      view_root  = path.join(paths.app,CONST.DIR.VIEW),
-      style_root = path.join(paths.app,CONST.DIR.STYLE),
-      targets =[];
+	var paths      = U.getAndValidateProjectPaths(program.outputPath),
+			view_root  = path.join(paths.app,CONST.DIR.VIEW),
+			style_root = path.join(paths.app,CONST.DIR.STYLE),
+			targets =[];
 
-  wrench.readdirSyncRecursive(view_root).forEach(function(view) {
-    if (view.match(".xml$")) {
-      var style = view.replace(/\.xml/,'.tss'),
-          style_path = path.join(style_root, style),
-          view_path  = path.join(view_root, view);
+	if (!program.all) {
+		U.die(" styles command must be used with the --all flag");
+	}
+	wrench.readdirSyncRecursive(view_root).forEach(function(view) {
+		if (view.match(".xml$")) {
+			var style = view.replace(/\.xml/,'.tss'),
+					style_path = path.join(style_root, style),
+					view_path  = path.join(view_root, view);
 
-      // only generate if the style doesn't exist
-      if (!path.existsSync(style_path)) {
-        // make sure the target folder exists
-        var fullDir = path.dirname(style_path);
-        if (!path.existsSync(fullDir)) {
-          wrench.mkdirSyncRecursive(fullDir);
-        } 
-        targets.push({style:style, style_path: style_path, view_path:view_path});
-      }
-    }
-  });
-  generateStyles(targets);
+			// make sure the target folder exists
+			var fullDir = path.dirname(style_path);
+			if (!path.existsSync(fullDir)) {
+				wrench.mkdirSyncRecursive(fullDir);
+			} 
+			targets.push({style:style, style_path: style_path, view_path:view_path});
+		}
+	});
+	generateStyles(targets);
 };
