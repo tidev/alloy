ee0641833d817d54eadcd26265e1bf75ee575986
tonylukasavage <anthony.lukasavage@gmail.com>
2013-07-22 12:28:26 -0400

    [ALOY-753] test case and linting for properties adapter reset() fix

--- Alloy/lib/alloy/sync/properties.js
+++ Alloy/lib/alloy/sync/properties.js
@@ -4,11 +4,11 @@ var Alloy = require('alloy'),
 
 function S4() {
    return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
-};
+}
 
 function guid() {
    return (S4()+S4()+'-'+S4()+'-'+S4()+'-'+S4()+'-'+S4()+S4()+S4());
-};
+}
 
 function Sync(method, model, opts) {
 	var prefix = model.config.adapter.collection_name ? model.config.adapter.collection_name : 'default';
@@ -36,7 +36,7 @@ function Sync(method, model, opts) {
 	else if (method === 'create' || method === 'update') {
 		if (!model.id) {
 			model.id = guid();
-	        model.set(model.idAttribute, model.id);
+			model.set(model.idAttribute, model.id);
 		}
 		TAP.setObject(prefix + '-' + model.id, model.toJSON() || {});
 		resp = model.toJSON();
@@ -48,10 +48,10 @@ function Sync(method, model, opts) {
 
 	// process success/error handlers, if present
 	if (resp) {
-        _.isFunction(opts.success) && opts.success(resp);
-        method === "read" && model.trigger("fetch");
+        if (_.isFunction(opts.success)) { opts.success(resp); }
+        if (method === "read") { model.trigger("fetch"); }
     } else {
-    	_.isFunction(opts.error) && opts.error(resp);
+		if (_.isFunction(opts.error)) { opts.error(resp); }
     }
 }
 
---
+++ test/apps/testing/ALOY-753/controllers/index.js
@@ -0,0 +1,9 @@
+$.index.open();
+
+var personCol = Alloy.createCollection('user');
+
+personCol.on('reset',function(){
+    alert('reset');
+});
+
+personCol.fetch();
