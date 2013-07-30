2b391378289e9d4f20298dfa5aa45cc70f35b492
tonylukasavage <anthony.lukasavage@gmail.com>
2013-07-30 17:27:39 -0400

    [ALOY-332] More efficient creation of TabGroups [ALOY-477] Tighten checks for upping stack size when rhino is used

--- Alloy/commands/compile/parsers/Ti.UI.TabGroup.js
+++ Alloy/commands/compile/parsers/Ti.UI.TabGroup.js
@@ -1,25 +1,45 @@
 var _ = require('../../../lib/alloy/underscore')._,
 	U = require('../../../utils'),
-	CU = require('../compilerUtils');
+	tiapp = require('../../../tiapp'),
+	CU = require('../compilerUtils'),
+	styler = require('../styler');
+
+// does the current TiSDK for the platform properly support the "tabs" property
+var platform = CU.getCompilerConfig().alloyConfig.platform;
+var SUPPORTS_TABS = platform === 'ios' ||
+	(platform !== 'blackberry' && tiapp.version.gte('3.1.0'));
 
 exports.parse = function(node, state) {
 	return require('./base').parse(node, state, parse);
 };
 
 function parse(node, state, args) {
-	var code = '';
+	var code = '',
+		tabArray, groupState;
 
-	// Create the initial TabGroup code
-	var groupState = require('./default').parse(node, state);
-	code += groupState.code;
+	if (!SUPPORTS_TABS) {
+		groupState = require('./default').parse(node, state);
+		code += groupState.code;
+	}
 
 	_.each(U.XML.getElementsFromNodes(node.childNodes), function(child) {
 		var theNode = CU.validateNodeName(child, ['Ti.UI.Tab','Ti.Android.Menu']);
 		if (theNode) {
 			var ext = { parent: {} };
 			if (theNode === 'Ti.UI.Tab') {
+				// create the tab array
+				if (SUPPORTS_TABS && !tabArray) {
+					tabArray = CU.generateUniqueId();
+					code += 'var ' + tabArray + '=[];';
+				}
+
+				// render the code for adding the tab to the array
 				ext.post = function(node, state, args) {
-					return groupState.parent.symbol + '.addTab(' + state.parent.symbol + ');';
+					if (SUPPORTS_TABS) {
+						return tabArray + '.push(' + state.parent.symbol + ');';
+					} else {
+						return groupState.parent.symbol + '.addTab(' + state.parent.symbol + ');';
+					}
 				};
 			} else if (theNode === 'Ti.Android.Menu') {
 				ext.parent.symbol = args.symbol;
@@ -32,6 +52,17 @@ function parse(node, state, args) {
 			]);
 		}
 	});
+
+	if (SUPPORTS_TABS) {
+		// attach all created tabs in one shot as the "tabs" property
+		var extras = [];
+		if (tabArray) { extras.push(['tabs', tabArray]); }
+		if (extras.length) { state.extraStyle = styler.createVariableStyle(extras); }
+
+		// Create the TabGroup itself
+		groupState = require('./default').parse(node, state);
+		code += groupState.code;
+	}
 
 	// Update the parsing state
 	return {
