232bd15067a840d647c97322ac4401823d08d78f
tonylukasavage <anthony.lukasavage@gmail.com>
2013-08-07 14:30:20 -0400

    sync adapter cleanup, eliminate utils.js so adapters are standalone

--- Alloy/lib/alloy/sync/util.js
+++
@@ -1,7 +0,0 @@
-function S4() {
-	return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
-};
-
-exports.guid = function() {
-	return (S4()+S4()+'-'+S4()+'-'+S4()+'-'+S4()+'-'+S4()+S4()+S4());
-}
