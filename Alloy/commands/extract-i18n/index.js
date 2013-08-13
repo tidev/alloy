bf0aa975702987f5cefd1b0a03011c45a9acf5bc
Fokke Zandbergen <mail@fokkezb.nl>
2013-08-13 15:53:48 +0200

    Refactored to parse XML views

--- Alloy/commands/extract-i18n/index.js
+++ Alloy/commands/extract-i18n/index.js
@@ -7,10 +7,35 @@ var fs = require('fs'),
     logger = require('../../logger'),
     i18nHandler = require('./i18nHandler');
 
-var searchString = "(?:set|[^a-z])(?:L|Ti.Locale.getString|Titanium.Locale.getString|titleid|textid|messageid|titlepromptid|subtitleid|hinttextid|promptid)" +
-    "\\s*[\\(:=]\\s*[\"']([a-z]\\w*?)[\"']";
+var properties = 'titleid|textid|messageid|titlepromptid|subtitleid|hinttextid|promptid';
+var viewProperties = properties.split('|');
+
+var searchString = "(?:set|[^a-z])(?:L|Ti.Locale.getString|Titanium.Locale.getString|" + properties + ")" +
+    "\\s*[\\(:]\\s*[\"']([a-z]\\w*?)[\"']";
 var searchRegex = new RegExp(searchString, 'gi');
 var valueRegex = new RegExp(searchString, 'i');
+
+function extractStringsFromViewNodes(nodes, strings) {
+    var elements = U.XML.getElementsFromNodes(nodes);
+
+    _.each(elements, function(element) {
+        _.each(properties, function (property) {
+            if (element.hasAttribute(property)) {
+                strings.push(element.getAttribute(property));
+            }
+        });
+        if (element.hasChildNodes) {
+            extractStringsFromViewNodes(element.childNodes, strings);
+        }
+    });
+
+    return strings;
+}
+
+function extractStringsFromView(view) {
+    var docRoot = U.XML.getAlloyFromFile(view);
+    return extractStringsFromViewNodes(docRoot.childNodes, []);
+}
 
 function extractStrings() {
     try {
@@ -20,26 +45,31 @@ function extractStrings() {
         var controllerSuffix = '.' + CONST.FILE_EXT.CONTROLLER;
         var viewSuffix = '.' + CONST.FILE_EXT.VIEW;
 
-        // filter only js, style and view files
-        files = _.filter(files, function(f) {
-            return f.substr(-styleSuffix.length) === styleSuffix ||
-                f.substr(-controllerSuffix.length) === controllerSuffix ||
-                f.substr(-viewSuffix.length) === viewSuffix;
-        });
-
-        var strings = [];
         _.each(files, function(f) {
             var file = path.join(sourceDir, f);
-            var fileContent = fs.readFileSync(file, 'utf8');
-            var calls = fileContent.match(searchRegex);
 
-            if (calls && calls.length > 0) {
-                logger.debug(file + ': ' + calls.length + ' strings found.');
+            // view
+            if (f.substr(-viewSuffix.length) === viewSuffix) {
+                var found = extractStringsFromView(file, strings);
 
-                _.each(calls, function(call) {
-                    var matches = call.match(valueRegex);
-                    strings.push(matches[1]);
-                });
+                if (found.length > 0) {
+                    logger.debug(file + ': ' + found.length + ' strings found.');
+                    strings = _.union(strings, found);
+                }
+
+            // controller or style
+            } else if (f.substr(-styleSuffix.length) === styleSuffix || f.substr(-controllerSuffix.length) === controllerSuffix) {
+                var fileContent = fs.readFileSync(file, 'utf8');
+                var calls = fileContent.match(searchRegex);
+
+                if (calls && calls.length > 0) {
+                    logger.debug(file + ': ' + calls.length + ' strings found.');
+
+                    _.each(calls, function(call) {
+                        var matches = call.match(valueRegex);
+                        strings.push(matches[1]);
+                    });
+                }
             }
         });
 
@@ -47,7 +77,7 @@ function extractStrings() {
         logger.info("Found " + strings.length + " unique i18n strings in code. Checking against current i18n file...");
         return strings;
     } catch(e) {
-        return [];
+        U.die('Error extracting strings', e);
     }
 }
 
