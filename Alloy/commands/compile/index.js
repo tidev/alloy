cb2a46322cc64238de5c3690823d5321afccf953
Tony Lukasavage <anthony.lukasavage@gmail.com>
2013-08-22 22:17:40 -0400

    [ALOY-803] fix __controllerPath on Windows

--- Alloy/commands/compile/index.js
+++ Alloy/commands/compile/index.js
@@ -317,7 +317,7 @@ function parseAlloyComponent(view,dir,manifest,noView) {
 			modelVariable: CONST.BIND_MODEL_VAR,
 			parentVariable: CONST.PARENT_SYMBOL_VAR,
 			itemTemplateVariable: CONST.ITEM_TEMPLATE_VAR,
-			controllerPath: (dirname ? path.join(dirname,viewName) : viewName).replace(/\\/g, '\\\\'),
+			controllerPath: (dirname ? path.join(dirname,viewName) : viewName).replace(/\\/g, '/'),
 			preCode: '',
 			postCode: '',
 			Widget: !manifest ? '' : "var " + CONST.WIDGET_OBJECT + " = new (require('alloy/widget'))('" + manifest.id + "');this.__widgetId='" + manifest.id + "';",
