0f70f7fb2482bee3f9d6fbe10901e6db3128ab63
Tony Lukasavage <anthony.lukasavage@gmail.com>
2013-10-23 14:04:09 -0400

    [ALOY-842] fix pr/249, make retina conversions automatic for measurement.js, test app

--- Alloy/builtins/measurement.js
+++ Alloy/builtins/measurement.js
@@ -4,7 +4,7 @@
  * These functions are only useful on the Android platform to support devices with different
  * screen densities and resolutions.
  *
- * To use the measurement builtin library, 
+ * To use the measurement builtin library,
  * require it with the `alloy` root directory in your `require` call. For example:
  *
  *     var measurement = require('alloy/measurement');
@@ -19,14 +19,13 @@ var dpi = Ti.Platform.displayCaps.dpi,
  * @method dpToPx
  * Converts a density-independent pixel value to screen pixels.
  * @param {Number} val Value in density-independent pixels.
- * @param {Boolean} retina Multiple dp by 2 for retina.
  * @return {Number} Converted value in screen pixels.
  */
-exports.dpToPX = function (val, retina) {
+exports.dpToPX = function (val) {
     if (OS_ANDROID) {
         return val * dpi / 160;
-    } else if (OS_IOS && retina) {
-        return val * (density === 'high') ? 2 : 1;
+    } else if (OS_IOS) {
+        return val * (density === 'high' ? 2 : 1);
     } else {
         return val;
     }
@@ -36,14 +35,13 @@ exports.dpToPX = function (val, retina) {
  * @method pxToDP
  * Converts a screen pixel value to density-independent pixels.
  * @param {Number} val Value in screen pixels.
- * @param {Boolean} retina Devide px by 2 for retina.
  * @return {Number} Converted value in density-independent pixels.
  */
-exports.pxToDP = function (val, retina) {
+exports.pxToDP = function (val) {
     if (OS_ANDROID) {
         return val / dpi * 160;
-    } else if (OS_IOS && retina) {
-        return val / (density === 'high') ? 2 : 1;   
+    } else if (OS_IOS) {
+        return val / (density === 'high' ? 2 : 1);
     } else {
         return val;
     }
@@ -53,12 +51,11 @@ exports.pxToDP = function (val, retina) {
  * @method pointPXToDP
  * Converts a coordinate (x, y) from screen pixels to density-independent pixels.
  * @param {Number} val Coordinate in screen pixels.
- *  @param {Boolean} retina Device px by 2 for retina.
  * @return {Number} Converted coordinate in density-independent pixels.
  */
-exports.pointPXToDP = function (pt, retina) {
-    if (OS_ANDROID || (OS_IOS && retina)) {
-        return { x: exports.pxToDP(pt.x, retina), y: exports.pxToDP(pt.y, retina) };
+exports.pointPXToDP = function (pt) {
+    if (OS_ANDROID || OS_IOS) {
+        return { x: exports.pxToDP(pt.x), y: exports.pxToDP(pt.y) };
     } else {
         return pt;
     }
