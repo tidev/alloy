6e98808151a04c4514ead083b28e8fa3d2c79b10
Tony Lukasavage <anthony.lukasavage@gmail.com>
2013-11-07 08:14:27 -0500

    [ALOY-262] Allow shorthand for systemButton attribute on <Button>

---
+++ test/apps/testing/ALOY-262/alloy.js
@@ -0,0 +1,11 @@
+var supported = false;
+if (OS_IOS) {
+	var major = Ti.Platform.version.split('.')[0];
+	if (major && parseInt(major, 10) <= 6) {
+		supported = true;
+	}
+}
+
+if (supported) {
+
+}
