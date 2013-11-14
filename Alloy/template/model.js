3059dbcdadf64b2b543c2b382cbd3ac493629ce9
Fokke Zandbergen <mail@fokkezb.nl>
2013-11-14 08:30:05 +0100

    Declare Alloy before inserting model definition
    This way you can use `Alloy.CFG.foo` in the definition.

--- Alloy/template/model.js
+++ Alloy/template/model.js
@@ -1,8 +1,8 @@
-<%= modelJs %>
-
 var Alloy = require('alloy'),
     _ = require("alloy/underscore")._,
 	model, collection;
+	
+<%= modelJs %>
 
 model = Alloy.M('<%= basename %>',
 	 exports.definition,
