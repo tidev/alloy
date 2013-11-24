68207a7ce956586b42cde1772db21a5e6451b750
Tony Lukasavage <anthony.lukasavage@gmail.com>
2013-11-24 09:47:29 -0500

    [ALOY-885] use the right version this time

--- Alloy/tiapp.js
+++ Alloy/tiapp.js
@@ -1,7 +1,7 @@
 var fs = require('fs'),
 	path = require('path'),
 	XMLSerializer = require("xmldom").XMLSerializer,
-	pkginfo = require('pkginfo')(module,'version'),
+	pkg = require('../package'),
 	U = require('./utils'),
 	CONST = require('./common/constants'),
 	logger = require('./logger');
@@ -60,7 +60,7 @@ tiapp.validateSdkVersion = function() {
 		logger.warn('Unable to determine Titanium SDK version from tiapp.xml.');
 		logger.warn('Your app may have unexpected behavior. Make sure your tiapp.xml is valid.');
 	} else if (tiapp.version.lt(tiVersion, CONST.MINIMUM_TI_SDK)) {
-		logger.error('Alloy ' + tiVersion + ' requires Titanium SDK ' +
+		logger.error('Alloy ' + pkg.version + ' requires Titanium SDK ' +
 			CONST.MINIMUM_TI_SDK + ' or higher.');
 		logger.error('"' + tiVersion + '" was found in the "sdk-version" field of your tiapp.xml.');
 		logger.error('If you are building with the legacy titanium.py script and are specifying ');
