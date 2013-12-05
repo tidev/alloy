6c3f39e8e08ca0afa4244c0f36c1ed32e09e8ff8
Tony Lukasavage <anthony.lukasavage@gmail.com>
2013-12-05 16:06:13 -0500

    [ALOY-886] fix broken widget test apps

--- test/apps/widgets/widget_bouncylogo/controllers/index.js
+++ test/apps/widgets/widget_bouncylogo/controllers/index.js
@@ -1,5 +1,5 @@
 function IndexOpen(e) {
-    $.logo.init({ image: '/images/alloy.png', width: 216, height: 200 });
+	$.logo.init({ image: '/images/alloy.png', width: 216, height: 200 });
 }
 
 $.index.open();
