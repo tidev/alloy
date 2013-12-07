81109f7af07d22a81a0a3c0c0eb54372f8056ed1
Tony Lukasavage <anthony.lukasavage@gmail.com>
2013-12-07 12:17:11 -0500

    [ALOY-897] don't attach Alloy hidden properties to arguments, lots of test updates

--- test/apps/basics/markup_require/_generated/blackberry/alloy/controllers/theRest.js
+++ test/apps/basics/markup_require/_generated/blackberry/alloy/controllers/theRest.js
@@ -1,9 +1,20 @@
+function __processArg(obj, key) {
+    var arg = null;
+    if (obj) {
+        arg = obj[key] || null;
+        delete obj[key];
+    }
+    return arg;
+}
+
 function Controller() {
     require("alloy/controllers/BaseController").apply(this, Array.prototype.slice.call(arguments));
     this.__controllerPath = "theRest";
-    var __parentSymbol = arguments[0] ? arguments[0]["__parentSymbol"] : null;
-    arguments[0] ? arguments[0]["$model"] : null;
-    arguments[0] ? arguments[0]["__itemTemplate"] : null;
+    if (arguments[0]) {
+        var __parentSymbol = __processArg(arguments[0], "__parentSymbol");
+        __processArg(arguments[0], "$model");
+        __processArg(arguments[0], "__itemTemplate");
+    }
     var $ = this;
     var exports = {};
     $.__views.middle = Alloy.createController("middle", {
