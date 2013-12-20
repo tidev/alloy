063cd1b6c597044a81314582548a0c5472d22255
Tony Lukasavage <anthony.lukasavage@gmail.com>
2013-12-20 15:03:36 -0500

    [ALOY-907] Properly report AST parsing error

--- Alloy/commands/compile/ast/controller.js
+++ Alloy/commands/compile/ast/controller.js
@@ -1,18 +1,24 @@
-var uglifyjs = require('uglify-js');
+var U = require('../../../utils'),
+	uglifyjs = require('uglify-js');
 
-exports.getBaseController = function(code) {
+exports.getBaseController = function(code, file) {
 	var baseController = '"BaseController"';
-	var ast = uglifyjs.parse(code);
-	ast.walk(new uglifyjs.TreeWalker(function(node) {
-		if (node instanceof uglifyjs.AST_Assign) {
-			var left = node.left.print_to_string();
-			if (left === 'exports.baseController' ||
-				left === 'exports["baseController"]' ||
-				left === "exports['baseController']") {
-				baseController = node.right.print_to_string();
+
+	try {
+		var ast = uglifyjs.parse(code);
+		ast.walk(new uglifyjs.TreeWalker(function(node) {
+			if (node instanceof uglifyjs.AST_Assign) {
+				var left = node.left.print_to_string();
+				if (left === 'exports.baseController' ||
+					left === 'exports["baseController"]' ||
+					left === "exports['baseController']") {
+					baseController = node.right.print_to_string();
+				}
 			}
-		}
-	}));
+		}));
+	} catch (e) {
+		U.die('Error generating AST for "' + file + '"', e);
+	}
 
 	return baseController;
 };
