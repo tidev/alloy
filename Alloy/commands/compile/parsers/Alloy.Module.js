29faf745e89ce2a23d0a6650b79804c5634322e2
Tim Poulsen <tim@skypanther.com>
2014-02-04 15:23:23 -0500

    [ALOY-800] Add support for Annotation within Module
    adding annotations via map module
    
    add annotations to parent map
    
    add comments

--- Alloy/commands/compile/parsers/Alloy.Module.js
+++ Alloy/commands/compile/parsers/Alloy.Module.js
@@ -1,4 +1,11 @@
 exports.parse = function(node, state) {
-	node.nodeName = 'View';
-	return require('./default').parse(node, state);
+	// default handling for View-based modules except ti.map
+	if(node.getAttribute('module')!=='ti.map' && node.getAttribute('ns')!=='Alloy.Globals.Map') {
+		node.nodeName = 'View';
+		return require('./default').parse(node, state);
+	} else {
+		// handle map views specially
+		node.nodeName = 'View';
+		return require('./Ti.Map.View').parse(node, state);
+	}
 };
