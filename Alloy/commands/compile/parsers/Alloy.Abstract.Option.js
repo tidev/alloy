7117dae7d311ddf573d112f8392e3c01656fe398
Tony Lukasavage <anthony.lukasavage@gmail.com>
2014-05-14 14:42:20 -0400

    [ALOY-1009] alloy localized strings in option XML

--- Alloy/commands/compile/parsers/Alloy.Abstract.Option.js
+++ Alloy/commands/compile/parsers/Alloy.Abstract.Option.js
@@ -2,6 +2,8 @@ var _ = require('../../../lib/alloy/underscore')._,
 	U = require('../../../utils'),
 	CU = require('../compilerUtils');
 
+var LOCALE_REGEX = /^\s*(?:L|Ti\.Locale\.getString|Titanium\.Locale\.getString)\(.+\)\s*$/;
+
 exports.parse = function(node, state) {
 	return require('./base').parse(node, state, parse);
 };
@@ -10,11 +12,15 @@ function parse(node, state, args) {
 	if (!state.itemsArray) {
 		U.die('Invalid use of <Option>. Must be the child of <Options>.');
 	}
-	var string = U.trim(U.XML.getNodeText(node) || '').replace(/"/g,'\\"');
+
+	var string = U.trim(U.XML.getNodeText(node) || '');
+	if (!LOCALE_REGEX.test(string)) {
+		string = '"' + string.replace(/"/g,'\\"') + '"';
+	}
 
 	return {
 		parent: {},
 		styles: state.styles,
-		code: state.itemsArray + '.push("' + string + '");'
+		code: state.itemsArray + '.push(' + string + ');'
 	};
 }
