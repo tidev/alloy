d5f301c69117d3fe1153d35c74a286f9a29eb8e2
Feon Sua <fmiao@appcelerator.com>
2014-05-21 10:46:16 +0800

    updated test app

--- test/apps/testing/ALOY-949/controllers/index.js
+++ test/apps/testing/ALOY-949/controllers/index.js
@@ -27,8 +27,8 @@ function changeWithArgs() {
 
 function changeWithArgsAgain() {
 	dialogs.confirm({
-		title: 'Confirm (args.no & args.yes)',
-		messages: 'Are the args.no & args.yes taking precedence?',
+		title: 'Confirm (args.no:Neh & args.yes:Yeah)',
+		message: 'Are the args.no & args.yes taking precedence?',
 		buttonNames:["Nope", 'Yup'],
 		no: 'Neh',
 		yes: 'Yeah',
