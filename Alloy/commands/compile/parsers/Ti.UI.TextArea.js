dc2e14dfdaa4b827fafeda27876b58ae47ffcd5c
Feon Sua <fmiao@appcelerator.com>
2014-06-06 15:55:38 +0800

    [ALOY-981] Implement KeyboardToolbar for TextArea

--- Alloy/commands/compile/parsers/Ti.UI.TextArea.js
+++ Alloy/commands/compile/parsers/Ti.UI.TextArea.js
@@ -1,7 +1,8 @@
 var _ = require('../../../lib/alloy/underscore')._,
 	styler = require('../styler'),
 	CU = require('../compilerUtils'),
-	U = require('../../../utils');
+	U = require('../../../utils'),
+	CONST = require('../../../common/constants');
 
 var KEYBOARD_TYPES = [
 	'DEFAULT', 'ASCII', 'NUMBERS_PUNCTUATION', 'URL', 'EMAIL', 'DECIMAL_PAD', 'NAMEPHONE_PAD',
@@ -20,11 +21,64 @@ exports.parse = function(node, state) {
 };
 
 function parse(node, state, args) {
-	// convert clearOnEdit to a boolean
-	if (node.hasAttribute('clearOnEdit')) {
-		var attr = node.getAttribute('clearOnEdit');
-		state.extraStyle = styler.createVariableStyle('clearOnEdit', attr === 'true');
-	}
+	var code = '',
+		postCode = '',
+		extras = [],
+		proxyProperties = {};
+
+	// iterate through all children of the TextArea
+	_.each(U.XML.getElementsFromNodes(node.childNodes), function(child) {
+		var fullname = CU.getNodeFullname(child),
+			isProxyProperty = false,
+			isControllerNode = false,
+			hasUiNodes = false,
+			controllerSymbol,
+			parentSymbol;
+
+		// validate the child element and determine if it's part of
+		// the textarea or a proxy property assigment
+		if (!CU.isNodeForCurrentPlatform(child)) {
+			return;
+		} else if (_.contains(CONST.CONTROLLER_NODES, fullname)) {
+			isControllerNode = true;
+		} else if (fullname.split('.')[0] === '_ProxyProperty') {
+			isProxyProperty = true;
+		}
+
+		// generate the node
+		code += CU.generateNodeExtended(child, state, {
+			parent: {},
+			post: function(node, state, args) {
+				controllerSymbol = state.controller;
+				parentSymbol = state.parent ? state.parent.symbol : null;
+			}
+		});
+
+		// manually handle controller node proxy properties
+		if (isControllerNode) {
+
+			// set up any proxy properties at the top-level of the controller
+			var inspect = CU.inspectRequireNode(child);
+			_.each(_.uniq(inspect.names), function(name) {
+				if (name.split('.')[0] === '_ProxyProperty') {
+					var prop = U.proxyPropertyNameFromFullname(name);
+					proxyProperties[prop] = controllerSymbol + '.getProxyPropertyEx("' + prop + '", {recurse:true})';
+				} else {
+					hasUiNodes = true;
+				}
+			});
+		}
+
+		// generate code for proxy property assignments
+		if (isProxyProperty) {
+			proxyProperties[U.proxyPropertyNameFromFullname(fullname)] = parentSymbol;
+
+		// generate code for the child components
+		} else if (hasUiNodes || !isControllerNode) {
+			postCode += '<%= parentSymbol %>.add(' + parentSymbol + ');';
+		}
+
+	});
 
 	// support shortcuts for keyboard type, return key type, and autocapitalization
 	var keyboardType = node.getAttribute('keyboardType');
@@ -40,6 +94,31 @@ function parse(node, state, args) {
 		node.setAttribute('autocapitalization', 'Ti.UI.TEXT_AUTOCAPITALIZATION_' + autocapitalization.toUpperCase());
 	}
 
-	// Generate runtime code using default
-	return require('./default').parse(node, state);
+
+	// add all creation time properties to the state
+	if (node.hasAttribute('clearOnEdit')) {
+		var attr = node.getAttribute('clearOnEdit');
+		extras.push(['clearOnEdit', attr === 'true']);
+	}
+	_.each(proxyProperties, function(v, k) {
+		extras.push([k, v]);
+	});
+	if (extras.length) { state.extraStyle = styler.createVariableStyle(extras); }
+
+	// generate the code for the textarea itself
+	var nodeState = require('./default').parse(node, state);
+	code += nodeState.code;
+
+	// add the rows to the section
+	if (postCode) {
+		code += _.template(postCode, {
+			parentSymbol: nodeState.parent.symbol
+		});
+	}
+
+	// Update the parsing state
+	return _.extend(state, {
+		parent: nodeState.parent,
+		code: code
+	});
 }
