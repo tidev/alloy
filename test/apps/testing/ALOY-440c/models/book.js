bfaaf3d7b8cb3803b513c035c91f10bba844a33e
skypanther <tim@skypanther.com>
2014-08-20 15:55:50 -0400

    [ALOY-440] Support dynamic collections on picker

---
+++ test/apps/testing/ALOY-440c/models/book.js
@@ -0,0 +1,23 @@
+exports.definition = {
+	config: {
+
+		adapter: {
+			type: "properties",
+			collection_name: "book"
+		}
+	},
+	extendModel: function(Model) {
+		_.extend(Model.prototype, {
+			// extended functions and properties go here
+		});
+
+		return Model;
+	},
+	extendCollection: function(Collection) {
+		_.extend(Collection.prototype, {
+			// extended functions and properties go here
+		});
+
+		return Collection;
+	}
+};
