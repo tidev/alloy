5db9ff4a7c0d99de3dfe3fff7e596461281088b5
skypanther <tim@skypanther.com>
2014-10-24 14:01:24 -0400

    [ALOY-1098] ActionView of MenuItem: backport to 1_6 branch

--- Alloy/template/Ti.Android.MenuItem.js
+++ Alloy/template/Ti.Android.MenuItem.js
@@ -1,3 +1,6 @@
 var <%= style %> = <%= styleCode %>;
+if(<%= actionView %>) {
+	<%= style %>.actionView = <%= actionView %>;
+}
 <%= item %> = <%= parent %>.add(_.pick(<%= style %>,Alloy.Android.menuItemCreateArgs));
 <%= item %>.applyProperties(_.omit(<%= style %>,Alloy.Android.menuItemCreateArgs));
