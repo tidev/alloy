9c609191388d442d345c05d0cc084eae188b5023
Feon Sua <feonsua@Feons-MacBook-Pro.local>
2014-10-24 21:11:22 +0800

    [ALOY-1158] Update model defn in tests for Backbone 1.1.2 on 1_6_X

--- test/apps/testing/ALOY-1080/models/fruits.js
+++ test/apps/testing/ALOY-1080/models/fruits.js
@@ -17,7 +17,15 @@ exports.definition = {
 	},
 	extendCollection: function(Collection) {
 		_.extend(Collection.prototype, {
-			// extended functions and properties go here
+			
+			// For Backbone v1.1.2, uncomment this to override the fetch method
+			/*
+			fetch: function(options) {
+				options = options ? _.clone(options) : {};
+				options.reset = true;
+				return Backbone.Collection.prototype.fetch.call(this, options);
+			},
+			*/
 		});
 
 		return Collection;
--- test/apps/testing/ALOY-817/models/empty.js
+++ test/apps/testing/ALOY-817/models/empty.js
@@ -1,3 +1,17 @@
 exports.definition = {
-	config: {}
+	config: {},
+	extendCollection : function(Collection) {
+		_.extend(Collection.prototype, {
+
+			// For Backbone v1.1.2, uncomment this to override the fetch method
+			/*
+			fetch: function(options) {
+				options = options ? _.clone(options) : {};
+				options.reset = true;
+				return Backbone.Collection.prototype.fetch.call(this, options);
+			},
+			*/
+		});
+		return Collection;
+	}
 };
