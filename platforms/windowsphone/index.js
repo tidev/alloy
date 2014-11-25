d45b24f339d0c909d1e12ae39ba5de1d47827447
skypanther <tim@skypanther.com>
2014-11-25 14:36:02 -0500

    update build platform name to match CLI

--- platforms/windowsphone/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = new (require('../base.js'))({
-	platform: 'windowsphone',
-	alloyFolder: 'windowsphone',
-	titaniumFolder: 'windowsphone',
-	name: 'Windows Phone',
-	osname: 'windowsphone'
-});
--- platforms/windowsstore/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = new (require('../base.js'))({
-	platform: 'windowsstore',
-	alloyFolder: 'windowsstore',
-	titaniumFolder: 'windowsstore',
-	name: 'Windows Store',
-	osname: 'windowsstore'
-});
