9a5358f54dee36d20b45cf2a845f153acb24df34
skypanther <tim@skypanther.com>
2015-01-07 17:03:20 -0500

    [ALOY-1222] Fix failing tests: install, compile of advanced/themes

--- test/apps/advanced/themes/_generated/blackberry/alloy/controllers/dialog.js
+++ test/apps/advanced/themes/_generated/blackberry/alloy/controllers/dialog.js
@@ -19,10 +19,17 @@ function Controller() {
     }
     require("alloy/controllers/BaseController").apply(this, Array.prototype.slice.call(arguments));
     this.__controllerPath = "dialog";
+    this.args = arguments[0] || {};
     if (arguments[0]) {
-        __processArg(arguments[0], "__parentSymbol");
-        __processArg(arguments[0], "$model");
-        __processArg(arguments[0], "__itemTemplate");
+        {
+            __processArg(arguments[0], "__parentSymbol");
+        }
+        {
+            __processArg(arguments[0], "$model");
+        }
+        {
+            __processArg(arguments[0], "__itemTemplate");
+        }
     }
     var $ = this;
     var exports = {};
