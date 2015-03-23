9e4867cbd107fd9968ff44977449b94f842eb85c
Fokke Zandbergen <mail@fokkezb.nl>
2015-03-23 09:27:42 +0100

    Add support for manually adding tracked event listeners

--- test/apps/testing/ALOY-1237/controllers/index.js
+++ test/apps/testing/ALOY-1237/controllers/index.js
@@ -1,19 +1,25 @@
 function doOpen() {
+
+  // manually add event
+  $.addListener($.index, 'click', doWindowClick);
+
 	// Showing the all events in this controller
-	var events = $.getEvents();
-	_.each(events, function(event){
+	_.each($.__events, function(event){
 		console.log(event);
 	});
 }
 
-function doClose() {
-	// Window closed event
+// Once event fire, you do not fire again.
+// All events to this window have been removed.
+function doWindowClick() {
+	$.removeListener($.index);
+  console.log('Removed the all events on this window');
 }
 
 // Once event fire, you do not fire again.
 // All events was nothing in this controller.
 function doClick() {
-	$.removeEvents();
+	$.removeListener();
 	console.log('Removed the all events in this controller');
 }
 
