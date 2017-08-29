eaca03432088f818f6bfd15ffc344791ef7c366e
Hans Knöchel <hansemannn@users.noreply.github.com>
2017-08-29 09:35:59 +0200

    Update Ti.UI.Toolbar.js

--- Alloy/commands/compile/parsers/Ti.UI.Toolbar.js
+++ Alloy/commands/compile/parsers/Ti.UI.Toolbar.js
@@ -6,13 +6,13 @@ exports.parse = function(node, state) {
 	var eventObject = 'e',
 		code = '',
 		xmlStyles = {};
-	//properties inheritted from ActionBar when Toolbar is passed to the activity as one
+	// Properties inherited from ActionBar when Toolbar is passed to the Activity
 	var inheritedProperties = ['backgroundImage', 'displayHomeAsUp', 'homeButtonEnabled', 'navigationMode', 'onHomeItemSelected'];
 
 	state = _.extend(state, {
 		itemContainerDefinition: {
 			children: [
-				{ name:'Alloy.Abstract.Items', property:'items' }
+				{ name: 'Alloy.Abstract.Items', property: 'items' }
 			]
 		}
 	});
@@ -20,26 +20,26 @@ exports.parse = function(node, state) {
 
 	var tempRes = require('./Alloy.Abstract._ItemContainer').parse(node, state);
 
-	//Only if the toolbar is passed as an ActionBar to the activity
+	// Only if the Ti.UI.Toolvar is passed as an ActionBar to the activity
 	if (state.parent && state.parent.node && state.parent.node.getAttribute('customToolbar') === node.getAttribute('id')) {
 
 		var activityTssStyles = _.filter(state.styles, function(elem) {
-			// generates a sorted array of styles filtered to include only elements
+			// Generates a sorted array of styles filtered to include only elements
 			// associated with the activity (by ID, class, or API name)
 			return elem.key === node.getAttribute('id') || elem.key === node.getAttribute('class') || elem.key === node.nodeName;
 		});
-		// create an object holding all the actionBar-related properties set in the XML
+		// Create an object holding all the actionBar-related properties set in the XML
 		_.each(inheritedProperties, function(prop) {
 			if (node.hasAttribute(prop)) {
 				xmlStyles[prop] = node.getAttribute(prop);
 			}
 		});
-		// to respect proper style hierarchy, take the last element in the array (which will be the highest priority)
+		// To respect proper style hierarchy, take the last element in the array (which will be the highest priority)
 		var activityTssKey = _.isArray(activityTssStyles) ? activityTssStyles.length - 1 : 0;
 		if (activityTssStyles[activityTssKey] && activityTssStyles[activityTssKey].style) {
 			_.defaults(xmlStyles, activityTssStyles[activityTssKey].style);
 		}
-		// generate the template code
+		// Generate the template code
 		if ((_.filter(_.values(xmlStyles), function(val) { return val !== undefined; })).length > 0) {
 			code += state.parent.symbol + '.activity.setSupportActionBar($.__views.' + node.getAttribute('id') + ');';
 			if (xmlStyles.backgroundImage)  { code += state.parent.symbol + '.activity.actionBar.backgroundImage = "' + xmlStyles.backgroundImage + '";'; }
