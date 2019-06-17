67f8e6cdc098db047527e78dd04d57447666ac33
Chris Williams <chris.a.williams@gmail.com>
2019-06-17 09:06:05 -0400

    fix(sourceMapper): fix generated source maps - report absolute path for 'file' property - report absolute path for 'sourceRoot', not a file:// URI - embed sourcesContent for mapped files - fix line counts in source map after hitting template marker lines

--- Alloy/commands/compile/sourceMapper.js
+++ Alloy/commands/compile/sourceMapper.js
@@ -61,8 +61,8 @@ exports.generateCodeAndSourceMap = function(generator, compileConfig) {
 	var relativeOutfile = path.relative(compileConfig.dir.project, outfile);
 	var markers = _.map(data, function(v, k) { return k; });
 	var mapper = new SM.SourceMapGenerator({
-		file: relativeOutfile,
-		sourceRoot: `file://${compileConfig.dir.project}/`
+		file: `${compileConfig.dir.project}/${relativeOutfile}`,
+		sourceRoot: compileConfig.dir.project
 	});
 	var genMap = {
 		file: target.filename,
@@ -72,17 +72,22 @@ exports.generateCodeAndSourceMap = function(generator, compileConfig) {
 
 	// initialize the rest of the generator properties
 	target.count = 1;
-	target.lines = getTextFromGenerator(target.templateContent, target.template).split(lineSplitter);
+	// ensure target.templateContent will have a value so we can embed in sourcesContent
+	target.templateContent = getTextFromGenerator(target.templateContent, target.template);
+	target.lines = target.templateContent.split(lineSplitter);
 	_.each(markers, function(m) {
 		var marker = data[m];
 		marker.count = 1;
-		marker.lines = getTextFromGenerator(marker.fileContent, marker.filepath).split(lineSplitter);
+		// ensure marker.fileContent will have a value so we can embed in sourcesContent
+		marker.fileContent = getTextFromGenerator(marker.fileContent, marker.filepath);
+		marker.lines = marker.fileContent.split(lineSplitter);
 	});
 
 	// generate the source map and composite code
 	_.each(target.lines, function(line) {
 		var trimmed = U.trim(line);
 		if (_.includes(markers, trimmed)) {
+			target.count++; // skip this line in the template count now or else we'll be off by one from here on out
 			_.each(data[trimmed].lines, function(line) {
 				mapLine(mapper, data[trimmed], genMap, line);
 			});
@@ -111,18 +116,20 @@ exports.generateCodeAndSourceMap = function(generator, compileConfig) {
 		]
 	});
 	if (compileConfig.sourcemap) {
+		// Tell babel to retain the lines so they stay correct (columns go wacky, but OH WELL)
+		// we produce our own source maps and we want the lines to stay as we mapped them
 		options.retainLines = true;
-		// FIXME: babel source map generation is broken! So we copy the source map we generated initially
-		// And we tell babel to retain the lines so they stay correct (columns go wacky, but OH WELL)
-		// options.sourceMaps = true;
-		// options.sourceMapTarget = target.filename;
-		// options.inputSourceMap = mapper.toJSON();
 	}
 	var outputResult = babel.transformFromAstSync(ast, genMap.code, options);
 
-	// write the generated controller code
+	// produce the source map and embed the original source (so the template source can be passed along)
+	const sourceMap = mapper.toJSON();
+	sourceMap.sourcesContent = [ target.templateContent, data[markers[0]].fileContent ];
+
+	// append pointer to the source map to the generated code
 	outputResult.code += `\n//# sourceMappingURL=file://${compileConfig.dir.project}/${CONST.DIR.MAP}/${relativeOutfile}.${CONST.FILE_EXT.MAP}`;
 
+	// write the generated controller code
 	fs.mkdirpSync(path.dirname(outfile));
 	chmodr.sync(path.dirname(outfile), 0755);
 	fs.writeFileSync(outfile, outputResult.code.toString());
@@ -135,10 +142,7 @@ exports.generateCodeAndSourceMap = function(generator, compileConfig) {
 		relativeOutfile = path.relative(compileConfig.dir.project, outfile);
 		fs.mkdirpSync(path.dirname(outfile));
 		chmodr.sync(path.dirname(outfile), 0755);
-		fs.writeFileSync(outfile, JSON.stringify(mapper.toJSON()));
-		// FIXME: babel source map generation is broken! So we copy the source map we generated initially
-		// fs.writeFileSync(outfile, JSON.stringify(outputResult.map));
-		// logger.debug('  map:        "' + relativeOutfile + '"');
+		fs.writeFileSync(outfile, JSON.stringify(sourceMap));
 	}
 };
 
@@ -148,8 +152,8 @@ exports.generateSourceMap = function(generator, compileConfig) {
 	var origFile = generator.origFile;
 	var markers = _.map(data, function(v, k) { return k; });
 	var mapper = new SM.SourceMapGenerator({
-		file: target.filename, // relative path to output file from project root
-		sourceRoot: `file://${compileConfig.dir.project}/`
+		file: `${compileConfig.dir.project}/${target.filename}`,
+		sourceRoot: compileConfig.dir.project
 	});
 	var genMap = {
 		count: 1,
@@ -157,25 +161,30 @@ exports.generateSourceMap = function(generator, compileConfig) {
 	};
 	// passed in to mapLine so "sources" points at original src file
 	var theMap = {
-		count: 1,
+		count: 1, // used to track line numbers for source mapping
 		filename: origFile.filename // relative path to src file from project root
 	};
 
 	// initialize the rest of the generator properties
 	target.count = 1;
 	// gets text from original src file since template content is empty
-	target.lines = getTextFromGenerator(null, origFile.filepath).split(lineSplitter);
+	// ensure target.templateContent will have a value so we can embed in sourcesContent if we want to
+	target.templateContent = getTextFromGenerator(null, origFile.filepath);
+	target.lines = target.templateContent.split(lineSplitter);
 	_.each(markers, function(m) {
 		var marker = data[m];
 		marker.count = 1;
-		marker.lines = getTextFromGenerator(marker.fileContent, marker.filename).split(lineSplitter);
+		// ensure marker.fileContent will have a value so we can embed in sourcesContent if we want to
+		marker.fileContent = getTextFromGenerator(marker.fileContent, marker.filename);
+		marker.lines = marker.fileContent.split(lineSplitter);
 	});
 
 	// generate the source map and composite code
-	// (shoudl simpley map every line from src -> dest)
+	// (should simply map every line from src -> dest)
 	_.each(target.lines, function(line) {
 		var trimmed = U.trim(line);
 		if (_.includes(markers, trimmed)) {
+			theMap.count++; // skip this line number in the input file now or else we'll be off by one from here on out
 			_.each(data[trimmed].lines, function(line) {
 				mapLine(mapper, data[trimmed], genMap, line);
 			});
@@ -199,6 +208,7 @@ exports.generateSourceMap = function(generator, compileConfig) {
 	}
 
 	// TODO: We do not run the babel plugins (optimizer/builtins) here. Is that ok?
+	// TODO: embed sourcesContent into source map? Shouldn't need to since this is supposed to be a straight copy
 
 	// write source map for the generated file
 	var relativeOutfile = path.relative(compileConfig.dir.project, target.filepath);
@@ -206,11 +216,6 @@ exports.generateSourceMap = function(generator, compileConfig) {
 	var outfile = path.join(mapDir, relativeOutfile) + '.' + CONST.FILE_EXT.MAP;
 	fs.mkdirpSync(path.dirname(outfile));
 	chmodr.sync(path.dirname(outfile), 0755);
-	// FIXME: babel source map generation is broken! So we copy the source map we generated initially
 	fs.writeFileSync(outfile, JSON.stringify(mapper.toJSON()));
-	// var tmp = outputResult.map;
-	// tmp.sources[0] = compiledFileName;
-	// tmp.sources[1] = origFileName;
-	// fs.writeFileSync(outfile, JSON.stringify(tmp));
 	logger.debug('  map:        "' + outfile + '"');
 };
