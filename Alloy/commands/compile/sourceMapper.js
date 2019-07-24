8fed9c71aaac2526e23268bc8f54a98ebcb10a3f
Sam Haig <samjhaig@gmail.com>
2019-07-24 13:26:38 +0100

    Better filename generation for error message

--- Alloy/commands/compile/sourceMapper.js
+++ Alloy/commands/compile/sourceMapper.js
@@ -109,7 +109,9 @@ exports.generateCodeAndSourceMap = function(generator, compileConfig) {
 			sourceType: 'module'
 		});
 	} catch (e) {
-		U.dieWithCodeFrame(`Error parsing code in ${outfile}. ${e.message}`, e.loc, genMap.code);
+		const filename = path.relative(compileConfig.dir.project, generator.target.template);
+
+		U.dieWithCodeFrame(`Error parsing code in ${filename}. ${e.message}`, e.loc, genMap.code);
 	}
 
 	// create source map and generated code
@@ -207,7 +209,9 @@ exports.generateSourceMap = function(generator, compileConfig) {
 			sourceType: 'module'
 		});
 	} catch (e) {
-		U.dieWithCodeFrame(`Error parsing code in ${target.filepath}. ${e.message}`, e.loc, genMap.code);
+		const filename = path.relative(compileConfig.dir.project, generator.target.template);
+
+		U.dieWithCodeFrame(`Error parsing code in ${filename}. ${e.message}`, e.loc, genMap.code);
 	}
 
 	// TODO: We do not run the babel plugins (optimizer/builtins) here. Is that ok?
