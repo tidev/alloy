0d97e00d33a8f9f63018941f88c93af6f78bd289
Brenton House <brenton.house@gmail.com>
2019-08-06 13:42:57 -0500

    added support for code xml element

---
+++ Alloy/commands/compile/parsers/Ti.UI.Code.js
@@ -0,0 +1,31 @@
+var _ = require('lodash');
+var CONST = require('../../../common/constants');
+var CU = require('../compilerUtils');
+var U = require('../../../utils');
+var fs = require('fs-extra');
+var path = require('path');
+
+exports.parse = function(node, state) {
+	var args = CU.getParserArgs(node, state);
+	var compilerConfig = CU.getCompilerConfig();
+	var code = '';
+
+	function getSourceCode(src) {
+		if ( src) {
+			const sourcePath = path.join(compilerConfig.dir.resourcesPlatform, src);
+			if (fs.existsSync(sourcePath)) {
+				return fs.readFileSync(sourcePath, 'utf8');
+			} 
+		}	
+	}
+
+	// get code from any external source
+	code += getSourceCode(args.createArgs.src) || '';
+
+	// get code from text node
+	code += U.XML.getNodeText(node) || '';
+
+	return {
+		code: code.trim() + '\n',
+	};
+};
