5a3988acaf47f5b186818bab7eb938c3811c9f7b
Ewan Harris <ewanharris93@gmail.com>
2020-08-28 15:11:43 +0100

    fix: handle widget case explicitly and revert back to previous default behaviour
    Fixes ALOY-1738

--- Alloy/commands/compile/parsers/Alloy.Require.js
+++ Alloy/commands/compile/parsers/Alloy.Require.js
@@ -129,19 +129,16 @@ function parse(node, state, args) {
 		args.createArgs,
 		state
 	) + ')';
-	let parent = { symbol: args.symbol };
-	if (args.parent.symbol && !state.templateObject && !state.androidMenu && !state.insideContainer) {
+	let parent = {
+		symbol: args.symbol + '.getViewEx({recurse:true})'
+	};
+	if (args.parent.symbol && !state.templateObject && !state.androidMenu) {
 		code += ';\n' + args.symbol + '.setParent(' + args.parent.symbol + ');\n';
-		parent = {
-			symbol: args.symbol + '.getViewEx({recurse:true})'
-		};
-	} else if ( state.insideContainer ) {
-		code += ';\n';
-		parent = {
-			symbol: args.symbol + '.getViewEx({recurse:true})'
-		};
-	} else {
+	} else if (type === 'widget') {
 		code += '.getViewEx({recurse:true});\n';
+		parent = { symbol: args.symbol };
+	} else {
+		code += ';\n';
 	}
 
 	return {
