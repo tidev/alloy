27392403b34655a50f1028992fef02c1d0695608
Rene Pot <rene@topener.nl>
2020-09-30 11:30:08 +0100

    Revert "remove mobileweb from test"
    This reverts commit 2dddeeff70617dee3417faec03cf69f37e6f8921.

--- test/specs/new.js
+++ test/specs/new.js
@@ -57,6 +57,7 @@ var TO_BE_CREATED = [
 	path.join('app','assets'),
 	path.join('app','assets','android'),
 	path.join('app','assets','iphone'),
+	path.join('app','assets','mobileweb'),
 	path.join('plugins'),
 	path.join('plugins','ti.alloy'),
 	path.join('plugins','ti.alloy','plugin.py'),
