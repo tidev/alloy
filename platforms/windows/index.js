7b341235e19372242b6f730839b7abc02d6e4fbb
Rene Pot <rene@topener.nl>
2020-09-30 11:30:05 +0100

    Revert "remove mobileweb and windows as they're no longer supported"
    This reverts commit 30f2c08ddd28ec9cf890908bb80eebad42c96a49.

---
+++ platforms/windows/index.js
@@ -0,0 +1,7 @@
+module.exports = new (require('../base.js'))({
+	platform: 'windows',
+	alloyFolder: 'windows',
+	titaniumFolder: 'windows',
+	name: 'Windows Phone',
+	osname: ['windowsphone', 'windowsstore']
+});
