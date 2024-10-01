e7fe3f10f012c96fbe89a16eecf3a6add12b4c7c
m1ga <migamiga@web.de>
2024-10-01 20:17:58 +0200

    just for testing

--- Alloy/tiapp.js
+++ Alloy/tiapp.js
@@ -44,7 +44,8 @@ function getSdkSelectVersion() {
 	var homeDir = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'],
 		file = path.join(homeDir, '.titanium', 'config.json');
 	if (!fs.existsSync(file)) {
-		U.die('Titanium configuration file does not exist at "' + file + '"');
+		//U.die('Titanium configuration file does not exist at "' + file + '"');
+		return ""
 	}
 	var ticonfig = JSON.parse(fs.readFileSync(file, {encoding: 'utf8'}));
 	return ticonfig.sdk.selected;
