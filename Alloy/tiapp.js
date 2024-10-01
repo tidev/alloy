b68578bc251349286f37638635ab7694765dc962
m1ga <migamiga@web.de>
2024-10-01 20:47:07 +0200

    ignore files again

--- Alloy/tiapp.js
+++ Alloy/tiapp.js
@@ -44,8 +44,7 @@ function getSdkSelectVersion() {
 	var homeDir = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'],
 		file = path.join(homeDir, '.titanium', 'config.json');
 	if (!fs.existsSync(file)) {
-		//U.die('Titanium configuration file does not exist at "' + file + '"');
-		return '12.5.0.GA';
+		U.die('Titanium configuration file does not exist at "' + file + '"');
 	}
 	var ticonfig = JSON.parse(fs.readFileSync(file, {encoding: 'utf8'}));
 	return ticonfig.sdk.selected;
