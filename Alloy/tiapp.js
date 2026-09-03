fdbdfdca5b629cd495260970b4695b8d78e5f2be
Michael Gangolf <m1ga@users.noreply.github.com>
2026-09-03 20:52:48 +0200

    Update Alloy/tiapp.js
    Co-authored-by: Chris Barber <chris@cb1inc.com>

--- Alloy/tiapp.js
+++ Alloy/tiapp.js
@@ -58,16 +58,6 @@ tiapp.getBundleId = function() {
 	}
 };
 
-function getSdkSelectVersion() {
-	var homeDir = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'],
-		file = path.join(homeDir, '.titanium', 'config.json');
-	if (!fs.existsSync(file)) {
-		U.die('Titanium configuration file does not exist at "' + file + '"');
-	}
-	var ticonfig = JSON.parse(fs.readFileSync(file, {encoding: 'utf8'}));
-	return ticonfig.sdk.selected;
-}
-
 // Get the value of a property from the tiapp.xml
 tiapp.getProperty = function(name) {
 	var props = doc.documentElement.getElementsByTagName('property');
